2fd02323c422e7f41094a6a6deda196e0e523d6d
Grant Heffernan <heffergm@gmail.com>
2014-03-27 08:43:48 -0400

    first stab at moving config files into this repo

---
+++ deploy/before_migrate.rb
@@ -0,0 +1,12 @@
+Chef::Log.info("Running deploy/before_migrate.rb...")
+
+Chef::Log.info('Creating tilestache config from template')
+template "#{node[:tilestache][:cfg_path]}/tilestache.conf" do
+  local     true
+  owner     node[:tilestache][:user]
+  group     node[:tilestache][:user]
+  mode      0664
+  source    "#{release_path}/deploy/templates/tilestache.conf.erb"
+  notifies  :restart, 'service[tilestache]', :delayed
+end
+
