f818f9063ca0a3fe21fb44aa0adc8742261a59b4
Grant Heffernan <heffergm@gmail.com>
2014-08-13 14:44:58 -0400

    no need to restart here

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,20 +1,11 @@
 Chef::Log.info("Running deploy/before_migrate.rb...")
 
 Chef::Log.info('Creating tilestache config from template')
-service 'tilestache' do
-  action :nothing
-end
-
-runit_service 'tilestache' do
-  action     :nothing
-  sv_timeout 60
-end 
 
 template "#{node[:tilestache][:cfg_path]}/tilestache.conf" do
-  local     true
-  owner     node[:tilestache][:user]
-  group     node[:tilestache][:user]
-  mode      0664
-  source    "#{release_path}/deploy/templates/tilestache.conf.erb"
-  notifies  :restart, 'runit_service[tilestache]', :delayed
+  local  true
+  owner  node[:tilestache][:user]
+  group  node[:tilestache][:user]
+  mode   0664
+  source "#{release_path}/deploy/templates/tilestache.conf.erb"
 end
