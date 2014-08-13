8b6e94b2760a12b610d82a19c147458eb0cafed4
Grant Heffernan <heffergm@gmail.com>
2014-08-13 14:15:28 -0400

    fix how we notify tilestache to resteart

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -5,12 +5,16 @@ service 'tilestache' do
   action :nothing
 end
 
+execute 'restart tilestache' do
+  action  :nothing
+  command 'sv -w 60 restart tilestache'
+end
+
 template "#{node[:tilestache][:cfg_path]}/tilestache.conf" do
   local     true
   owner     node[:tilestache][:user]
   group     node[:tilestache][:user]
   mode      0664
   source    "#{release_path}/deploy/templates/tilestache.conf.erb"
-  notifies  :restart, 'service[tilestache]', :delayed
+  notifies  :run, 'execute[restart tilestache]', :delayed
 end
-
