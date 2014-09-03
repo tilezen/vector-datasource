4bc3ddca6e89ebd67d8eee7e516c8f9a27185f79
Robert Marianski <rob@marianski.com>
2014-09-03 10:49:16 -0400

    take pgopts directly from cmd line
    Taking the postgresql options directly from the command line is more
    consistent with how the other sql scripts are used.

--- data/perform-sql-updates.sh
+++ data/perform-sql-updates.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
-if [ -z "$1" ]
-then
-    echo 'Missing database name'
-    exit 1
-fi
-
-# first arg is required to be the database name
-dbname="$1"
-
-# subsequent args are used to supplement the psql query connection string
-shift
 
 # subsequent sql depends on functions installed
-psql -d $dbname $@ -f functions.sql
+psql $@ -f functions.sql
 
 # apply updates in parallel across tables
-psql -d $dbname $@ -f apply-updates-non-planet-tables.sql &
-psql -d $dbname $@ -f apply-planet_osm_polygon.sql &
-psql -d $dbname $@ -f apply-planet_osm_line.sql &
-psql -d $dbname $@ -f apply-planet_osm_point.sql &
+psql $@ -f apply-updates-non-planet-tables.sql &
+psql $@ -f apply-planet_osm_polygon.sql &
+psql $@ -f apply-planet_osm_line.sql &
+psql $@ -f apply-planet_osm_point.sql &
 
 wait
 
 # triggers should get added last
-psql -d $dbname $@ -f triggers.sql
+psql $@ -f triggers.sql
