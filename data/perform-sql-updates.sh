e5a56227d4174a90b6999777796d5afb8be94b64
Grant Heffernan <heffergm@gmail.com>
2014-11-21 08:30:28 -0500

    newlines ftw

--- data/perform-sql-updates.sh
+++ data/perform-sql-updates.sh
@@ -6,7 +6,7 @@ psql $@ -f functions.sql
 echo "done."
 
 # apply updates in parallel across tables
-echo -e "Applying updates in parallel across tables... \c"
+echo -e "\nApplying updates in parallel across tables... \c"
 psql $@ -f apply-updates-non-planet-tables.sql &
 psql $@ -f apply-planet_osm_polygon.sql &
 psql $@ -f apply-planet_osm_line.sql &
@@ -15,8 +15,8 @@ wait
 echo "done."
 
 # triggers should get added last
-echo -e "Creating triggers... \c"
+echo -e "\nCreating triggers... \c"
 psql $@ -f triggers.sql
 echo "done."
 
-echo "All updates complete. Exiting."
+echo "\nAll updates complete. Exiting."
