036c9693a26d40f7a5ebebef8e27f2a98cc6a102
Yohan Boniface <yb@enix.org>
2015-01-18 18:36:58 +0100

    Add index on waterway lines

--- data/apply-planet_osm_line.sql
+++ data/apply-planet_osm_line.sql
@@ -26,5 +26,6 @@ UPDATE planet_osm_line AS line SET
     WHERE line.osm_id = road.osm_id;
 
 PERFORM mz_create_partial_index_if_not_exists('planet_osm_line_mz_road_level_index', 'planet_osm_line', 'mz_road_level', 'mz_road_level IS NOT NULL');
+PERFORM mz_create_partial_index_if_not_exists('planet_osm_line_waterway', 'planet_osm_line', 'waterway', 'waterway IS NOT NULL');
 
 END $$;
