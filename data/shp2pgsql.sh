d60d9e4f1630fc792ac5be99aac89e7aede4ce3a
Severyn Kozak <severyn.kozak@gmail.com>
2015-06-04 14:24:32 -0400

    Clean up shp2pgsql.sh
    data/shp2pgsql.sh
    	-Move the `shp2pgsql` command and all of its necessary arguments
    	into a function called `import_shapefile()`, to avoid repeating
    	them for each shapefile.

--- data/shp2pgsql.sh
+++ data/shp2pgsql.sh
@@ -2,18 +2,23 @@
 #
 # After unpacking the contents of each zip archive, pipe this script to psql.
 #
-shp2pgsql -dID -s 900913 -W Windows-1252 -g the_geom ne_10m_lakes-merc.shp ne_10m_lakes
-shp2pgsql -dID -s 900913 -W Windows-1252 -g the_geom ne_10m_ocean-merc.shp ne_10m_ocean
-shp2pgsql -dID -s 900913 -W Windows-1252 -g the_geom ne_10m_playas-merc.shp ne_10m_playas
-shp2pgsql -dID -s 900913 -W Windows-1252 -g the_geom ne_110m_lakes-merc.shp ne_110m_lakes
-shp2pgsql -dID -s 900913 -W Windows-1252 -g the_geom ne_110m_ocean-merc.shp ne_110m_ocean
-shp2pgsql -dID -s 900913 -W Windows-1252 -g the_geom ne_50m_lakes-merc.shp ne_50m_lakes
-shp2pgsql -dID -s 900913 -W Windows-1252 -g the_geom ne_50m_ocean-merc.shp ne_50m_ocean
-shp2pgsql -dID -s 900913 -W Windows-1252 -g the_geom ne_50m_playas-merc.shp ne_50m_playas
-shp2pgsql -dID -s 900913 -W Windows-1252 -g the_geom ne_50m_urban_areas-merc.shp ne_50m_urban_areas
-shp2pgsql -dID -s 900913 -W Windows-1252 -g the_geom ne_10m_urban_areas-merc.shp ne_10m_urban_areas
-shp2pgsql -dID -s 900913 -W Windows-1252 -g the_geom ne_10m_parks_and_protected_lands-merc.shp ne_10m_parks_and_protected_lands
-shp2pgsql -dID -s 900913 -W Windows-1252 -g the_geom ne_10m_land-tiled-merc.shp ne_10m_land
-shp2pgsql -dID -s 900913 -W Windows-1252 -g the_geom ne_50m_land-merc.shp ne_50m_land
-shp2pgsql -dID -s 900913 -W Windows-1252 -g the_geom ne_110m_land-merc.shp ne_110m_land
-shp2pgsql -dID -s 900913 -W Windows-1252 -g the_geom ne_10m_populated_places-merc.shp ne_10m_populated_places
+
+import_shapefile(){
+	shp2pgsql -dID -s 900913 -W Windows-1252 -g the_geom "$1" "$2"
+}
+
+import_shapefile ne_10m_lakes-merc.shp ne_10m_lakes
+import_shapefile ne_10m_ocean-merc.shp ne_10m_ocean
+import_shapefile ne_10m_playas-merc.shp ne_10m_playas
+import_shapefile ne_110m_lakes-merc.shp ne_110m_lakes
+import_shapefile ne_110m_ocean-merc.shp ne_110m_ocean
+import_shapefile ne_50m_lakes-merc.shp ne_50m_lakes
+import_shapefile ne_50m_ocean-merc.shp ne_50m_ocean
+import_shapefile ne_50m_playas-merc.shp ne_50m_playas
+import_shapefile ne_50m_urban_areas-merc.shp ne_50m_urban_areas
+import_shapefile ne_10m_urban_areas-merc.shp ne_10m_urban_areas
+import_shapefile ne_10m_parks_and_protected_lands-merc.shp ne_10m_parks_and_protected_lands
+import_shapefile ne_10m_land-tiled-merc.shp ne_10m_land
+import_shapefile ne_50m_land-merc.shp ne_50m_land
+import_shapefile ne_110m_land-merc.shp ne_110m_land
+import_shapefile ne_10m_populated_places-merc.shp ne_10m_populated_places
