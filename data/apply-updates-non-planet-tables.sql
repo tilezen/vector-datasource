27bfc232fea7bcfc29cfd2945429f0fa0cdba7ec
Robert Marianski <rob@marianski.com>
2015-06-17 12:04:33 -0400

    Remove centroid sql update
    Remove the centroid column and index from tables that no longer use it.

--- data/apply-updates-non-planet-tables.sql
+++ data/apply-updates-non-planet-tables.sql
@@ -32,16 +32,4 @@ PERFORM mz_create_index_if_not_exists('water_polygons_wayarea_index', 'water_pol
 -- additional updates
 PERFORM mz_create_index_if_not_exists('ne_10m_populated_places_scalerank_index', 'ne_10m_populated_places', 'scalerank');
 
-PERFORM AddGeometryColumn('ne_50m_urban_areas', 'mz_centroid', 900913, 'Geometry', 2);
-UPDATE ne_50m_urban_areas SET mz_centroid=ST_Centroid(the_geom);
-CREATE INDEX ne_50m_urban_areas_centroid_index ON ne_50m_urban_areas USING gist(mz_centroid);
-
-PERFORM AddGeometryColumn('ne_10m_parks_and_protected_lands', 'mz_centroid', 900913, 'Geometry', 2);
-UPDATE ne_10m_parks_and_protected_lands SET mz_centroid=ST_Centroid(the_geom);
-CREATE INDEX ne_10m_parks_and_protected_lands_centroid_index ON ne_10m_parks_and_protected_lands USING gist(mz_centroid);
-
-PERFORM AddGeometryColumn('ne_10m_urban_areas', 'mz_centroid', 900913, 'Geometry', 2);
-UPDATE ne_10m_urban_areas SET mz_centroid=ST_Centroid(the_geom);
-CREATE INDEX ne_10m_urban_areas_centroid_index ON ne_10m_urban_areas USING gist(mz_centroid);
-
 END $$;
