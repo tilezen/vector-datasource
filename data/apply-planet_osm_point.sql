51a2d3888474e92726943566001a6c7ca719e45e
Matt Amos <zerebubuth@gmail.com>
2015-09-18 17:55:59 +0100

    Don't index on POI level, as we'll most likely be using the geo index. Might add a partial index later, depends on performance.

--- data/apply-planet_osm_point.sql
+++ data/apply-planet_osm_point.sql
@@ -6,7 +6,9 @@ BEGIN
 --------------------------------------------------------------------------------
 
 CREATE INDEX planet_osm_point_place_index ON planet_osm_point(place) WHERE name IS NOT NULL AND place IN ('borough', 'city', 'continent', 'country', 'county', 'district', 'farm', 'hamlet', 'island', 'isolated_dwelling', 'lake', 'locality', 'neighbourhood', 'ocean', 'province', 'quarter', 'sea', 'state', 'suburb', 'town', 'village');
-CREATE INDEX planet_osm_point_level_index ON planet_osm_point(mz_calculate_poi_level("aerialway", "aeroway", "amenity", "barrier", "craft", "highway", "historic", "leisure", "lock", "man_made", "natural", "office", "power", "railway", "shop", "tourism", "waterway", 0::real)) WHERE mz_calculate_poi_level("aerialway", "aeroway", "amenity", "barrier", "craft", "highway", "historic", "leisure", "lock", "man_made", "natural", "office", "power", "railway", "shop", "tourism", "waterway", 0::real) IS NOT NULL;
+-- NOTE: removed index on mz_poi_min_zoom, possibly will replace with
+-- a partial index at some point in the future, depends on how the
+-- query performs.
 
 ALTER TABLE planet_osm_point ADD COLUMN mz_poi_min_zoom SMALLINT;
 -- the coalesce here is just an optimisation, as the poi level
