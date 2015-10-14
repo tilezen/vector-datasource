c2ab32638273f7960a66df4cb2ba9ebbb78964aa
Matt Amos <zerebubuth@gmail.com>
2015-10-14 18:57:46 +0100

    Add town halls as POIs.

--- data/functions.sql
+++ data/functions.sql
@@ -161,6 +161,7 @@ BEGIN
       WHEN historic_val = 'landmark'         THEN LEAST(zoom + 1.76, 15)
       WHEN leisure_val  = 'marina'           THEN LEAST(zoom + 3.45, 17)
       WHEN amenity_val  = 'place_of_worship' THEN LEAST(2 * zoom - 9.55, 17)
+      WHEN amenity_val  = 'townhall'         THEN LEAST(zoom + 1.85, 16)
       WHEN (barrier_val IN ('gate')
             OR craft_val IN ('sawmill')
             OR highway_val IN ('gate', 'mini_roundabout')
