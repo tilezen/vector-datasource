4775dacb4f1543e2da978e2c16924a738088f027
Robert Marianski <rob@marianski.com>
2015-10-28 17:05:26 -0400

    Update WOF table schema
    * Add hash, n_photos, and geometry columns
    * Add index on wof_id that will be used on updates

--- data/wof-schema.sql
+++ data/wof-schema.sql
@@ -1,8 +1,12 @@
 CREATE TABLE wof_neighbourhood (
-  wof_id BIGINT UNIQUE,
-  name TEXT NOT NULL
+  wof_id BIGINT UNIQUE NOT NULL,
+  name TEXT NOT NULL,
+  hash TEXT NOT NULL,
+  n_photos INTEGER,
+  label_position geometry(Point, 900913) NOT NULL,
+  geometry geometry(Geometry, 900913) NOT NULL
 );
 
-SELECT AddGeometryColumn ('wof_neighbourhood', 'label_position', 900913, 'POINT', 2);
-
+CREATE INDEX wof_neighbourhood_wof_id_index ON wof_neighbourhood(wof_id);
 CREATE INDEX wof_neighbourhood_label_position_index ON wof_neighbourhood USING GIST(label_position);
+CREATE INDEX wof_neighbourhood_geometry_index ON wof_neighbourhood USING GIST(geometry);
