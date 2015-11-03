ece4db37562ea18fa2c41238424e4623125d3161
Robert Marianski <rob@marianski.com>
2015-11-03 17:00:08 -0500

    Add lookup table for placetype code -> string

--- data/wof-schema.sql
+++ data/wof-schema.sql
@@ -12,3 +12,13 @@ CREATE TABLE wof_neighbourhood (
 CREATE INDEX wof_neighbourhood_wof_id_index ON wof_neighbourhood(wof_id);
 CREATE INDEX wof_neighbourhood_label_position_index ON wof_neighbourhood USING GIST(label_position);
 CREATE INDEX wof_neighbourhood_geometry_index ON wof_neighbourhood USING GIST(geometry);
+
+CREATE TABLE wof_neighbourhood_placetype (
+  placetype_code SMALLINT UNIQUE NOT NULL,
+  placetype_string text NOT NULL
+);
+
+INSERT INTO wof_neighbourhood_placetype VALUES
+  (1, 'neighbourhood'),
+  (2, 'microhood'),
+  (3, 'macrohood');
