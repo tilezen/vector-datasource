3766e122f1d4e2e5ead195b4fe0eec4e3cb8d567
Stephen Caraher <moskvax@gmail.com>
2015-11-10 14:03:02 +1100

    Add aerialway to mz_calculate_road_level call in line table trigger

--- data/triggers.sql
+++ data/triggers.sql
@@ -33,8 +33,8 @@ CREATE TRIGGER mz_trigger_point BEFORE INSERT OR UPDATE ON planet_osm_point FOR 
 CREATE OR REPLACE FUNCTION mz_trigger_function_line()
 RETURNS TRIGGER AS $$
 BEGIN
-    NEW.mz_road_level := mz_calculate_road_level(NEW.highway, NEW.railway, NEW.aeroway, NEW.route, NEW.service, NEW.way);
-    NEW.mz_transit_level := mz_calculate_transit_level(NEW.route);
+    NEW.mz_road_level := mz_calculate_road_level(NEW."highway", NEW."railway", NEW."aeroway", NEW."route", NEW."service", NEW."aerialway", NEW."way");
+    NEW.mz_transit_level := mz_calculate_transit_level(NEW."route");
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql VOLATILE;
