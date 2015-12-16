ea8a8e62b35289acd095ed39d721c826a6d362f4
Matt Amos <zerebubuth@gmail.com>
2015-12-16 20:02:30 +0000

    Fix missing extra shop & amenity tags to update.

--- data/migrations/v0.6.0-planet_osm_polygon.sql
+++ data/migrations/v0.6.0-planet_osm_polygon.sql
@@ -23,10 +23,10 @@ WHERE landuse IN ('rural', 'military', 'winter_sports')
 UPDATE planet_osm_polygon
   SET mz_poi_min_zoom = mz_calculate_poi_level("aerialway", "aeroway", "amenity", "barrier", "craft", "highway", "historic", "leisure", "lock", "man_made", "natural", "office", "power", "railway", "tags"->'rental', "shop", "tourism", "waterway", way_area)
 WHERE railway = 'station'
-   OR shop IN ('toys', 'ski')
+   OR shop IN ('toys', 'ski', 'alcohol', 'wine', 'ice_cream')
    OR "natural" = 'beach'
    OR tags->'rental' = 'ski'
-   OR amenity IN ('ski_rental', 'ski_school')
+   OR amenity IN ('ski_rental', 'ski_school', 'ice_cream')
    OR man_made = 'snow_cannon'
    OR highway = 'motorway_junction'
    OR tourism = 'zoo';
