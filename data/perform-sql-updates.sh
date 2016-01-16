fef686084b7b274158c354f4c32c63687b4761a8
Matt Amos <zerebubuth@gmail.com>
2016-01-16 16:30:26 +0000

    Move WOF table creation to start of updates script so that the WOF tables are present when the functions are defined.

--- data/perform-sql-updates.sh
+++ data/perform-sql-updates.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# apply wof schema first, as functions now depend on it.
+echo "Creating WOF schema..."
+psql $@ -f wof-schema.sql
+echo "done."
+
 # subsequent sql depends on functions installed
 echo "Creating functions..."
 psql $@ -f functions.sql
@@ -11,7 +16,6 @@ psql $@ -f apply-updates-non-planet-tables.sql &
 psql $@ -f apply-planet_osm_polygon.sql &
 psql $@ -f apply-planet_osm_line.sql &
 psql $@ -f apply-planet_osm_point.sql &
-psql $@ -f wof-schema.sql &
 wait
 echo "done."
 
