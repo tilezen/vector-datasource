08c6de4177ea89ab2949ebbeb6595c8a8d1948db
Matt Amos <zerebubuth@gmail.com>
2016-01-22 15:09:32 +0000

    Clearing out v0.7 migrations, starting v0.8 ones.

--- data/migrations/v0.7.0-planet_osm_line.sql
+++
@@ -1,6 +0,0 @@
-UPDATE planet_osm_line
-  SET mz_road_level = mz_calculate_road_level(
-    highway, railway, aeroway, route, service, aerialway, leisure, sport,
-    man_made, way)
-  WHERE
-    highway = 'motorway_link';
