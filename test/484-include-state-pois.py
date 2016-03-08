c463c707b14a663f65a39ebb98a8fe9690c11f2d
Matt Amos <zerebubuth@gmail.com>
2016-03-08 17:57:04 +0000

    Add state of stations.

---
+++ test/484-include-state-pois.py
@@ -0,0 +1,7 @@
+assert_has_feature(
+    18, 42390, 101119, 'pois',
+    {'id': 3353451464, 'state': 'proposed'})
+
+assert_has_feature(
+    19, 84630, 202201, 'pois',
+    {'id': 3354463416, 'state': 'proposed'})
