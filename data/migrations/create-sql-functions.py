bdac7d2171eed1e142b94984810fa9eee9e0b243
Robert Marianski <rob@marianski.com>
2016-03-11 12:26:35 -0500

    Support kind and default rules

--- data/migrations/create-sql-functions.py
+++ data/migrations/create-sql-functions.py
@@ -4,13 +4,14 @@ from jinja2 import FileSystemLoader
 from TileStache.Goodies.VecTiles.transform import _parse_kt
 import csv
 
-Rule = namedtuple('Rule', 'min_zoom_calc equals not_exists set_memberships')
+Rule = namedtuple('Rule', 'calc equals not_exists set_memberships default_rule')
 
 
-def create_rule(keys, row, min_zoom_calc):
+def create_rule(keys, row, calc):
     equals = []
     not_exists = []
     set_memberships = []
+    default_rule = None
     for key, matcher in zip(keys, row):
         assert matcher, 'Invalid value for row: %s' % row
         # skip all * values
@@ -23,8 +24,10 @@ def create_rule(keys, row, min_zoom_calc):
             set_memberships.append((key, candidates))
         else:
             equals.append((key, matcher))
-    assert equals or not_exists, 'Invalid rule: %s' % row
-    return Rule(min_zoom_calc, equals, not_exists, set_memberships)
+    if not equals and not not_exists and not set_memberships:
+        default_rule = calc
+    return Rule(
+        calc, equals, not_exists, set_memberships, default_rule)
 
 
 def format_key(key):
@@ -37,6 +40,7 @@ def format_key(key):
 
 def create_case_statement(rules):
     when_parts = []
+    default_rule = None
     for rule in rules:
         when_equals = ''
         when_not_exists = ''
@@ -44,6 +48,8 @@ def create_case_statement(rules):
 
         when_conds = []
 
+        # TODO consider splitting this up into separate functions to
+        # more easily test
         if rule.equals:
             when_equals_parts = ['%s = \'%s\'' % (format_key(key), matcher)
                                  for key, matcher in rule.equals]
@@ -63,24 +69,39 @@ def create_case_statement(rules):
             when_in = ' AND '.join(when_in_parts)
             when_conds.append(when_in)
 
+        if rule.default_rule:
+            assert not default_rule, 'Multiple default rules detected'
+            # indent
+            default_rule = '    ELSE %s' % rule.default_rule
+            continue
+
         when_cond = ' AND '.join(when_conds)
-        when_part = 'WHEN %s THEN %s' % (when_cond, rule.min_zoom_calc)
+        when_part = 'WHEN %s THEN %s' % (when_cond, rule.calc)
+        # indent
+        when_part = '    %s' % when_part
         when_parts.append(when_part)
 
-    case_statement = 'CASE\n%s\nEND' % '\n'.join(when_parts)
+    if default_rule:
+        when_parts.append(default_rule)
+    when_block = '\n'.join(when_parts)
+
+    case_statement = 'CASE\n%s\n  END' % when_block
     return case_statement
 
 
-rules = []
+kind_rules = []
+min_zoom_rules = []
 with open('../../spreadsheets/kind/landuse.csv') as fh:
     reader = csv.reader(fh, skipinitialspace=True)
     keys = None
     for row in reader:
         if keys is None:
             # assume the last key is the kind value
-            row.pop(-1)
+            kind = row.pop(-1)
+            assert kind == 'kind'
             # assume the second to last key is the min_zoom
-            # TODO this might need to be changed once we have more files in play
+            # TODO this might need to be changed once we have more
+            # files in play
             min_zoom = row.pop(-1)
             assert min_zoom == 'min_zoom'
             keys = []
@@ -89,21 +110,35 @@ with open('../../spreadsheets/kind/landuse.csv') as fh:
                 keys.append(key)
         else:
             # assume kind is last
-            _ = row.pop(-1)
+            kind_calc = row.pop(-1)
             # and next is the min_zoom calculation
             min_zoom_calc = row.pop(-1)
-            # is this variable syntax necessary?
-            # we can assume that the string will just be valid sql
-            # TODO remove the variable syntax
-            assert (min_zoom_calc.startswith('${') and
-                    min_zoom_calc.endswith('}')), 'Unexpected min_zoom'
-            min_zoom_calc = min_zoom_calc[2:-1]
-            rule = create_rule(keys, row, min_zoom_calc)
-            rules.append(rule)
 
-case_statement = create_case_statement(rules)
+            if min_zoom_calc and min_zoom_calc != '*':
+                # is this variable syntax necessary?
+                # we can assume that the string will just be valid sql
+                # TODO remove the variable syntax
+                assert (min_zoom_calc.startswith('${') and
+                        min_zoom_calc.endswith('}')), 'Unexpected min_zoom'
+                min_zoom_calc = min_zoom_calc[2:-1]
+                min_zoom_rule = create_rule(keys, row, min_zoom_calc)
+                min_zoom_rules.append(min_zoom_rule)
+
+            if kind_calc and kind_calc != '*':
+                # TODO we might want to allow the ability to execute
+                # arbitrary sql here. For now we will assume it's a plain
+                # string though
+                kind_calc = "'%s'" % kind_calc
+                kind_rule = create_rule(keys, row, kind_calc)
+                kind_rules.append(kind_rule)
+
+landuse_kind_case = create_case_statement(kind_rules)
+landuse_min_zoom_case = create_case_statement(min_zoom_rules)
 template_name = 'sql.jinja2'
 environment = Environment(loader=FileSystemLoader('.'))
 template = environment.get_template(template_name)
-sql = template.render(case_statement=case_statement)
+sql = template.render(
+    landuse_kind=landuse_kind_case,
+    landuse_level=landuse_min_zoom_case,
+)
 print sql
