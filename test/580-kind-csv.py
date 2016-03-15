40823606c81b3e842cab670a3e13cd171e455c83
Robert Marianski <rob@marianski.com>
2016-03-15 17:16:22 -0400

    Add kind tests

---
+++ test/580-kind-csv.py
@@ -0,0 +1,23 @@
+assert_has_feature(
+    18, 41944, 101306, 'pois',
+    { 'kind': 'post_office' })
+
+assert_has_feature(
+    18, 41942, 101317, 'pois',
+    { 'kind': 'museum' })
+
+assert_has_feature(
+    18, 41951, 101317, 'pois',
+    { 'kind': 'gate' })
+
+assert_has_feature(
+    18, 41867, 101362, 'pois',
+    { 'kind': 'enclosure' })
+
+assert_has_feature(
+    18, 41887, 101370, 'landuse',
+    { 'kind': 'forest' })
+
+assert_has_feature(
+    18, 41913, 101317, 'landuse',
+    { 'kind': 'substation' })
