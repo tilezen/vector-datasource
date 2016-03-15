061a20a696a82afcc0c155e3c2ddce2543ecaf7b
Robert Marianski <rob@marianski.com>
2016-03-15 14:56:20 -0400

    Add migrations

--- data/migrations/v0.9.0-cleanup.sql
+++ data/migrations/v0.9.0-cleanup.sql
@@ -2,3 +2,25 @@
 DROP FUNCTION IF EXISTS mz_calculate_poi_level(
   text, text, text, text, text, text, text, text, text, text, text, text, text,
   text, text, text, text, text, hstore, real);
+
+ALTER TABLE planet_osm_polygon DROP COLUMN IF EXISTS mz_is_landuse;
+
+BEGIN;
+
+DROP INDEX planet_osm_polygon_landuse_geom_9_index;
+ALTER INDEX new_planet_osm_polygon_landuse_geom_9_index RENAME TO planet_osm_polygon_landuse_geom_9_index;;
+DROP INDEX planet_osm_polygon_landuse_geom_12_index;
+ALTER INDEX new_planet_osm_polygon_landuse_geom_12_index RENAME TO planet_osm_polygon_landuse_geom_12_index;
+DROP INDEX planet_osm_polygon_landuse_geom_15_index;
+ALTER INDEX new_planet_osm_polygon_landuse_geom_15_index RENAME TO planet_osm_polygon_landuse_geom_15_index;
+
+DROP INDEX planet_osm_polygon_landuse_boundary_geom_4_index;
+ALTER INDEX new_planet_osm_polygon_landuse_boundary_geom_4_index RENAME TO planet_osm_polygon_landuse_boundary_geom_4_index;
+DROP INDEX planet_osm_polygon_landuse_boundary_geom_6_index;
+ALTER INDEX new_planet_osm_polygon_landuse_boundary_geom_6_index RENAME TO planet_osm_polygon_landuse_boundary_geom_6_index;
+DROP INDEX planet_osm_polygon_landuse_boundary_geom_8_index;
+ALTER INDEX new_planet_osm_polygon_landuse_boundary_geom_8_index RENAME TO planet_osm_polygon_landuse_boundary_geom_8_index;
+
+COMMIT;
+
+ANALYZE planet_osm_polygon;
