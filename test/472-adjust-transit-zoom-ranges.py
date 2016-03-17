332e1c1c68db8ff409c1636136c4429404397564
Robert Marianski <rob@marianski.com>
2016-03-17 13:55:42 -0400

    Adjust zoom ranges for subway, light_rail, tram

---
+++ test/472-adjust-transit-zoom-ranges.py
@@ -0,0 +1,11 @@
+assert_has_feature(
+    8, 40, 98, 'transit',
+    { 'kind': 'subway' })
+
+assert_has_feature(
+    9, 81, 197, 'transit',
+    { 'kind': 'light_rail' })
+
+assert_has_feature(
+    9, 81, 197, 'transit',
+    { 'kind': 'tram' })
