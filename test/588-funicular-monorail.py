09e0073a2aec6abc32a639fb2791af19e17eed1c
Robert Marianski <rob@marianski.com>
2016-03-18 11:53:16 -0400

    Add test for funicular feature

--- test/588-funicular-monorail.py
+++ test/588-funicular-monorail.py
@@ -2,3 +2,8 @@
 assert_has_feature(
     16, 10486, 25367, 'transit',
     { 'kind': 'monorail' })
+
+# relation 6060405
+assert_has_feature(
+    16, 18201, 24705, 'transit',
+    { 'kind': 'funicular' })
