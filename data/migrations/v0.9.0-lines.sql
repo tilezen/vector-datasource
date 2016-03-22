6c89152ac4a192dfd319680135c3d79582c5c8b2
Robert Marianski <rob@marianski.com>
2016-03-22 18:12:42 -0400

    Add transit migration and update initial data load

--- data/migrations/v0.9.0-lines.sql
+++ data/migrations/v0.9.0-lines.sql
@@ -5,7 +5,10 @@ UPDATE planet_osm_line
     railway IN ('subway', 'funicular');
 
 UPDATE planet_osm_line
-  SET mz_transit_level = mz_calculate_transit_level(route, service)
+  SET mz_transit_level = mz_calculate_min_zoom_transit(planet_osm_line.*)
   WHERE
-    route IN ('railway', 'subway', 'light_rail', 'tram', 'funicular', 'monorail')
-    OR (route = 'train' AND service IN ('high_speed', 'long_distance', 'international'));
+    route IN ('railway', 'subway', 'light_rail', 'tram', 'funicular', 'monorail', 'train')
+    OR railway IN ('halt', 'stop', 'tram_stop', 'platform', 'station')
+    OR public_transport IN ('platform', 'stop_area')
+    OR highway IN ('platform', 'bus_stop')
+    OR tags->'site' = 'stop_area';
--- data/migrations/v0.9.0-points.sql
+++ data/migrations/v0.9.0-points.sql
@@ -2,5 +2,8 @@ UPDATE planet_osm_point SET
   mz_poi_min_zoom = mz_calculate_min_zoom_pois(planet_osm_point.*)
   WHERE
     "disused" <> 'no'
-    OR "railway" IN ('station', 'halt', 'tram_stop')
+    OR "railway" IN ('station', 'halt', 'tram_stop', 'platform', 'stop')
+    OR "highway" IN ('platform', 'bus_stop')
+    OR public_transport IN ('platform', 'stop_area')
+    OR tags->'site' = 'stop_area'
     OR "aerialway" = 'station';
