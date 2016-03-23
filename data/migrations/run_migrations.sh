0400e16eb87c7782ffd603dbd362ccf3d11c0c99
Robert Marianski <rob@marianski.com>
2016-03-23 16:03:44 -0400

    Generate functions again when running migration

--- data/migrations/run_migrations.sh
+++ data/migrations/run_migrations.sh
@@ -56,6 +56,11 @@ for table in planet_osm_point planet_osm_line planet_osm_polygon; do
     psql -c "ALTER TABLE ${table} ENABLE TRIGGER USER" $*
 done
 
+# re-generate the functions to avoid issues when a migration updates
+# the schema
+python ${migration_dir}/create-sql-functions.py | psql --set ON_ERROR_STOP=1 $*
+if [ $? -ne 0 ]; then echo "Installing generated functions second time failed.">&2; exit 1; fi
+
 for python in ${migration_dir}/*.py; do
     # break the loop if the file doesn't exist - this is generally the case
     # if the glob matches nothing and we end up looking for a file which is
