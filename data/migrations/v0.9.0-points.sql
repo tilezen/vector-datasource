bb4d16e293b6251a769f3a37acf639c8bdeec4cc
Matt Amos <zerebubuth@gmail.com>
2016-03-29 19:26:48 +0100

    Remove v0.9.0 migrations in preparation for starting v0.10.0.

--- data/migrations/v0.9.0-points.sql
+++
@@ -1,9 +0,0 @@
-UPDATE planet_osm_point SET
-  mz_poi_min_zoom = mz_calculate_min_zoom_pois(planet_osm_point.*)
-  WHERE
-    "disused" <> 'no'
-    OR "railway" IN ('station', 'halt', 'tram_stop', 'platform', 'stop')
-    OR "highway" IN ('platform', 'bus_stop')
-    OR public_transport IN ('platform', 'stop_area')
-    OR tags->'site' = 'stop_area'
-    OR "aerialway" = 'station';
