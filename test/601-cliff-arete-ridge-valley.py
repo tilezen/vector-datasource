6d5f1afdfe963a927e510a9a2e044fbd4796184f
Nathaniel Kelso <nvkelso@gmail.com>
2016-04-08 17:41:01 -0400

    [tests] add tests for cliff, arete, ridge, valley

---
+++ test/601-cliff-arete-ridge-valley.py
@@ -0,0 +1,23 @@
+#cliff in Yosemite
+assert_has_feature(
+    13, 1374, 3166, "earth",
+    {"kind": "cliff", "id": 291684864,
+     "sort_key": 227})
+
+#arete in Yosemite
+assert_has_feature(
+    13, 1379, 3164, "earth",
+    {"kind": "arete", "id": 375271242,
+     "sort_key": 228})
+
+#ridge with name in Santa Cruz Mountains, California
+assert_has_feature(
+    13, 1317, 3182, "earth",
+    {"kind": "ridge", "id": 115675159,
+     "name": "Castle Rock Ridge", "label_placement": 'yes'})
+
+#valley with name in Yosemite
+assert_has_feature(
+    13, 1381, 3164, "earth",
+    {"kind": "valley", "id": 407467016,
+     "name": "Lyell Canyon", "label_placement": 'yes'})
