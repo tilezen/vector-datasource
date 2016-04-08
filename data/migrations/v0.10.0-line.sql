0fbb24d9be300d17bd032f30c5036cf255b618ee
Nathaniel Kelso <nvkelso@gmail.com>
2016-04-08 17:43:34 -0400

    * add new index for select natural features

--- data/migrations/v0.10.0-line.sql
+++ data/migrations/v0.10.0-line.sql
@@ -10,14 +10,18 @@ CREATE INDEX new_planet_osm_line_boundary_geom_9_index ON planet_osm_line USING 
 CREATE INDEX new_planet_osm_line_boundary_geom_12_index ON planet_osm_line USING gist(way) WHERE mz_boundary_min_zoom <= 12;
 CREATE INDEX new_planet_osm_line_boundary_geom_15_index ON planet_osm_line USING gist(way) WHERE mz_boundary_min_zoom <= 15;
 
+CREATE INDEX new_planet_osm_line_natural_geom_index ON planet_osm_line USING gist(way) WHERE "natural" IN ('cliff','arete','ridge','valley');
+
 BEGIN;
   DROP INDEX IF EXISTS planet_osm_line_water_geom_9_index;
   DROP INDEX IF EXISTS planet_osm_line_water_geom_12_index;
   DROP INDEX IF EXISTS planet_osm_line_water_geom_15_index;
+  DROP INDEX IF EXISTS planet_osm_line_natural_geom_index;
 
   ALTER INDEX new_planet_osm_line_water_geom_9_index RENAME TO planet_osm_line_water_geom_9_index;
   ALTER INDEX new_planet_osm_line_water_geom_12_index RENAME TO planet_osm_line_water_geom_12_index;
   ALTER INDEX new_planet_osm_line_water_geom_15_index RENAME TO planet_osm_line_water_geom_15_index;
+  ALTER INDEX new_planet_osm_line_natural_geom_index RENAME TO planet_osm_line_natural_geom_index;
 
   DROP INDEX IF EXISTS planet_osm_line_boundary_geom_9_index;
   DROP INDEX IF EXISTS planet_osm_line_boundary_geom_12_index;
@@ -27,3 +31,4 @@ BEGIN;
   ALTER INDEX new_planet_osm_line_boundary_geom_12_index RENAME TO planet_osm_line_boundary_geom_12_index;
   ALTER INDEX new_planet_osm_line_boundary_geom_15_index RENAME TO planet_osm_line_boundary_geom_15_index;
 COMMIT;
+
