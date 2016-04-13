90b7b30770d019886756b553524460b90964261f
Matt Amos <zerebubuth@gmail.com>
2016-04-13 16:32:55 +0100

    Add whitewater parameter to function.

--- data/triggers.sql
+++ data/triggers.sql
@@ -63,7 +63,7 @@ DECLARE
   new_way_ids bigint[] := CASE WHEN is_new_path_major_route_relation THEN NEW.parts[NEW.way_off+1:NEW.rel_off] ELSE ARRAY[]::bigint[] END;
 BEGIN
   UPDATE planet_osm_line
-    SET mz_road_level = mz_calculate_road_level("highway", "railway", "aeroway", "route", "service", "aerialway", "leisure", "sport", "man_made", "way", "name", "bicycle", "foot", "horse", tags->'snowmobile', tags->'ski', osm_id)
+    SET mz_road_level = mz_calculate_road_level("highway", "railway", "aeroway", "route", "service", "aerialway", "leisure", "sport", "man_made", "way", "name", "bicycle", "foot", "horse", tags->'snowmobile', tags->'ski', osm_id, tags->'whitewater')
     WHERE (array[osm_id] <@ old_way_ids) <> (array[osm_id] <@ new_way_ids);
   RETURN NEW;
 END;
@@ -76,7 +76,7 @@ DECLARE
   new_way_ids bigint[] := CASE WHEN is_new_path_major_route_relation THEN NEW.parts[NEW.way_off+1:NEW.rel_off] ELSE ARRAY[]::bigint[] END;
 BEGIN
   UPDATE planet_osm_line
-    SET mz_road_level = mz_calculate_road_level("highway", "railway", "aeroway", "route", "service", "aerialway", "leisure", "sport", "man_made", "way", "name", "bicycle", "foot", "horse", tags->'snowmobile', tags->'ski', osm_id)
+    SET mz_road_level = mz_calculate_road_level("highway", "railway", "aeroway", "route", "service", "aerialway", "leisure", "sport", "man_made", "way", "name", "bicycle", "foot", "horse", tags->'snowmobile', tags->'ski', osm_id, tags->'whitewater')
     WHERE (array[osm_id] <@ new_way_ids);
     RETURN NEW;
 END;
@@ -89,7 +89,7 @@ DECLARE
   old_way_ids bigint[] := CASE WHEN is_old_path_major_route_relation THEN OLD.parts[OLD.way_off+1:OLD.rel_off] ELSE ARRAY[]::bigint[] END;
 BEGIN
   UPDATE planet_osm_line
-    SET mz_road_level = mz_calculate_road_level("highway", "railway", "aeroway", "route", "service", "aerialway", "leisure", "sport", "man_made", "way", "name", "bicycle", "foot", "horse", tags->'snowmobile', tags->'ski', osm_id)
+    SET mz_road_level = mz_calculate_road_level("highway", "railway", "aeroway", "route", "service", "aerialway", "leisure", "sport", "man_made", "way", "name", "bicycle", "foot", "horse", tags->'snowmobile', tags->'ski', osm_id, tags->'whitewater')
     WHERE (array[osm_id] <@ old_way_ids);
     RETURN NULL;
 END;
