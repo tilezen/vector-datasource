50ac34a2a3a8299bbfcaa30355a362e1c66ce3e8
Nathaniel Kelso <nvkelso@gmail.com>
2016-04-13 22:15:03 -0700

    [migration] add outdoor shops for OSM point and polygon

--- data/migrations/v0.10.0-point.sql
+++ data/migrations/v0.10.0-point.sql
@@ -2,7 +2,7 @@ UPDATE planet_osm_point
 SET mz_poi_min_zoom = mz_calculate_min_zoom_pois(planet_osm_point.*)
 WHERE
   amenity = 'boat_rental' OR
-  shop = 'boat_rental' OR
+  shop IN ('boat_rental', 'fishing', 'hunting', 'outdoor', 'scuba_diving', 'gas', 'motorcycle') OR
   tags->'rental' = 'boat' OR
   (shop = 'boat' AND tags->'rental' = 'yes') OR
   man_made IN ('beacon', 'cross', 'mineshaft', 'adit', 'water_well') OR
