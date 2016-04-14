3f3c00c849a4401e5a300dd9f2d33e9e726cd8fd
Nathaniel Kelso <nvkelso@gmail.com>
2016-04-13 22:35:44 -0700

    [migration] add ranger_station amenity migration

--- data/migrations/v0.10.0-polygon.sql
+++ data/migrations/v0.10.0-polygon.sql
@@ -1,7 +1,7 @@
 UPDATE planet_osm_polygon
 SET mz_poi_min_zoom = mz_calculate_min_zoom_pois(planet_osm_polygon.*)
 WHERE
-  amenity = 'boat_rental' OR
+  amenity = ('boat_rental', 'ranger_station') OR
   shop IN ('boat_rental', 'fishing', 'hunting', 'outdoor', 'scuba_diving', 'gas', 'motorcycle') OR
   tags->'rental' = 'boat' OR
   (shop = 'boat' AND tags->'rental' = 'yes') OR
