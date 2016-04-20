ebfabe7c61937cda9025b0d3567f0e579669f584
Nathaniel Kelso <nvkelso@gmail.com>
2016-04-20 10:38:38 -0700

    init bike stuff

---
+++ test/592-add-adjust-bicycle-pois.py
@@ -0,0 +1,12 @@
+#http://www.openstreetmap.org/node/414269441
+# Valencia Cyclery in SF
+assert_has_feature(
+    15, 5240, 12667, 'pois',
+    { 'kind': 'bicycle', 'min_zoom': 15 })
+
+#http://www.openstreetmap.org/node/414269441
+# Valencia Cyclery in SF
+assert_has_feature(
+    15, 5240, 12667, 'pois',
+    { 'kind': 'bicycle', 'min_zoom': 15 })
+
