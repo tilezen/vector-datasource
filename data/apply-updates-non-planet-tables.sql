b3efd2672044c5e8b08003b3a2f0a41217031ff7
Nathaniel Kelso <nvkelso@gmail.com>
2016-04-21 02:05:13 -0700

    improve legibility

--- data/apply-updates-non-planet-tables.sql
+++ data/apply-updates-non-planet-tables.sql
@@ -1,14 +1,21 @@
 DO $$
 BEGIN
 
+UPDATE ne_10m_urban_areas SET way_area=ST_Area(the_geom) WHERE the_geom IS NOT NULL;
+CREATE INDEX ne_10m_urban_areas_way_area_index ON ne_10m_urban_areas(way_area);
+
+UPDATE ne_50m_urban_areas SET way_area=ST_Area(the_geom) WHERE the_geom IS NOT NULL;
+CREATE INDEX ne_50m_urban_areas_way_area_index ON ne_50m_urban_areas(way_area);
+
+UPDATE ne_10m_parks_and_protected_lands SET way_area=ST_Area(the_geom) WHERE the_geom IS NOT NULL;
+CREATE INDEX ne_10m_parks_and_protected_lands_way_area_index ON ne_10m_parks_and_protected_lands(way_area);
+
+
 UPDATE ne_110m_ocean SET way_area=ST_Area(the_geom) WHERE the_geom IS NOT NULL;
 CREATE INDEX ne_110m_ocean_wayarea_index ON ne_110m_ocean(way_area);
 
 UPDATE ne_110m_lakes SET way_area=ST_Area(the_geom) WHERE the_geom IS NOT NULL;
 CREATE INDEX ne_110m_lakes_wayarea_index ON ne_110m_lakes(way_area);
-
-UPDATE ne_110m_land SET way_area=ST_Area(the_geom) WHERE the_geom IS NOT NULL;
-CREATE INDEX ne_110m_land_wayarea_index ON ne_110m_land(way_area);
 
 UPDATE ne_50m_ocean SET way_area=ST_Area(the_geom) WHERE the_geom IS NOT NULL;
 CREATE INDEX ne_50m_ocean_wayarea_index ON ne_50m_ocean(way_area);
@@ -19,9 +26,6 @@ CREATE INDEX ne_50m_lakes_wayarea_index ON ne_50m_lakes(way_area);
 UPDATE ne_50m_playas SET way_area=ST_Area(the_geom) WHERE the_geom IS NOT NULL;
 CREATE INDEX ne_50m_playas_wayarea_index ON ne_50m_playas(way_area);
 
-UPDATE ne_50m_land SET way_area=ST_Area(the_geom) WHERE the_geom IS NOT NULL;
-CREATE INDEX ne_50m_land_wayarea_index ON ne_50m_land(way_area);
-
 UPDATE ne_10m_ocean SET way_area=ST_Area(the_geom) WHERE the_geom IS NOT NULL;
 CREATE INDEX ne_10m_ocean_wayarea_index ON ne_10m_ocean(way_area);
 
@@ -31,21 +35,21 @@ CREATE INDEX ne_10m_lakes_wayarea_index ON ne_10m_lakes(way_area);
 UPDATE ne_10m_playas SET way_area=ST_Area(the_geom) WHERE the_geom IS NOT NULL;
 CREATE INDEX ne_10m_playas_wayarea_index ON ne_10m_playas(way_area);
 
-UPDATE ne_10m_urban_areas SET way_area=ST_Area(the_geom) WHERE the_geom IS NOT NULL;
-CREATE INDEX ne_10m_urban_areas_way_area_index ON ne_10m_urban_areas(way_area);
+UPDATE water_polygons SET way_area=ST_Area(the_geom) WHERE the_geom IS NOT NULL;
 
-UPDATE ne_50m_urban_areas SET way_area=ST_Area(the_geom) WHERE the_geom IS NOT NULL;
-CREATE INDEX ne_50m_urban_areas_way_area_index ON ne_50m_urban_areas(way_area);
 
-UPDATE ne_10m_parks_and_protected_lands SET way_area=ST_Area(the_geom) WHERE the_geom IS NOT NULL;
-CREATE INDEX ne_10m_parks_and_protected_lands_way_area_index ON ne_10m_parks_and_protected_lands(way_area);
+CREATE INDEX water_polygons_wayarea_index ON water_polygons(way_area);
+
+
+UPDATE ne_110m_land SET way_area=ST_Area(the_geom) WHERE the_geom IS NOT NULL;
+CREATE INDEX ne_110m_land_wayarea_index ON ne_110m_land(way_area);
+
+UPDATE ne_50m_land SET way_area=ST_Area(the_geom) WHERE the_geom IS NOT NULL;
+CREATE INDEX ne_50m_land_wayarea_index ON ne_50m_land(way_area);
 
 UPDATE ne_10m_land SET way_area=ST_Area(the_geom) WHERE the_geom IS NOT NULL;
 CREATE INDEX ne_10m_land_way_area_index ON ne_10m_land(way_area);
 
-
-UPDATE water_polygons SET way_area=ST_Area(the_geom) WHERE the_geom IS NOT NULL;
-CREATE INDEX water_polygons_wayarea_index ON water_polygons(way_area);
 
 UPDATE land_polygons SET way_area=ST_Area(the_geom) WHERE the_geom IS NOT NULL;
 CREATE INDEX land_polygons_wayarea_index ON land_polygons(way_area);
