98cc13da679d6e400346c1c33cd5452a894c7b0b
Nathaniel Kelso <nvkelso@gmail.com>
2016-04-25 15:59:53 +0100

    restore failing tests

--- test/399-add-island-labels.py
+++ test/399-add-island-labels.py
@@ -30,15 +30,15 @@ assert_has_feature(
 
 # NODE archipelago labels (from place nodes)
 # http://www.openstreetmap.org/node/3178316462
-# assert_has_feature(
-#     15, 10776, 14921, 'earth',
-#     { 'kind': 'archipelago', 'label_placement': 'yes', 'min_zoom': 15, 'name': 'Les Saintes' })
+assert_has_feature(
+    15, 10776, 14921, 'earth',
+    { 'kind': 'archipelago', 'label_placement': 'yes', 'min_zoom': 15, 'name': 'Les Saintes' })
 
 # NODE archipelago labels (from place nodes)
 # http://www.openstreetmap.org/node/358955020
-# assert_has_feature(
-#     15, 9367, 12534, 'earth',
-#     { 'kind': 'archipelago', 'label_placement': 'yes', 'min_zoom': 15, 'name': 'Three Sisters Islands' })
+assert_has_feature(
+    15, 9367, 12534, 'earth',
+    { 'kind': 'archipelago', 'label_placement': 'yes', 'min_zoom': 15, 'name': 'Three Sisters Islands' })
 
 # LARGE archipelago labels (from place polygons)
 # There aren't any today
@@ -68,34 +68,30 @@ assert_has_feature(
 # NODE island labels (from place nodes)
 # http://www.openstreetmap.org/node/358761955
 # Bird Island, north of SF
-# assert_has_feature(
-#     15, 5230, 12659, 'earth',
-#     { 'kind': 'island', 'label_placement': 'yes', 'name': 'Bird Island' })
+assert_has_feature(
+    15, 5230, 12659, 'earth',
+    { 'kind': 'island', 'label_placement': 'yes', 'name': 'Bird Island' })
 
 # NODE island labels (from place nodes)
 # http://www.openstreetmap.org/node/358768646
 # Kent Island, north of SF
-# assert_has_feature(
-#     15, 5217, 12649, 'earth',
-#     { 'kind': 'island', 'label_placement': 'yes', 'name': 'Kent Island' })
+assert_has_feature(
+    15, 5217, 12649, 'earth',
+    { 'kind': 'island', 'label_placement': 'yes', 'name': 'Kent Island' })
 
 # LARGE island labels (from place polygons)
 # http://www.openstreetmap.org/relation/4227580
 # Manitoulin Island, Canada
-# We'd like to see this at zoom 7, but the join only includes OSM at 9+
-#    9, 139, 182, 'earth',
-# assert_has_feature(
-#     7, 34, 45, 'earth',
-#     { 'kind': 'island', 'label_placement': 'yes', 'name': 'Manitoulin Island' })
+assert_has_feature(
+    7, 34, 45, 'earth',
+    { 'kind': 'island', 'label_placement': 'yes', 'name': 'Manitoulin Island' })
     
 # LARGE island labels (from place polygons)
 # http://www.openstreetmap.org/relation/5176042
 # Trinidad, the island of the nation
-# We'd like to see this at zoom 7, but the join only includes OSM at 9+
-#    9, 168, 241, 'earth',
-# assert_has_feature(
-#     7, 42, 60, 'earth',
-#     { 'kind': 'island', 'label_placement': 'yes', 'name': 'Trinidad' })
+assert_has_feature(
+    7, 42, 60, 'earth',
+    { 'kind': 'island', 'label_placement': 'yes', 'name': 'Trinidad' })
 
 # MEDIUM island labels (from place polygons)
 # http://www.openstreetmap.org/way/124916662
@@ -159,9 +155,9 @@ assert_has_feature(
 # NODE islet labels (from place nodes)
 # http://www.openstreetmap.org/node/358795646
 # Pyramid Rock, SF
-# assert_has_feature(
-#     16, 10466, 25327, 'earth',
-#     { 'kind': 'islet', 'label_placement': 'yes', 'name': 'Pyramid Rock', 'min_zoom': 17 })
+assert_has_feature(
+    16, 10466, 25327, 'earth',
+    { 'kind': 'islet', 'label_placement': 'yes', 'name': 'Pyramid Rock', 'min_zoom': 17 })
 
 # LARGE islet labels (from place polygons)
 # http://www.openstreetmap.org/way/40500803
@@ -181,13 +177,13 @@ assert_has_feature(
 # SMALL islet labels (from place polygons)
 # http://www.openstreetmap.org/way/306344403
 # Sail Rock, near SF
-# assert_has_feature(
-#     16, 10467, 25395, 'earth',
-#     { 'kind': 'islet', 'label_placement': 'yes', 'name': 'Sail Rock', 'min_zoom': 17 })
+assert_has_feature(
+    16, 10467, 25395, 'earth',
+    { 'kind': 'islet', 'label_placement': 'yes', 'name': 'Sail Rock', 'min_zoom': 17 })
 
 # SMALL islet labels (from place polygons)
 # http://www.openstreetmap.org/way/32289183
 # Little Mile Rock, SF
-# assert_has_feature(
-#     16, 10465, 25326, 'earth',
-#     { 'kind': 'islet', 'label_placement': 'yes', 'name': 'Little Mile Rock', 'min_zoom': 17 })
+assert_has_feature(
+    16, 10465, 25326, 'earth',
+    { 'kind': 'islet', 'label_placement': 'yes', 'name': 'Little Mile Rock', 'min_zoom': 17 })
