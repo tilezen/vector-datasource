287dc6f7a7f0321fec8e35d1dc08f07a3b12f63b
Robert Marianski <rob@marianski.com>
2016-04-28 11:20:53 -0400

    Add piste test to catch dev issue

--- test/342-winter-sports-pistes.py
+++ test/342-winter-sports-pistes.py
@@ -13,3 +13,11 @@ assert_has_feature(
       'piste_type': 'downhill',
       'piste_difficulty': 'expert',
       'id': 313466720 })
+
+# Way: 49'er (313466490) http://www.openstreetmap.org/way/313466490
+assert_has_feature(
+    16, 10939, 25061, 'roads',
+    { 'kind': 'piste',
+      'piste_type': 'downhill',
+      'piste_difficulty': 'intermediate',
+      'id': 313466490 })
