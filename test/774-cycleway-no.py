80f256e9fdf9a82f832171d3848814c057a7a894
Robert Marianski <rob@marianski.com>
2016-05-02 14:20:59 -0400

    Drop cycleway:(left|right) = no as well

--- test/774-cycleway-no.py
+++ test/774-cycleway-no.py
@@ -2,3 +2,19 @@
 assert_no_matching_feature(
     16, 10484, 25327, 'roads',
     { 'cycleway': 'no' })
+
+# Way: Wedge Parkway (263563960) http://www.openstreetmap.org/way/263563960
+assert_no_matching_feature(
+    16, 10966, 24952, 'roads',
+    { 'cycleway_left': 'no' })
+assert_has_feature(
+    16, 10966, 24952, 'roads',
+    { 'cycleway_right': 'lane' })
+
+# Way: Wedge Parkway (263563950) http://www.openstreetmap.org/way/263563950
+assert_no_matching_feature(
+    16, 10965, 24952, 'roads',
+    { 'cycleway_right': 'no' })
+assert_has_feature(
+    16, 10965, 24952, 'roads',
+    { 'cycleway_left': 'lane' })
