ea4340087e93cb0cf61caf15cdef6fe98623708f
Matt Amos <zerebubuth@gmail.com>
2016-05-06 10:04:55 +0100

    Update state boundaries from NE to include statistical boundaries.

---
+++ test/797-add-missing-boundaries.py
@@ -0,0 +1,10 @@
+# NE data - no OSM elements
+# boundary between NV and CA is _also_ a "statistical" boundary
+assert_has_feature(
+    7, 21, 49, 'boundaries',
+    { 'kind': 'state' })
+
+# boundary between MT and ND is _also_ a "statistical meta" boundary
+assert_has_feature(
+    7, 21, 49, 'boundaries',
+    { 'kind': 'state' })
