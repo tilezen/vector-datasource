3bbfc62cb194c1c68ce24ffe9fa0732a0f00fd9c
Robert Marianski <rob@marianski.com>
2016-05-09 17:28:38 -0400

    Add sort_key assertion to raceway tests

--- test/664-raceway.py
+++ test/664-raceway.py
@@ -1,10 +1,10 @@
 # https://www.openstreetmap.org/way/28825404
 assert_has_feature(
     16, 10476, 25242, 'roads',
-    { 'id': 28825404, 'kind': 'minor_road', 'highway': 'raceway' })
+    { 'id': 28825404, 'kind': 'minor_road', 'highway': 'raceway', 'sort_key': 375 })
 
 # https://www.openstreetmap.org/way/59440900
 # Thunderoad Speedway Go-carts
 assert_has_feature(
     16, 10516, 25247, 'roads',
-    { 'id': 59440900, 'kind': 'minor_road', 'highway': 'raceway' })
+    { 'id': 59440900, 'kind': 'minor_road', 'highway': 'raceway', 'sort_key': 375 })
