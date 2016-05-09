32384741dae17caaea46b728b4ed268c4098ed18
Robert Marianski <rob@marianski.com>
2016-05-09 15:17:17 -0400

    Add highway=raceway to roads layer

---
+++ test/664-raceway.py
@@ -0,0 +1,9 @@
+# https://www.openstreetmap.org/way/28825404
+assert_has_feature(
+    16, 10476, 25242, 'roads',
+    { 'id': 28825404, 'kind': 'minor_road', 'highway': 'raceway' })
+
+# Thunderoad Speedway Go-carts https://www.openstreetmap.org/way/59440900
+assert_has_feature(
+    16, 10516, 25247, 'roads',
+    { 'id': 59440900, 'kind': 'minor_road', 'highway': 'raceway' })
