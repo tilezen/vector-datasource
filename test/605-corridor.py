49b51f3662495cfa8481496b1af82d50cca20d5d
Robert Marianski <rob@marianski.com>
2016-05-11 16:36:09 -0400

    Add corridors

---
+++ test/605-corridor.py
@@ -0,0 +1,5 @@
+# Way: The Nave (205644309)
+# http://www.openstreetmap.org/way/205644309
+assert_has_feature(
+    16, 10485, 25332, 'roads',
+    { 'id': 205644309, 'kind': 'path', 'highway': 'corridor' })
