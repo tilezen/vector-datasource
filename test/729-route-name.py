e6309d2d63e57bdaec0088c0a9102fe42f41e68a
Robert Marianski <rob@marianski.com>
2016-05-16 11:07:53 -0400

    Add test for updated route_name transform

---
+++ test/729-route-name.py
@@ -0,0 +1,7 @@
+# Relation: M-Ocean View: Inbound to Downtown (91022)
+# http://www.openstreetmap.org/relation/91022
+assert_has_feature(
+    16, 10486, 25326, 'transit',
+    { 'id': -91022, 'kind': 'light_rail', 'osm_relation': True,
+      'name': 'M-Ocean View: Inbound to Downtown',
+      'route_name': type(None) })
