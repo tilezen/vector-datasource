8d1b806e6b28bcb80bed64d1449716a218008529
Robert Marianski <rob@marianski.com>
2016-05-16 14:49:29 -0400

    Add l10n test for Hollywood neighbourhood

---
+++ test/418-wof-l10n_name.py
@@ -0,0 +1,8 @@
+# Hollywood (wof neighbourhood)
+# https://whosonfirst.mapzen.com/data/858/260/37/85826037.geojson
+assert_has_feature(
+    16, 11227, 26157, 'places',
+    { 'id': 85826037, 'kind': 'neighbourhood',
+      'source': "whosonfirst.mapzen.com",
+      'name': 'Hollywood',
+      'name:ko': '\xed\x97\x90\xeb\xa6\xac\xec\x9a\xb0\xeb\x93\x9c' })
