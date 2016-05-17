2b81c1a424705c0ec942aa66336eb0b0ad2a80a9
Robert Marianski <rob@marianski.com>
2016-05-17 15:45:40 -0400

    Add highway=rest_area|services to pois and landuse

---
+++ test/480-rest_area-services.py
@@ -0,0 +1,22 @@
+# Node: Pilot Travel Center #237 (1145095463)
+# http://www.openstreetmap.org/node/1145095463
+assert_has_feature(
+    16, 10625, 25584, 'pois',
+    { 'kind': 'service_area', 'id': 1145095463, 'min_zoom': 11 })
+
+# http://www.openstreetmap.org/node/3202314563
+assert_has_feature(
+    16, 10554, 25508, 'pois',
+    { 'kind': 'rest_area', 'id': 3202314563, 'min_zoom': 11 })
+
+# Way: Crystal Springs Rest Area (97057565)
+# http://www.openstreetmap.org/way/97057565
+assert_has_feature(
+    16, 10492, 25385, 'landuse',
+    { 'kind': 'rest_area', 'id': 97057565, 'sort_key': 41 })
+
+# Way: Nicole Driveway (274732386)
+# http://www.openstreetmap.org/way/274732386
+assert_has_feature(
+    16, 10900, 25256, 'landuse',
+    { 'kind': 'service_area', 'id': 274732386, 'sort_key': 42 })
