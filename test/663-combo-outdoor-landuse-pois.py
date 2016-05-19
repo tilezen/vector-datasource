07772791f01a316f80f5d208c92750fac2ee2314
Matt Amos <zerebubuth@gmail.com>
2016-05-19 20:29:32 +0100

    Dog park was updated and turned into an area! Test needed updating to match.

--- test/663-combo-outdoor-landuse-pois.py
+++ test/663-combo-outdoor-landuse-pois.py
@@ -173,11 +173,11 @@ assert_has_feature(
 
 
 
-#https://www.openstreetmap.org/node/3646717009
+#https://www.openstreetmap.org/way/417184097
 # Dog Run at Upper Noe Valley Rec Center, SF
 assert_has_feature(
     16, 10480, 25338, 'pois',
-    { 'kind': 'dog_park', 'min_zoom': 17 })
+    { 'kind': 'dog_park', 'min_zoom': 16 })
 
 #https://www.openstreetmap.org/way/375333476
 # Dog park at Walter Hass Playground, SF
