75e86f349bbf3c0bad7d84dea05ab3b2519423d8
Robert Marianski <rob@marianski.com>
2016-05-20 11:05:28 -0400

    Add tests to exercise lookup and country suffix

--- test/418-wof-l10n_name.py
+++ test/418-wof-l10n_name.py
@@ -24,3 +24,19 @@ assert_has_feature(
       'source': "openstreetmap.org",
       'name': 'San Francisco',
       'name:zho': '\xe8\x88\x8a\xe9\x87\x91\xe5\xb1\xb1\xe5\xb8\x82\xe8\x88\x87\xe7\xb8\xa3' })
+
+# Node: Londonderry/Derry (267762522)
+# http://www.openstreetmap.org/node/267762522
+assert_has_feature(
+    16, 31436, 20731, 'places',
+    { 'id': 267762522, 'name:eng_GB': 'Londonderry'})
+
+# Node: Jerusalem (29090735)
+# http://www.openstreetmap.org/node/29090735
+assert_has_feature(
+    16, 39180, 26661, 'places',
+    { 'id': 29090735,
+      'name:nan': 'I\xc3\xa2-l\xc5\x8d\xcd\x98-sat-l\xc3\xa9ng',
+      'name:zho': '\xe8\x80\xb6\xe8\xb7\xaf\xe6\x92\x92\xe5\x86\xb7',
+      'name:yue': '\xe8\x80\xb6\xe8\xb7\xaf\xe6\x92\x92\xe5\x86\xb7',
+      })
