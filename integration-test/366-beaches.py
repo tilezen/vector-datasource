6cda221478d3f67bc10ed13eb57854b493f6dbe2
Matt Amos <zerebubuth@gmail.com>
2016-05-31 12:49:00 +0100

    Update test - Baker beach was turned into a relation.

--- integration-test/366-beaches.py
+++ integration-test/366-beaches.py
@@ -1,5 +1,5 @@
 # Baker beach, SF
-# https://www.openstreetmap.org/way/195638009
+# https://www.openstreetmap.org/relation/6260732
 assert_has_feature(
     18, 41881, 101308, 'landuse',
     { 'kind': 'beach' })
