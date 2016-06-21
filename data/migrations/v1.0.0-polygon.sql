8eb3adeefb7b441552d31abe9858f8024d943c48
Robert Marianski <rob@marianski.com>
2016-06-21 11:44:11 -0400

    Exclude location=underground buildings at z13

--- data/migrations/v1.0.0-polygon.sql
+++ data/migrations/v1.0.0-polygon.sql
@@ -12,3 +12,10 @@ UPDATE
   WHERE
     highway IN  ('services', 'rest_area')
     AND COALESCE(mz_landuse_min_zoom, 999) <> COALESCE(mz_calculate_min_zoom_landuse(planet_osm_polygon.*), 999);
+
+UPDATE
+  planet_osm_polygon
+  SET mz_building_min_zoom = mz_calculate_min_zoom_buildings(planet_osm_polygon.*)
+  WHERE
+    tags->'location' = 'underground'
+    AND COALESCE(mz_building_min_zoom, 999) <> COALESCE(mz_calculate_min_zoom_buildings(planet_osm_polygon.*), 999);
