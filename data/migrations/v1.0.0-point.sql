e12812e7b9030ac63faeca37cf326af9e368e185
Robert Marianski <rob@marianski.com>
2016-07-07 14:21:56 -0400

    Remove country labels from zooms 0 and 1

--- data/migrations/v1.0.0-point.sql
+++ data/migrations/v1.0.0-point.sql
@@ -5,3 +5,9 @@ UPDATE
     (barrier = 'toll_booth' OR
      highway IN ('services', 'rest_area'))
     AND COALESCE(mz_poi_min_zoom, 999) <> COALESCE(mz_calculate_min_zoom_pois(planet_osm_point.*), 999);
+
+UPDATE
+  planet_osm_point
+  SET mz_places_min_zoom = mz_calculate_min_zoom_places(planet_osm_point.*)
+  WHERE
+    place = 'country' AND "name" IS NOT NULL;
---
+++ integration-test/837-no-country-label-low-zoom.py
@@ -0,0 +1,9 @@
+# United States
+# http://www.openstreetmap.org/node/424317935
+assert_no_matching_feature(
+    0, 0, 0, 'places', {'kind': 'country'})
+
+# United States
+# http://www.openstreetmap.org/node/424317935
+assert_no_matching_feature(
+    1, 0, 0, 'places', {'kind': 'country'})
