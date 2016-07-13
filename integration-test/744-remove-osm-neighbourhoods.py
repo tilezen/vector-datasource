6418fcf8a0b0dc3830deca3a279471135b47fe5f
Robert Marianski <rob@marianski.com>
2016-07-13 16:23:04 -0400

    Remove osm neighbourhoods

---
+++ integration-test/744-remove-osm-neighbourhoods.py
@@ -0,0 +1,17 @@
+# Node: Mount Pocono (158473043)
+# http://www.openstreetmap.org/node/158473043
+assert_no_matching_feature(
+    16, 19048, 24541, 'places',
+    { 'kind': 'borough', 'source': 'openstreetmap.org' })
+
+# Node: Centerville District (150939391)
+# http://www.openstreetmap.org/node/150939391
+assert_no_matching_feature(
+    16, 10558, 25381, 'places',
+    { 'kind': 'suburb', 'source': 'openstreetmap.org' })
+
+# Node: Northeast (2790349074)
+# http://www.openstreetmap.org/node/2790349074
+assert_no_matching_feature(
+    16, 18754, 25065, 'places',
+    { 'kind': 'quarter', 'source': 'openstreetmap.org' })
