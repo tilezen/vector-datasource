d59559d39e37e58146bae257d214b8affc9109c1
Olga Kavvada <okavvada@gmail.com>
2016-07-18 15:38:42 -0700

    update polygon migrations

--- data/migrations/v1.0.0-polygon.sql
+++ data/migrations/v1.0.0-polygon.sql
@@ -13,7 +13,8 @@ UPDATE
   SET mz_poi_min_zoom = mz_calculate_min_zoom_pois(planet_osm_polygon.*)
   WHERE
     (barrier = 'toll_booth' OR
-     highway IN ('services', 'rest_area'))
+     highway IN ('services', 'rest_area') OR
+     tourism = 'camp_site')
     AND COALESCE(mz_poi_min_zoom, 999) <> COALESCE(mz_calculate_min_zoom_pois(planet_osm_polygon.*), 999);
 
 UPDATE
@@ -25,6 +26,7 @@ UPDATE
      historic = 'citywalls' OR
      man_made = 'snow_fence' OR
      waterway = 'dam' OR
+     tourism = 'camp_site' OR
      "natural" IN ('forest', 'park'))
     AND COALESCE(mz_landuse_min_zoom, 999) <> COALESCE(mz_calculate_min_zoom_landuse(planet_osm_polygon.*), 999);
 
@@ -34,3 +36,4 @@ UPDATE
   WHERE
     tags->'location' = 'underground'
     AND COALESCE(mz_building_min_zoom, 999) <> COALESCE(mz_calculate_min_zoom_buildings(planet_osm_polygon.*), 999);
+    
