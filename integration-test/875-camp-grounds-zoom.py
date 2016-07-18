8162d558b3f982e04ae68d19a9204505685f8f04
Olga Kavvada <okavvada@gmail.com>
2016-07-18 14:41:34 -0700

    added test for camp grounds in landuse at appropriate zoom

---
+++ integration-test/875-camp-grounds-zoom.py
@@ -0,0 +1,44 @@
+# update landuse to include campground features and fix label zoom
+
+# large campground in landuse zoom 16
+# http://www.openstreetmap.org/way/431725967
+assert_has_feature(
+    16, 10959, 25337, 'landuse',
+    { 'kind': 'camp_site'})
+
+# large campground in point zoom 16
+assert_has_feature(
+    16, 10959, 25337, 'pois',
+    { 'kind': 'camp_site'})
+
+# large campground in landuse zoom 13
+
+assert_has_feature(
+    13, 1369, 3167, 'landuse',
+    { 'kind': 'camp_site'})
+
+# large campground in point zoom 13
+assert_has_feature(
+    13, 1369, 3167, 'pois',
+    { 'kind': 'camp_site'})
+
+# small campground in landuse zoom 16
+# http://www.openstreetmap.org/way/417405356
+assert_has_feature(
+    16, 10471, 25326, 'landuse',
+    { 'kind': 'camp_site'})
+
+# small campground in point zoom 16
+assert_has_feature(
+    16, 10471, 25326, 'pois',
+    { 'kind': 'camp_site'})
+
+# small campground in landuse zoom 13
+assert_has_feature(
+    13, 1308, 3165, 'landuse',
+    { 'kind': 'camp_site'})
+
+# small campground in point zoom 13
+assert_no_matching_feature(
+    13, 1308, 3165, 'pois',
+    { 'kind': 'camp_site'})
