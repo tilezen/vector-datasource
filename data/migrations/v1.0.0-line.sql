7dea720e5994378557f53773c08cd2c8f5fb9b5a
Robert Marianski <rob@marianski.com>
2016-07-18 11:13:31 -0400

    Normalize boundaries kind

--- data/migrations/v1.0.0-line.sql
+++ data/migrations/v1.0.0-line.sql
@@ -11,7 +11,9 @@ UPDATE
     (barrier IN ('city_wall', 'retaining_wall', 'fence') OR
      historic = 'citywalls' OR
      man_made = 'snow_fence' OR
-     waterway = 'dam')
+     waterway = 'dam' OR
+     boundary = 'aboriginal_lands' OR
+     tags->'boundary:type' = 'aboriginal_lands')
     AND COALESCE(mz_boundary_min_zoom, 999) <> COALESCE(mz_calculate_min_zoom_boundaries(planet_osm_line.*), 999);
 
 UPDATE
--- integration-test/797-add-missing-boundaries.py
+++ integration-test/797-add-missing-boundaries.py
@@ -2,9 +2,9 @@
 # boundary between NV and CA is _also_ a "statistical" boundary
 assert_has_feature(
     7, 21, 49, 'boundaries',
-    { 'kind': 'state' })
+    { 'kind': 'region' })
 
 # boundary between MT and ND is _also_ a "statistical meta" boundary
 assert_has_feature(
     7, 27, 44, 'boundaries',
-    { 'kind': 'state' })
+    { 'kind': 'region' })
