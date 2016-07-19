dc742e5a345113d2bb84400d0d61f78068d905ba
Robert Marianski <rob@marianski.com>
2016-07-19 16:02:18 -0400

    Update aeroway roads kind

---
+++ integration-test/895-extract-airport-lines-minor_road.py
@@ -0,0 +1,11 @@
+# Way: 10L/28R (22567191)
+# http://www.openstreetmap.org/way/22567191
+assert_has_feature(
+    16, 10490, 25366, 'roads',
+    {'id': 22567191, 'kind': 'aeroway', 'kind_detail': 'runway'})
+
+# Way: Q (23718500)
+# http://www.openstreetmap.org/way/23718500
+assert_has_feature(
+    16, 10488, 25365, 'roads',
+    {'id': 23718500, 'kind': 'aeroway', 'kind_detail': 'taxiway'})
