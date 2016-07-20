fa2c31427e26d231d2db8a5573a6ec87c6f68e4a
Olga Kavvada <okavvada@gmail.com>
2016-07-20 11:27:56 -0700

    connects to #860 updated point migration

--- data/migrations/v1.0.0-point.sql
+++ data/migrations/v1.0.0-point.sql
@@ -4,7 +4,8 @@ UPDATE
   WHERE
     (barrier = 'toll_booth' OR
      highway IN ('services', 'rest_area') OR
-     tourism = 'camp_site')
+     tourism = 'camp_site' OR
+     man_made = 'lighthouse')
     AND COALESCE(mz_poi_min_zoom, 999) <> COALESCE(mz_calculate_min_zoom_pois(planet_osm_point.*), 999);
 
 UPDATE
