11dbd5398b7b1e0d170a620ee84d4ea4092703cd
Olga Kavvada <okavvada@gmail.com>
2016-07-20 09:36:37 -0700

    updated migrations

--- data/migrations/v1.0.0-point.sql
+++ data/migrations/v1.0.0-point.sql
@@ -2,9 +2,12 @@ UPDATE
   planet_osm_point
   SET mz_poi_min_zoom = mz_calculate_min_zoom_pois(planet_osm_point.*)
   WHERE
-    (barrier = 'toll_booth' OR
-     highway IN ('services', 'rest_area') OR
-     tourism = 'camp_site')
+    (
+     barrier = 'toll_booth'
+     OR highway IN ('services', 'rest_area')
+     OR tourism = 'camp_site'
+     OR man_made = 'windmill'
+    )
     AND COALESCE(mz_poi_min_zoom, 999) <> COALESCE(mz_calculate_min_zoom_pois(planet_osm_point.*), 999);
 
 UPDATE
