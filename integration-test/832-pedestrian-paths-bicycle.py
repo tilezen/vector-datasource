85b883373d8da2c15275130eaed1f662760857b6
Olga Kavvada <okavvada@gmail.com>
2016-07-21 15:54:14 -0700

    connects to #832 created test

---
+++ integration-test/832-pedestrian-paths-bicycle.py
@@ -0,0 +1,6 @@
+# Add footway properties to pedestrian paths
+
+# http://www.openstreetmap.org/way/8919991
+assert_has_feature(
+    13, 1309, 3165, 'roads',
+    { 'id': 8919991, 'kind': 'path', 'bicycle': 'designated'})
