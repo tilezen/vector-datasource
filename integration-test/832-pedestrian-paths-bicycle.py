e4c1d13ef623b7d4bc59d430cff2b34c65fa6ed2
Olga Kavvada <okavvada@gmail.com>
2016-07-21 16:39:04 -0700

    added pier to test

--- integration-test/832-pedestrian-paths-bicycle.py
+++ integration-test/832-pedestrian-paths-bicycle.py
@@ -1,6 +1,13 @@
-# Add footway properties to pedestrian paths
+# Add footway properties to pedestrian paths and piers
 
+# Pedestrian path
 # http://www.openstreetmap.org/way/8919991
 assert_has_feature(
     13, 1309, 3165, 'roads',
     { 'id': 8919991, 'kind': 'path', 'bicycle': 'designated'})
+
+# Pier
+# http://www.openstreetmap.org/way/133920164
+assert_has_feature(
+    13, 1304, 2933, 'roads',
+    { 'id': 133920164, 'kind': 'path', 'bicycle': 'yes'})
