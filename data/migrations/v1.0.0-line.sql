60ae364a3661848a51e23851fab3e71ab59ddd73
Olga Kavvada <okavvada@gmail.com>
2016-07-21 12:24:57 -0700

    updated line migrations

--- data/migrations/v1.0.0-line.sql
+++ data/migrations/v1.0.0-line.sql
@@ -1,7 +1,7 @@
 UPDATE planet_osm_line
   SET mz_road_level = mz_calculate_min_zoom_roads(planet_osm_line.*)
   WHERE
-    highway IN ('raceway', 'corridor') AND
+    highway IN ('raceway', 'corridor', 'bridleway') AND
     COALESCE(mz_road_level, 999) <> COALESCE(mz_calculate_min_zoom_roads(planet_osm_line.*), 999);
 
 UPDATE
