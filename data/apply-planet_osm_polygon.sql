5a82e99f658837a897cbeb34a9013d0289584dc0
Robert Marianski <rob@marianski.com>
2016-07-21 15:38:59 -0400

    Normalize new indexes created

--- data/apply-planet_osm_polygon.sql
+++ data/apply-planet_osm_polygon.sql
@@ -5,12 +5,6 @@ BEGIN
 -- planet_osm_polygon
 --------------------------------------------------------------------------------
 
--- indexes on existing columns
-CREATE INDEX planet_osm_polygon_wayarea_index ON planet_osm_polygon(way_area);
-CREATE INDEX planet_osm_polygon_building_index ON planet_osm_polygon(building) WHERE building IS NOT NULL;
-CREATE INDEX planet_osm_polygon_is_building_or_part_index ON planet_osm_polygon(mz_calculate_is_building_or_part(building, "building:part")) WHERE mz_calculate_is_building_or_part(building, "building:part") = TRUE;
-
-
 UPDATE planet_osm_polygon SET
     mz_landuse_min_zoom = mz_calculate_min_zoom_landuse(planet_osm_polygon.*)
     WHERE mz_calculate_min_zoom_landuse(planet_osm_polygon.*) IS NOT NULL;
@@ -41,11 +35,10 @@ UPDATE planet_osm_polygon
   WHERE mz_calculate_min_zoom_buildings(planet_osm_polygon.*) IS NOT NULL;
 
 
+CREATE INDEX planet_osm_polygon_landuse_geom_index ON planet_osm_polygon USING gist(way) WHERE mz_landuse_min_zoom IS NOT NULL;
 CREATE INDEX planet_osm_polygon_landuse_geom_9_index ON planet_osm_polygon USING gist(way) WHERE mz_landuse_min_zoom <= 9;
 CREATE INDEX planet_osm_polygon_landuse_geom_12_index ON planet_osm_polygon USING gist(way) WHERE mz_landuse_min_zoom <= 12;
 CREATE INDEX planet_osm_polygon_landuse_geom_15_index ON planet_osm_polygon USING gist(way) WHERE mz_landuse_min_zoom <= 15;
-
-CREATE INDEX planet_osm_polygon_railway_platform_index ON planet_osm_polygon USING gist(way) WHERE railway='platform';
 
 CREATE INDEX planet_osm_polygon_transit_geom_index ON planet_osm_polygon USING gist(way) WHERE mz_transit_level IS NOT NULL;
 CREATE INDEX planet_osm_polygon_transit_geom_6_index ON planet_osm_polygon USING gist(way) WHERE mz_transit_level <= 6;
@@ -53,18 +46,22 @@ CREATE INDEX planet_osm_polygon_transit_geom_9_index ON planet_osm_polygon USING
 CREATE INDEX planet_osm_polygon_transit_geom_12_index ON planet_osm_polygon USING gist(way) WHERE mz_transit_level <= 12;
 CREATE INDEX planet_osm_polygon_transit_geom_15_index ON planet_osm_polygon USING gist(way) WHERE mz_transit_level <= 15;
 
+CREATE INDEX planet_osm_polygon_earth_geom_index ON planet_osm_polygon USING gist(way) WHERE mz_earth_min_zoom IS NOT NULL;
 CREATE INDEX planet_osm_polygon_earth_geom_9_index  ON planet_osm_polygon USING gist(way) WHERE mz_earth_min_zoom <= 9;
 CREATE INDEX planet_osm_polygon_earth_geom_12_index ON planet_osm_polygon USING gist(way) WHERE mz_earth_min_zoom <= 12;
 CREATE INDEX planet_osm_polygon_earth_geom_15_index ON planet_osm_polygon USING gist(way) WHERE mz_earth_min_zoom <= 15;
 
+CREATE INDEX planet_osm_polygon_water_geom_index ON planet_osm_polygon USING gist(way) WHERE mz_water_min_zoom IS NOT NULL;
 CREATE INDEX planet_osm_polygon_water_geom_9_index ON planet_osm_polygon USING gist(way) WHERE mz_water_min_zoom <= 9;
 CREATE INDEX planet_osm_polygon_water_geom_12_index ON planet_osm_polygon USING gist(way) WHERE mz_water_min_zoom <= 12;
 CREATE INDEX planet_osm_polygon_water_geom_15_index ON planet_osm_polygon USING gist(way) WHERE mz_water_min_zoom <= 15;
 
+CREATE INDEX planet_osm_polygon_boundary_geom_index ON planet_osm_polygon USING gist(way) WHERE mz_boundary_min_zoom IS NOT NULL;
 CREATE INDEX planet_osm_polygon_boundary_geom_9_index ON planet_osm_polygon USING gist(way) WHERE mz_boundary_min_zoom <= 9;
 CREATE INDEX planet_osm_polygon_boundary_geom_12_index ON planet_osm_polygon USING gist(way) WHERE mz_boundary_min_zoom <= 12;
 CREATE INDEX planet_osm_polygon_boundary_geom_15_index ON planet_osm_polygon USING gist(way) WHERE mz_boundary_min_zoom <= 15;
 
+CREATE INDEX planet_osm_polygon_building_geom_index ON planet_osm_polygon USING gist(way) WHERE mz_building_min_zoom IS NOT NULL;
 CREATE INDEX planet_osm_polygon_building_geom_15_index ON planet_osm_polygon USING gist(way) WHERE mz_building_min_zoom <= 15;
 
 END $$;
