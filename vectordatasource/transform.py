5c5a55dc05120a4abbe08f3e7fb00cb661d38dcb
Olga Kavvada <okavvada@gmail.com>
2016-07-22 11:25:37 -0700

    added is_bicycle_related

--- vectordatasource/transform.py
+++ vectordatasource/transform.py
@@ -3473,6 +3473,8 @@ def add_is_bicycle_related(shape, props, fid, zoom):
             'cycleway' in props or
             'cycleway_left' in props or
             'cycleway_right' in props or
+            props.get('bicycle') == 'yes' or
+            props.get('bicycle') == 'designated' or
             props.get('highway') == 'cycleway'):
         props['is_bicycle_related'] = True
     return shape, props, fid
