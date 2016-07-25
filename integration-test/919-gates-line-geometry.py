8ebf38d698138babd09b5c441729aa4df1ecdbaf
Olga Kavvada <okavvada@gmail.com>
2016-07-25 16:39:49 -0700

    connects to #919 integration test

---
+++ integration-test/919-gates-line-geometry.py
@@ -0,0 +1,7 @@
+# Add barrier:gates with line geometries in landuse
+
+# Line barrier:ghate feature
+# http://www.openstreetmap.org/way/391260223
+assert_has_feature(
+    16, 10482, 25335, 'landuse',
+    { 'id': 391260223, 'kind': 'gate'})
