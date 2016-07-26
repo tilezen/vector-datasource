5c1a87703c79ff529bd4a437a65f9916f35685fd
Olga Kavvada <okavvada@gmail.com>
2016-07-25 18:15:22 -0700

    update point migrations

--- data/migrations/v1.0.0-point.sql
+++ data/migrations/v1.0.0-point.sql
@@ -7,7 +7,8 @@ UPDATE
      tourism = 'camp_site' OR
      man_made = 'lighthouse' OR
      man_made = 'windmill' OR
-     leisure = 'garden')
+     leisure = 'garden' OR
+     barrier = 'gate')
     AND COALESCE(mz_poi_min_zoom, 999) <> COALESCE(mz_calculate_min_zoom_pois(planet_osm_point.*), 999);
 
 UPDATE
