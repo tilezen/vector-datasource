46369e604f2867fd2ec0b77fae6b6bb9a9f9e24f
Olga Kavvada <okavvada@gmail.com>
2016-07-26 14:11:36 -0700

    reverted back to pois

--- data/migrations/v1.0.0-point.sql
+++ data/migrations/v1.0.0-point.sql
@@ -2,12 +2,11 @@ UPDATE
   planet_osm_point
   SET mz_poi_min_zoom = mz_calculate_min_zoom_pois(planet_osm_point.*)
   WHERE
-    (barrier = 'toll_booth' OR
+    (barrier IN ('toll_booth', 'gate') OR
      highway IN ('services', 'rest_area') OR
      tourism = 'camp_site' OR
      man_made IN ('lighthouse', 'windmill') OR
-     leisure = 'garden' OR
-     railway IN ('halt', 'stop', 'tram_stop')
+     leisure = 'garden')
     AND COALESCE(mz_poi_min_zoom, 999) <> COALESCE(mz_calculate_min_zoom_pois(planet_osm_point.*), 999);
 
 UPDATE
