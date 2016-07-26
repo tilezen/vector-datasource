5e49873c8fcac15e1f742f2b47af50787d178560
Olga Kavvada <okavvada@gmail.com>
2016-07-26 11:01:23 -0700

    updated migration to include barriers ad man_made in one line

--- data/migrations/v1.0.0-point.sql
+++ data/migrations/v1.0.0-point.sql
@@ -5,8 +5,7 @@ UPDATE
     (barrier IN ('toll_booth', 'gate') OR
      highway IN ('services', 'rest_area') OR
      tourism = 'camp_site' OR
-     man_made = 'lighthouse' OR
-     man_made = 'windmill' OR
+     man_made IN ('lighthouse', 'windmill') OR
      leisure = 'garden')
     AND COALESCE(mz_poi_min_zoom, 999) <> COALESCE(mz_calculate_min_zoom_pois(planet_osm_point.*), 999);
 
