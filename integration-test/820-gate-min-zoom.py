9fa8c7ba18eb7836baba7ca1df810eda739d7c30
Olga Kavvada <okavvada@gmail.com>
2016-07-26 08:39:57 -0700

    test integration test

--- integration-test/820-gate-min-zoom.py
+++ integration-test/820-gate-min-zoom.py
@@ -2,15 +2,15 @@
 
 # Gate on secondary road
 # https://www.openstreetmap.org/node/2784141829
-assert_has_feature(
-    15, 9237, 12635, 'pois',
-    {'id': 2784141829, 'kind': 'gate'})
+#assert_has_feature(
+    #15, 9237, 12635, 'pois',
+    #{'id': 2784141829, 'kind': 'gate', 'min_zoom': 15})
 
 # Gate on minor road
 # http://www.openstreetmap.org/node/2591034891
 assert_has_feature(
     16, 19244, 24628, 'pois',
-    {'id': 2591034891, 'kind': 'gate'})
+    {'id': 2591034891, 'kind': 'gate', 'min_zoom': 16})
 
 assert_no_matching_feature(
     15, 9622, 12314, 'pois',
@@ -20,7 +20,7 @@ assert_no_matching_feature(
 # http://www.openstreetmap.org/node/276321344
 assert_has_feature(
     16, 10549, 25415, 'pois',
-    {'id': 276321344, 'kind': 'gate'})
+    {'id': 276321344, 'kind': 'gate', 'min_zoom': 16})
 
 assert_no_matching_feature(
     15, 5274, 12707, 'pois',
@@ -30,7 +30,7 @@ assert_no_matching_feature(
 # http://www.openstreetmap.org/node/302482019
 assert_has_feature(
     16, 10466, 25328, 'pois',
-    {'id': 302482019, 'kind': 'gate'})
+    {'id': 302482019, 'kind': 'gate', 'min_zoom': 16})
 
 assert_no_matching_feature(
     15, 5233, 12664, 'pois',
