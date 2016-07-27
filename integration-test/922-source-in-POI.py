24c64604b6832145ae6dd44094f9ff4864aa7daf
Olga Kavvada <okavvada@gmail.com>
2016-07-27 14:52:45 -0700

    test if POI has source

---
+++ integration-test/922-source-in-POI.py
@@ -0,0 +1,6 @@
+# Add source info in POIs
+
+# https://www.openstreetmap.org/way/423023928
+assert_has_feature(
+    14, 2615, 6330, 'pois',
+    {'id': 423023928, 'kind': 'lighthouse', 'source':'openstreetmap.org'})
