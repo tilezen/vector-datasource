45c13f7774ab4551ae9cd08561ac667479654107
Olga Kavvada <okavvada@gmail.com>
2016-08-02 13:59:52 -0700

    migration landuse for graveyard

--- data/migrations/v1.0.0-polygon.sql
+++ data/migrations/v1.0.0-polygon.sql
@@ -17,9 +17,13 @@ UPDATE
     (barrier = 'toll_booth' OR
      highway IN ('services', 'rest_area') OR
      tourism = 'camp_site' OR
-     man_made = 'windmill' OR
-     man_made = 'lighthouse' OR
-     leisure = 'garden')
+     man_made IN ('windmill', 'lighthouse', 'wastewater_plant', 'water_works', 'works') OR
+     leisure IN ('garden', 'golf_course', 'nature_reserve', 'park', 'pitch') OR
+     amenity = 'grave_yard' OR
+     landuse IN ('cemetery', 'farm', 'forest', 'military', 'quarry', 'recreation_ground', 'village_green', 'winter_sports') OR
+     boundary IN ('national_park', 'protected_area') OR
+     power IN ('plant', 'substation') OR
+     "natural" IN ('wood'))
     AND COALESCE(mz_poi_min_zoom, 999) <> COALESCE(mz_calculate_min_zoom_pois(planet_osm_polygon.*), 999);
 
 UPDATE
@@ -29,11 +33,15 @@ UPDATE
     (highway IN  ('services', 'rest_area') OR
      barrier IN ('city_wall', 'retaining_wall', 'fence') OR
      historic = 'citywalls' OR
-     man_made = 'snow_fence' OR
+     man_made IN ('snow_fence', 'wastewater_plant', 'water_works', 'works') OR
      waterway = 'dam' OR
      tourism = 'camp_site' OR
-     leisure = 'garden' OR
-     "natural" IN ('forest', 'park'))
+     leisure IN ('garden', 'golf_course', 'nature_reserve', 'park', 'pitch') OR
+     "natural" IN ('forest', 'park', 'wood') OR
+     amenity = 'grave_yard' OR
+     landuse IN ('cemetery', 'farm', 'forest', 'military', 'quarry', 'recreation_ground', 'village_green', 'winter_sports') OR
+     boundary IN ('national_park', 'protected_area') OR
+     power IN ('plant', 'substation'))
     AND COALESCE(mz_landuse_min_zoom, 999) <> COALESCE(mz_calculate_min_zoom_landuse(planet_osm_polygon.*), 999);
 
 UPDATE
