595410e5f41167703d7a2ef8fef6591d7f7ef27f
Olga Kavvada <okavvada@gmail.com>
2016-08-04 17:41:23 -0700

    id = int

--- integration-test/931-locality-changes-places.py
+++ integration-test/931-locality-changes-places.py
@@ -1,39 +1,39 @@
 # ne state capitals
 assert_has_feature(
     9, 83, 196, 'places',
-    { 'id': 6809, 'kind': 'locality'})
+    { 'id': int, 'kind': 'locality'})
 
 assert_no_matching_feature(
     9, 83, 196, 'places',
-    { 'id': 6809, 'kind': 'city'})
+    { 'id': int, 'kind': 'city'})
 
 assert_has_feature(
     9, 83, 196, 'places',
-    { 'id': 6809, 'name': 'Sacramento', 'region_capital': True})
+    { 'id': int, 'name': 'Sacramento', 'region_capital': True})
 
 assert_no_matching_feature(
     9, 83, 196, 'places',
-    { 'id': 6809, 'name': 'Sacramento', 'state_capital': True})
+    { 'id': int, 'name': 'Sacramento', 'state_capital': True})
 
 # ne country capitals
 assert_has_feature(
     9, 289, 197, 'places',
-    { 'id': 7285, 'kind': 'locality'})
+    { 'id': int, 'kind': 'locality'})
 
 assert_no_matching_feature(
     9, 289, 197, 'places',
-    { 'id': 7285, 'kind': 'city'})
+    { 'id': int, 'kind': 'city'})
 
 # ne populated place
 assert_has_feature(
     8, 41, 98, 'places',
-    { 'id': 609, 'kind': 'locality'})
+    { 'id': int, 'kind': 'locality'})
 
 assert_no_matching_feature(
     8, 41, 98, 'places',
-    { 'id': 609, 'kind': 'city'})
+    { 'id': int, 'kind': 'city'})
 
 # ne Scientific station
 assert_has_feature(
     7, 41, 94, 'places',
-    { 'id': 4838, 'kind': 'locality', 'kind_detail': 'scientific_station'})
+    { 'id': int, 'kind': 'locality', 'kind_detail': 'scientific_station'})
