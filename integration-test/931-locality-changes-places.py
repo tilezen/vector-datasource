500b6dbe6153a364e1aab609089e862e8fa7a1f7
Olga Kavvada <okavvada@gmail.com>
2016-08-05 13:27:59 -0700

    take ids out

--- integration-test/931-locality-changes-places.py
+++ integration-test/931-locality-changes-places.py
@@ -1,31 +1,31 @@
 # ne Admin-0 capital
 assert_has_feature(
     3, 6, 3, 'places',
-    { 'id': 7242, 'kind': 'locality', 'name': 'Seoul', 'capital': True})
+    { 'kind': 'locality', 'name': 'Seoul', 'capital': True})
 
 assert_no_matching_feature(
     3, 6, 3, 'places',
-    { 'id': 7242, 'kind': 'city', 'name': 'Seoul', 'capital': True})
+    { 'kind': 'city', 'name': 'Seoul', 'capital': True})
 
 # ne Admin-1 capital
 assert_has_feature(
     3, 7, 4, 'places',
-    { 'id': 7287, 'kind': 'locality', 'name': 'Sydney', 'region_capital': True})
+    { 'kind': 'locality', 'name': 'Sydney', 'region_capital': True})
 
 assert_no_matching_feature(
     3, 7, 4, 'places',
-    { 'id': 7287, 'kind': 'city', 'name': 'Sydney', 'region_capital': True})
+    { 'kind': 'city', 'name': 'Sydney', 'region_capital': True})
 
 # ne Populated place
 assert_has_feature(
     3, 1, 3, 'places',
-    { 'id': 7223, 'kind': 'locality', 'name': 'San Francisco'})
+    { 'kind': 'locality', 'name': 'San Francisco'})
 
 assert_no_matching_feature(
     3, 1, 3, 'places',
-    { 'id': 7223, 'kind': 'city', 'name': 'San Francisco'})
+    { 'kind': 'city', 'name': 'San Francisco'})
 
 # ne Scientific station
 assert_has_feature(
     7, 44, 91, 'places',
-    { 'id': 4831, 'kind': 'locality', 'kind_detail': 'scientific_station'})
+    { 'kind': 'locality', 'kind_detail': 'scientific_station'})
