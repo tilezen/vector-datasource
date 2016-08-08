7917e07f211c2212f9f7a45dd3b2e585a721c980
Olga Kavvada <okavvada@gmail.com>
2016-08-08 12:55:46 -0700

    updated migrations hstore

--- data/migrations/v1.0.0-point.sql
+++ data/migrations/v1.0.0-point.sql
@@ -2,11 +2,11 @@ UPDATE
   planet_osm_point
   SET mz_poi_min_zoom = mz_calculate_min_zoom_pois(planet_osm_point.*)
   WHERE
-    (man_made IN ('wastewater_plant', 'water_works', 'works') OR
-     leisure IN ('golf_course', 'nature_reserve', 'park', 'pitch') OR
-     landuse IN ('cemetery', 'farm', 'forest', 'military', 'quarry', 'recreation_ground', 'village_green', 'winter_sports') OR
-     amenity = 'grave_yard' OR
-     boundary IN ('national_park', 'protected_area') OR
-     power IN ('plant', 'substation') OR
-     "natural" IN ('wood'))
+    (tags -> 'man_made' IN ('wastewater_plant', 'water_works', 'works') OR
+     tags -> 'leisure' IN ('golf_course', 'nature_reserve', 'park', 'pitch') OR
+     tags -> 'landuse' IN ('cemetery', 'farm', 'forest', 'military', 'quarry', 'recreation_ground', 'village_green', 'winter_sports') OR
+     tags -> 'amenity' = 'grave_yard' OR
+     tags -> 'boundary' IN ('national_park', 'protected_area') OR
+     tags -> 'power' IN ('plant', 'substation') OR
+     tags -> 'natural' IN ('wood'))
     AND COALESCE(mz_poi_min_zoom, 999) <> COALESCE(mz_calculate_min_zoom_pois(planet_osm_point.*), 999);
