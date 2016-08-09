6da24b8b6f875fedce15ef217679d3136dfa3b72
Olga Kavvada <okavvada@gmail.com>
2016-08-09 15:39:30 -0700

    natural forest and wood

--- data/migrations/v1.0.0-polygon.sql
+++ data/migrations/v1.0.0-polygon.sql
@@ -5,10 +5,10 @@ UPDATE
     (tags -> 'man_made' IN ('wastewater_plant', 'water_works', 'works') OR
      tags -> 'leisure' IN ('golf_course', 'nature_reserve', 'park', 'pitch') OR
      tags -> 'amenity' = 'grave_yard' OR
-     tags -> 'landuse' IN ('cemetery', 'farm', 'forest', 'military', 'quarry', 'recreation_ground', 'village_green', 'winter_sports') OR
+     tags -> 'landuse' IN ('cemetery', 'farm', 'forest', 'military', 'quarry', 'recreation_ground', 'village_green', 'winter_sports', 'wood') OR
      tags -> 'boundary' IN ('national_park', 'protected_area') OR
      tags -> 'power' IN ('plant', 'substation') OR
-     tags -> 'natural' IN ('wood'))
+     tags -> 'natural' IN ('wood', 'forest'))
     AND COALESCE(mz_poi_min_zoom, 999) <> COALESCE(mz_calculate_min_zoom_pois(planet_osm_polygon.*), 999);
 
 UPDATE
@@ -19,7 +19,8 @@ UPDATE
      tags -> 'leisure' IN ('golf_course', 'nature_reserve', 'park', 'pitch') OR
      tags -> 'natural' IN ('forest', 'park', 'wood') OR
      tags -> 'amenity' = 'grave_yard' OR
-     tags -> 'landuse' IN ('cemetery', 'farm', 'forest', 'military', 'quarry', 'recreation_ground', 'village_green', 'winter_sports') OR
+     tags -> 'landuse' IN ('cemetery', 'farm', 'forest', 'military', 'quarry', 'recreation_ground', 'village_green', 'winter_sports', 'wood') OR
      tags -> 'boundary' IN ('national_park', 'protected_area') OR
-     tags -> 'power' IN ('plant', 'substation'))
+     tags -> 'power' IN ('plant', 'substation') OR
+     tags -> 'natural' IN ('wood', 'forest'))
     AND COALESCE(mz_landuse_min_zoom, 999) <> COALESCE(mz_calculate_min_zoom_landuse(planet_osm_polygon.*), 999);
