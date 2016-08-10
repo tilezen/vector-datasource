2892717f354dab80830c593b30651ab9651ce559
Olga Kavvada <okavvada@gmail.com>
2016-08-10 11:53:08 -0700

    remove migrations of other branch

--- data/migrations/v1.0.0-polygon.sql
+++
@@ -1,6 +0,0 @@
-UPDATE
-  planet_osm_polygon
-  SET mz_water_min_zoom = mz_calculate_min_zoom_water(planet_osm_polygon.*)
-  WHERE
-    tags -> 'place' = 'sea'
-    AND COALESCE(mz_water_min_zoom, 999) <> COALESCE(mz_calculate_min_zoom_water(planet_osm_polygon.*), 999);
