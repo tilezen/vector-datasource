c9d7e370b20969b9323a83d7712caeb5d9cc8f8d
Olga Kavvada <okavvada@gmail.com>
2016-08-10 17:21:59 -0700

    included test for labeling

--- integration-test/951-remove-sea.py
+++ integration-test/951-remove-sea.py
@@ -1,11 +1,10 @@
-# Add source info in POIs
-
-# http://www.openstreetmap.org/way/329133245
-assert_has_feature(
-    12, 2315, 1580, 'roads',
-    {'id': 329133245, 'kind': 'minor_road'})
+# Drop sea polygon but keep the label
 
 # http://www.openstreetmap.org/relation/4594226
 assert_no_matching_feature(
     12, 2315, 1580, 'water',
-    {'id': -4594226, 'kind': 'sea'})
+    {'id': -4594226, 'kind': 'sea', 'label_placement': None})
+
+assert_matching_feature(
+    12, 2315, 1580, 'water',
+    {'id': -4594226, 'kind': 'sea', 'label_placement': True})
