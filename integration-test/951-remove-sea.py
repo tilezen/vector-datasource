291ddc69ea7477bac6fe980b1b426a104914f75f
Matt Amos <matt.amos@mapzen.com>
2016-08-11 12:33:38 +0100

    Find centroid of sea polygon which should be present as a label.

--- integration-test/951-remove-sea.py
+++ integration-test/951-remove-sea.py
@@ -5,6 +5,10 @@ assert_no_matching_feature(
     12, 2315, 1580, 'water',
     {'id': -4594226, 'kind': 'sea', 'label_placement': None})
 
-assert_matching_feature(
-    12, 2315, 1580, 'water',
+assert_has_feature(
+    9, 292, 197, 'water',
+    {'id': -4594226, 'kind': 'sea', 'label_placement': True})
+
+assert_has_feature(
+    12, 2338, 1579, 'water',
     {'id': -4594226, 'kind': 'sea', 'label_placement': True})
