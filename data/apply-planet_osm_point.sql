e5687285e33704959feba19d13179f5b39696ea1
Robert Marianski <rob@marianski.com>
2016-08-22 16:43:48 -0400

    Add pois indexes for polygon and point tables

--- data/apply-planet_osm_point.sql
+++ data/apply-planet_osm_point.sql
@@ -25,10 +25,11 @@ UPDATE planet_osm_point
   SET mz_building_min_zoom = mz_calculate_min_zoom_buildings(planet_osm_point.*)
   WHERE mz_calculate_min_zoom_buildings(planet_osm_point.*) IS NOT NULL;
 
-CREATE INDEX planet_osm_point_min_zoom_way_index ON planet_osm_point USING gist(way) WHERE mz_poi_min_zoom IS NOT NULL;
-CREATE INDEX planet_osm_point_min_zoom_way_9_index ON planet_osm_point USING gist(way) WHERE mz_poi_min_zoom <= 9;
-CREATE INDEX planet_osm_point_min_zoom_way_12_index ON planet_osm_point USING gist(way) WHERE mz_poi_min_zoom <= 12;
-CREATE INDEX planet_osm_point_min_zoom_way_15_index ON planet_osm_point USING gist(way) WHERE mz_poi_min_zoom <= 15;
+CREATE INDEX planet_osm_point_min_zoom_pois_index ON planet_osm_point USING gist(way) WHERE mz_poi_min_zoom IS NOT NULL;
+CREATE INDEX planet_osm_point_min_zoom_pois_6_index ON planet_osm_point USING gist(way) WHERE mz_poi_min_zoom <= 6;
+CREATE INDEX planet_osm_point_min_zoom_pois_9_index ON planet_osm_point USING gist(way) WHERE mz_poi_min_zoom <= 9;
+CREATE INDEX planet_osm_point_min_zoom_pois_12_index ON planet_osm_point USING gist(way) WHERE mz_poi_min_zoom <= 12;
+CREATE INDEX planet_osm_point_min_zoom_pois_15_index ON planet_osm_point USING gist(way) WHERE mz_poi_min_zoom <= 15;
 
 CREATE INDEX planet_osm_point_min_zoom_earth_index ON planet_osm_point USING gist(way) WHERE mz_earth_min_zoom IS NOT NULL;
 CREATE INDEX planet_osm_point_min_zoom_earth_9_index ON planet_osm_point USING gist(way) WHERE mz_earth_min_zoom <= 9;
