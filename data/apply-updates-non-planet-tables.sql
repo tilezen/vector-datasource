90358396348770217f334abe46b51d9d562d3d22
Robert Marianski <rob@marianski.com>
2016-08-29 17:18:41 -0400

    Handle invalid geometries in apply non planet

--- data/apply-updates-non-planet-tables.sql
+++ data/apply-updates-non-planet-tables.sql
@@ -1,3 +1,23 @@
+-- correct the invalid data in the ne tables from reprojection
+UPDATE ne_10m_lakes SET the_geom=ST_MakeValid(the_geom) WHERE NOT ST_IsValid(the_geom);
+UPDATE ne_110m_land SET the_geom=ST_MakeValid(the_geom) WHERE NOT ST_IsValid(the_geom);
+
+UPDATE ne_10m_admin_0_boundary_lines_land
+  SET
+    the_geom = (CASE WHEN GeometryType(ST_MakeValid(the_geom)) = 'MULTILINESTRING'
+                          THEN ST_MakeValid(the_geom)
+                     ELSE the_geom END)
+  WHERE NOT ST_IsValid(the_geom);
+DELETE FROM ne_10m_admin_0_boundary_lines_land WHERE NOT ST_IsValid(the_geom);
+
+UPDATE ne_10m_admin_1_states_provinces_lines
+  SET
+    the_geom = (CASE WHEN GeometryType(ST_MakeValid(the_geom)) = 'MULTILINESTRING'
+                          THEN ST_MakeValid(the_geom)
+                     ELSE the_geom END)
+  WHERE NOT ST_IsValid(the_geom);
+DELETE FROM ne_10m_admin_1_states_provinces_lines WHERE NOT ST_IsValid(the_geom);
+
 UPDATE ne_110m_land
   SET mz_earth_min_zoom = mz_calculate_min_zoom_earth(ne_110m_land.*)
   WHERE mz_calculate_min_zoom_earth(ne_110m_land.*) IS NOT NULL;
@@ -13,7 +33,6 @@ UPDATE ne_10m_land
 UPDATE land_polygons
   SET mz_earth_min_zoom = mz_calculate_min_zoom_earth(land_polygons.*)
   WHERE mz_calculate_min_zoom_earth(land_polygons.*) IS NOT NULL;
-
 
 UPDATE ne_110m_ocean
   SET mz_water_min_zoom = mz_calculate_min_zoom_water(ne_110m_ocean.*)
@@ -83,9 +102,28 @@ UPDATE ne_10m_admin_1_states_provinces_lines
   SET mz_boundary_min_zoom = mz_calculate_min_zoom_boundaries(ne_10m_admin_1_states_provinces_lines.*)
   WHERE mz_calculate_min_zoom_boundaries(ne_10m_admin_1_states_provinces_lines.*) IS NOT NULL;
 
+UPDATE ne_10m_populated_places
+  SET mz_places_min_zoom = mz_calculate_min_zoom_places(ne_10m_populated_places.*)
+  WHERE mz_calculate_min_zoom_places(ne_10m_populated_places.*) IS NOT NULL;
 
-DO $$
-BEGIN
+UPDATE water_polygons SET mz_label_placement = ST_PointOnSurface(the_geom);
+UPDATE land_polygons SET mz_label_placement = ST_PointOnSurface(the_geom);
+UPDATE ne_110m_ocean SET mz_label_placement = ST_PointOnSurface(the_geom);
+UPDATE ne_50m_ocean SET mz_label_placement = ST_PointOnSurface(the_geom);
+UPDATE ne_10m_ocean SET mz_label_placement = ST_PointOnSurface(the_geom);
+UPDATE ne_110m_coastline SET mz_label_placement = ST_PointOnSurface(the_geom);
+UPDATE ne_50m_coastline SET mz_label_placement = ST_PointOnSurface(the_geom);
+UPDATE ne_10m_coastline SET mz_label_placement = ST_PointOnSurface(the_geom);
+UPDATE ne_110m_lakes SET mz_label_placement = ST_PointOnSurface(the_geom);
+UPDATE ne_50m_lakes SET mz_label_placement = ST_PointOnSurface(the_geom);
+UPDATE ne_10m_lakes SET mz_label_placement = ST_PointOnSurface(the_geom);
+UPDATE ne_50m_playas SET mz_label_placement = ST_PointOnSurface(the_geom);
+UPDATE ne_10m_playas SET mz_label_placement = ST_PointOnSurface(the_geom);
+UPDATE ne_110m_land SET mz_label_placement = ST_PointOnSurface(the_geom);
+UPDATE ne_50m_land SET mz_label_placement = ST_PointOnSurface(the_geom);
+UPDATE ne_10m_land SET mz_label_placement = ST_PointOnSurface(the_geom);
+UPDATE ne_50m_urban_areas SET mz_label_placement = ST_PointOnSurface(the_geom);
+UPDATE ne_10m_urban_areas SET mz_label_placement = ST_PointOnSurface(the_geom);
 
 UPDATE ne_10m_urban_areas SET way_area=ST_Area(the_geom) WHERE the_geom IS NOT NULL;
 CREATE INDEX ne_10m_urban_areas_way_area_index ON ne_10m_urban_areas(way_area);
@@ -120,7 +158,6 @@ CREATE INDEX ne_10m_playas_wayarea_index ON ne_10m_playas(way_area);
 UPDATE water_polygons SET way_area=ST_Area(the_geom) WHERE the_geom IS NOT NULL;
 CREATE INDEX water_polygons_wayarea_index ON water_polygons(way_area);
 
-
 UPDATE ne_110m_land SET way_area=ST_Area(the_geom) WHERE the_geom IS NOT NULL;
 CREATE INDEX ne_110m_land_wayarea_index ON ne_110m_land(way_area);
 
@@ -133,25 +170,3 @@ CREATE INDEX ne_10m_land_way_area_index ON ne_10m_land(way_area);
 UPDATE land_polygons SET way_area=ST_Area(the_geom) WHERE the_geom IS NOT NULL;
 CREATE INDEX land_polygons_wayarea_index ON land_polygons(way_area);
 
-UPDATE ne_10m_populated_places SET mz_places_min_zoom = mz_calculate_min_zoom_places(ne_10m_populated_places.*);
-
-UPDATE water_polygons SET mz_label_placement = ST_PointOnSurface(the_geom);
-UPDATE land_polygons SET mz_label_placement = ST_PointOnSurface(the_geom);
-UPDATE ne_110m_ocean SET mz_label_placement = ST_PointOnSurface(the_geom);
-UPDATE ne_50m_ocean SET mz_label_placement = ST_PointOnSurface(the_geom);
-UPDATE ne_10m_ocean SET mz_label_placement = ST_PointOnSurface(the_geom);
-UPDATE ne_110m_coastline SET mz_label_placement = ST_PointOnSurface(the_geom);
-UPDATE ne_50m_coastline SET mz_label_placement = ST_PointOnSurface(the_geom);
-UPDATE ne_10m_coastline SET mz_label_placement = ST_PointOnSurface(the_geom);
-UPDATE ne_110m_lakes SET mz_label_placement = ST_PointOnSurface(the_geom);
-UPDATE ne_50m_lakes SET mz_label_placement = ST_PointOnSurface(the_geom);
-UPDATE ne_10m_lakes SET mz_label_placement = ST_PointOnSurface(the_geom);
-UPDATE ne_50m_playas SET mz_label_placement = ST_PointOnSurface(the_geom);
-UPDATE ne_10m_playas SET mz_label_placement = ST_PointOnSurface(the_geom);
-UPDATE ne_110m_land SET mz_label_placement = ST_PointOnSurface(the_geom);
-UPDATE ne_50m_land SET mz_label_placement = ST_PointOnSurface(the_geom);
-UPDATE ne_10m_land SET mz_label_placement = ST_PointOnSurface(the_geom);
-UPDATE ne_50m_urban_areas SET mz_label_placement = ST_PointOnSurface(the_geom);
-UPDATE ne_10m_urban_areas SET mz_label_placement = ST_PointOnSurface(the_geom);
-
-END $$;
