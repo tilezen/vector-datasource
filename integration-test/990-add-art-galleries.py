e783d7aa539b1b00f18e6fa1de1d86908f1201ff
Ian Dees <ian.dees@gmail.com>
2016-08-30 18:44:03 -0400

    Adjust tests per @nvkelso's comments

--- integration-test/990-add-art-galleries.py
+++ integration-test/990-add-art-galleries.py
@@ -1,9 +1,9 @@
 # http://www.openstreetmap.org/node/2026996113
 assert_has_feature(
-    17, 20971, 50656, 'pois',
-    { 'id': 2026996113, 'kind': 'gallery' })
+    16, 10485, 25328, 'pois',
+    { 'id': 2026996113, 'kind': 'gallery', 'min_zoom': 16 })
 
 # http://www.openstreetmap.org/way/31510288
 assert_has_feature(
-    17, 65485, 43583, 'pois',
+    15, 16371, 10895, 'pois',
     { 'id': 31510288, 'kind': 'gallery' })
