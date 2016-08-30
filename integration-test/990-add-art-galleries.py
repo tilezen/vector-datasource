2817c4c4317d937e435280c62fd7461256ef4206
Ian Dees <ian.dees@gmail.com>
2016-08-30 11:45:25 -0400

    Add integration test

---
+++ integration-test/990-add-art-galleries.py
@@ -0,0 +1,9 @@
+# http://www.openstreetmap.org/node/2026996113
+assert_has_feature(
+    17, 20971, 50656, 'pois',
+    { 'id': 2026996113, 'kind': 'gallery' })
+
+# http://www.openstreetmap.org/way/31510288
+assert_has_feature(
+    17, 65485, 43583, 'pois',
+    { 'id': 31510288, 'kind': 'gallery' })
