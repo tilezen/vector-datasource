2e9b1291235f4e87e15938a34eeec75302f8bc0c
Ian Dees <ian.dees@gmail.com>
2016-09-01 18:09:22 -0400

    Add a test for localized names in a tile with NY/NJ boundaries

---
+++ integration-test/1016-missing-localized-names.py
@@ -0,0 +1,10 @@
+# New Jersey - New York state boundary
+# http://www.openstreetmap.org/relation/224951 (New Jersey)
+# http://www.openstreetmap.org/relation/61320 (New York state)
+assert_has_feature(
+    15, 9643, 12327, "boundaries",
+    {"kind": "state", "name": "New York - New Jersey",
+    "name:right": "New York", "name:left": "New Jersey",
+    "name:right:es": "Nueva York", "name:left:es": "Nueva Jersey",
+    "name:right:lv":"\u0145\u016bd\u017eersija", "name:left:lv":"\u0145ujorka"
+    })
