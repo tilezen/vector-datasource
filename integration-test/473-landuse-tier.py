bc955b16d5bc47b68aad3fc82f8880a506d0c39a
Matt Amos <matt.amos@mapzen.com>
2016-09-07 17:01:50 +0100

    Use min zoom for landuse polygon tests.

--- integration-test/473-landuse-tier.py
+++ integration-test/473-landuse-tier.py
@@ -1,7 +1,7 @@
 # http://www.openstreetmap.org/way/167274589
 # area 300363008
 assert_has_feature(
-    16, 10818, 21900, 'landuse',
+    4, 2, 5, 'landuse',
     { 'kind': 'national_park', 'id': 167274589, 'tier': 1,
       'min_zoom': 3 })
 assert_has_feature(
@@ -12,7 +12,7 @@ assert_has_feature(
 # http://www.openstreetmap.org/relation/921675
 # area 30089300
 assert_has_feature(
-    16, 14579, 29651, 'landuse',
+    8, 56, 115, 'landuse',
     { 'kind': 'national_park', 'id': -921675, 'tier': 1,
       'min_zoom': 8 })
 assert_no_matching_feature(
@@ -27,7 +27,7 @@ assert_has_feature(
 # http://www.openstreetmap.org/way/34416231
 # area 86685400
 assert_has_feature(
-    16, 18270, 25157, 'landuse',
+    8, 71, 98, 'landuse',
     { 'kind': 'national_park', 'id': 34416231,
       'tier': 2, 'min_zoom': 8 })
 # this one is clipped by the polygon min_zoom, and so appears at the same
