260cd3b96df3a4746560db0032d7b6042c55d7fc
Ian Dees <ian.dees@gmail.com>
2016-09-09 11:00:30 -0400

    Add tests for source and min_zoom

--- integration-test/976-fractional-pois.py
+++ integration-test/976-fractional-pois.py
@@ -3,3 +3,51 @@
 assert_has_feature(
     15, 5242, 12664, 'pois',
     { 'id': 147689077, 'min_zoom': 15.68 })
+
+# Test that source and min_zoom are set properly for boundaries, roads, transit, and water
+assert_has_feature(
+    5, 9, 12, 'boundaries',
+    { 'min_zoom': 0 , 'id': 8024,
+    'source': 'naturalearthdata.com',
+    'name': 'New Jersey - Pennsylvania' })
+
+assert_has_feature(
+    5, 9, 12, 'roads',
+    { 'min_zoom': 5 , 'id': 90,
+    'source': 'naturalearthdata.com' })
+
+# There is no transit data from Natural Earth
+
+assert_has_feature(
+    5, 9, 12, 'water',
+    { 'min_zoom': 0 , 'id': 1144,
+    'source': 'naturalearthdata.com',
+    'name': 'John H. Kerr Reservoir' })
+
+# https://www.openstreetmap.org/relation/224951
+# https://www.openstreetmap.org/relation/61320
+assert_has_feature(
+    9, 150, 192, 'boundaries',
+    { 'min_zoom': 8, 'id':  -224951,
+    'source': 'openstretmap.org',
+    'name': 'New Jersey - New York' })
+
+assert_has_feature(
+    9, 150, 192, 'roads',
+    { 'min_zoom': 8, 'sort_key':  381,
+    'source': 'openstretmap.org',
+    'kind': 'Major Road',
+    'network': 'US:NJ:Hudson' })
+
+assert_has_feature(
+    9, 150, 192, 'transit',
+    { 'min_zoom': 5, 'ref':  '54-57',
+    'source': 'openstretmap.org',
+    'name': 'Vermonter' })
+
+assert_has_feature(
+    9, 150, 192, 'water',
+    { 'min_zoom': 0, 'id':  10613,
+    'source': 'openstretmapdata.com',
+    'kind': 'ocean',
+    'name': '' })
