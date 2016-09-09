9afb2c0641b549abdb898141602a15fbf53cce2a
Matt Amos <matt.amos@mapzen.com>
2016-09-09 18:45:19 +0100

    Update name->ref. Drop different properties on low-zoom NE data.

--- integration-test/896-ne-shield-enums-2.py
+++ integration-test/896-ne-shield-enums-2.py
@@ -19,3 +19,17 @@ assert_no_matching_feature(
     7, 63, 42, 'roads',
     { 'source': 'naturalearthdata.com', 'kind': 'highway',
       'namealtt': None })
+
+# network & shield text should stay on highways at lower zooms.
+# E30 (M4) in UK
+for z in range(5, 6):
+    # tile 7/63/42 was tested in 896-ne-shield-enums.py
+    x = 63 >> (7 - z)
+    y = 42 >> (7 - z)
+    assert_has_feature(
+        z, x, y, 'roads',
+        { 'source': 'naturalearthdata.com', 'network': 'e-road',
+          'kind': 'highway', 'shield_text': '30' })
+    # but not name, ref or all_*
+    for prop in ['name', 'ref', 'all_networks', 'all_shield_texts']:
+        assert_no_matching_feature(z, x, y, 'roads', { prop: None })
