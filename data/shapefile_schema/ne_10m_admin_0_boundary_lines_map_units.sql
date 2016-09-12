26e1c1562c59d22fc050abb41c96d6f751409c8f
Nathaniel Kelso <nvkelso@gmail.com>
2016-09-12 08:55:33 -0700

    fix mapcolor > mapcolor9 on map_unit SHP import

--- data/shapefile_schema/ne_10m_admin_0_boundary_lines_map_units.sql
+++ data/shapefile_schema/ne_10m_admin_0_boundary_lines_map_units.sql
@@ -7,6 +7,6 @@ CREATE TABLE "ne_10m_admin_0_boundary_lines_map_units" (
   "comment" varchar(100),
   "adm0_a3" varchar(3),
   "adm0_name" varchar(100),
-  "mapcolor" int2);
+  "mapcolor9" int2);
 ALTER TABLE "ne_10m_admin_0_boundary_lines_map_units" ADD PRIMARY KEY (gid);
 SELECT AddGeometryColumn('','ne_10m_admin_0_boundary_lines_map_units','the_geom','3857','MULTILINESTRING',2);
