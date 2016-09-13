87aa80ad6c558ae3f374611f29a85f694e1b86f8
Robert Marianski <rob@marianski.com>
2016-09-13 10:32:55 -0400

    Add missing comma

--- data/shapefile_schema/ne_50m_admin_1_states_provinces_lines.sql
+++ data/shapefile_schema/ne_50m_admin_1_states_provinces_lines.sql
@@ -3,7 +3,7 @@ CREATE TABLE "ne_50m_admin_1_states_provinces_lines" (
   gid serial,
   "objectid" int4,
   "scalerank" numeric,
-  "featurecla" varchar(32)
+  "featurecla" varchar(32),
   "name" varchar(100),
   "adm0_a3" varchar(3),
   "adm0_name" varchar(100),
