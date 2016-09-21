33c555eec889e5bdbee2e526ed58c05d6c9bd50c
Robert Marianski <rob@marianski.com>
2016-09-21 12:45:14 -0400

    Restore ne 1st Order Admin Lines rule

--- data/migrations/v1.0.0-other.sql
+++ data/migrations/v1.0.0-other.sql
@@ -13,3 +13,21 @@ UPDATE ne_50m_land SET mz_label_placement = ST_PointOnSurface(the_geom) WHERE mz
 UPDATE ne_10m_land SET mz_label_placement = ST_PointOnSurface(the_geom) WHERE mz_label_placement IS NULL;
 UPDATE ne_50m_urban_areas SET mz_label_placement = ST_PointOnSurface(the_geom) WHERE mz_label_placement IS NULL;
 UPDATE ne_10m_urban_areas SET mz_label_placement = ST_PointOnSurface(the_geom) WHERE mz_label_placement IS NULL;
+
+UPDATE ne_110m_admin_0_boundary_lines_land
+  SET mz_boundary_min_zoom = mz_calculate_min_zoom_boundaries(ne_110m_admin_0_boundary_lines_land.*);
+
+UPDATE ne_50m_admin_0_boundary_lines_land
+  SET mz_boundary_min_zoom = mz_calculate_min_zoom_boundaries(ne_50m_admin_0_boundary_lines_land.*);
+
+UPDATE ne_50m_admin_1_states_provinces_lines
+  SET mz_boundary_min_zoom = mz_calculate_min_zoom_boundaries(ne_50m_admin_1_states_provinces_lines.*);
+
+UPDATE ne_10m_admin_0_boundary_lines_land
+  SET mz_boundary_min_zoom = mz_calculate_min_zoom_boundaries(ne_10m_admin_0_boundary_lines_land.*);
+
+UPDATE ne_10m_admin_0_boundary_lines_map_units
+  SET mz_boundary_min_zoom = mz_calculate_min_zoom_boundaries(ne_10m_admin_0_boundary_lines_map_units.*);
+
+UPDATE ne_10m_admin_1_states_provinces_lines
+  SET mz_boundary_min_zoom = mz_calculate_min_zoom_boundaries(ne_10m_admin_1_states_provinces_lines.*);
