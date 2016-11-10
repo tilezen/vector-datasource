a2f52f243e5d099057d8c67efd1a16a3dea5fcf5
Ian Dees <ian.dees@gmail.com>
2016-11-09 21:59:54 -0500

    I think I'm tired

--- integration-test/1106-merge-ocean-earth.py
+++ integration-test/1106-merge-ocean-earth.py
@@ -1,4 +1,4 @@
 # There should be a single (merged) ocean feature in this tile
-assert_less_than_n_features(9, 167, 186, 'ocean', {'kind': 'ocean'}, 2)
+assert_less_than_n_features(9, 167, 186, 'water', {'kind': 'ocean'}, 2)
 # There should be a single (merged) earth feature in this tile
 assert_less_than_n_features(9, 170, 186, 'earth', {'kind': 'earth'}, 2)
