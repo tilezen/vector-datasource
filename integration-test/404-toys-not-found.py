7c784f47a64d8a09dbf4180bc8fef023d16f821a
Nathaniel Kelso <nvkelso@gmail.com>
2017-01-06 15:20:15 -0800

    this toy store was deleted; we have other examples so removing (rather than replacing)

--- integration-test/404-toys-not-found.py
+++ integration-test/404-toys-not-found.py
@@ -7,7 +7,6 @@ tiles = [
     (17, 21019, 50617), # https://www.openstreetmap.org/node/2286100659
     (17, 21028, 50645), # https://www.openstreetmap.org/node/3711137981
     (17, 38597, 49266), # https://www.openstreetmap.org/node/3810578539
-    (17, 38598, 49259), # http://www.openstreetmap.org/node/2678466844
     (17, 38600, 49261), # https://www.openstreetmap.org/node/1429062988
     (17, 38601, 49258), # https://www.openstreetmap.org/node/1058296287
 ]
