a9711705a8c122bf7e7f1edbf9b640c3be5f8510
Robert Marianski <rob@marianski.com>
2017-03-23 11:40:26 -0400

    Update water boundary sort key test zooms

--- integration-test/552-water-boundary-sort-key.py
+++ integration-test/552-water-boundary-sort-key.py
@@ -1,4 +1,4 @@
 # from https://github.com/mapzen/vector-datasource/issues/552
 assert_has_feature(
-    19, 83900, 202617, "water",
+    16, 10487, 25327, "water",
     {"kind": "ocean", "boundary": True, "sort_rank": 205})
