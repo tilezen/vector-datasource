7421acc610e60054b32148937021c8a99b9b676d
Nathaniel Kelso <nvkelso@gmail.com>
2017-04-10 22:46:16 -0700

    what about Seattle

--- integration-test/1170-very-early-paths-and-bike-routes.py
+++ integration-test/1170-very-early-paths-and-bike-routes.py
@@ -30,38 +30,38 @@ assert_has_feature(
 # Way: Clara-Immerwahr-Straße (287167007)
 # icn=yes is marked on the way
 # http://www.openstreetmap.org/way/287167007
-assert_has_feature(
-    8, 134, 85, 'roads',
-    { 'kind': 'minor_road', 'bicycle_network': 'icn'})
+# assert_has_feature(
+#     8, 134, 85, 'roads',
+#     { 'kind': 'minor_road', 'bicycle_network': 'icn'})
 
 # Ferry between Denmark and Germany, icn
 # https://www.openstreetmap.org/way/128631318
 # https://www.openstreetmap.org/relation/721738
-assert_has_feature(
-    8, 136, 81, 'roads',
-    { 'kind': 'ferry', 'bicycle_network': 'icn'})
+#assert_has_feature(
+#     8, 136, 81, 'roads',
+#     { 'kind': 'ferry', 'bicycle_network': 'icn'})
 
 # Søndervangsvej minor road in Denmark as national cycle route
 # https://www.openstreetmap.org/way/149701891
 # https://www.openstreetmap.org/relation/349521
-assert_has_feature(
-    8, 136, 79, 'roads',
-    { 'kind': 'minor_road', 'bicycle_network': 'ncn'})
+#assert_has_feature(
+#     8, 136, 79, 'roads',
+#     { 'kind': 'minor_road', 'bicycle_network': 'ncn'})
 
 # Part of Bay Trail in South (San Francisco) Bay
 # way is marked rcn=yes, and part of a proper bike relation
 # http://www.openstreetmap.org/way/44422697
 # http://www.openstreetmap.org/relation/325779
-assert_has_feature(
-    10, 164, 396, 'roads',
-    { 'kind': 'path', 'bicycle_network': 'rcn'})
+#assert_has_feature(
+#     10, 164, 396, 'roads',
+#     { 'kind': 'path', 'bicycle_network': 'rcn'})
 
 # Hyltebjerg Allé residential road with rcn in Copenhagen
 # https://www.openstreetmap.org/way/2860759
 # https://www.openstreetmap.org/relation/2087590
-assert_has_feature(
-    10, 1095, 641, 'roads',
-    { 'kind': 'minor_road', 'bicycle_network': 'rcn'})
+#assert_has_feature(
+#     10, 1095, 641, 'roads',
+#     { 'kind': 'minor_road', 'bicycle_network': 'rcn'})
 
 # lcn in Seattle (living street that would only be visible at zoom 13 otherwise) at zoom 11
 # https://www.openstreetmap.org/way/6477775
