30352005cfec65848c4f83c73e9c3a83f2c1f6d1
Matt Amos <matt.amos@mapzen.com>
2017-04-19 11:44:30 +0100

    Fix migrations to update all no-name things.

--- data/migrations/v1.3.0-polygon.sql
+++ data/migrations/v1.3.0-polygon.sql
@@ -1,8 +1,13 @@
 UPDATE
   planet_osm_polygon p
-  SET mz_poi_min_zoom = mz_calculate_min_zoom_pois(p.*)
+  SET mz_poi_min_zoom = mz_calculate_min_zoom_pois(p.*),
+      mz_landuse_min_zoom = mz_calculate_min_zoom_landuse(p.*)
   WHERE
     (tags -> 'leisure' = 'garden' OR
      tags -> 'landuse' = 'village_green' OR
-     tags -> 'natural' IN ('wood', 'forest'))
-    AND COALESCE(mz_poi_min_zoom, 999) <> COALESCE(mz_calculate_min_zoom_pois(p.*), 999);
+     tags -> 'natural' IN ('wood', 'forest') OR
+     (NOT tags ? 'name' AND mz_poi_min_zoom IS NOT NULL))
+    AND (
+      COALESCE(mz_poi_min_zoom, 999) <> COALESCE(mz_calculate_min_zoom_pois(p.*), 999) OR
+      COALESCE(mz_landuse_min_zoom, 999) <> COALESCE(mz_calculate_min_zoom_landuse(p.*), 999)
+    );
