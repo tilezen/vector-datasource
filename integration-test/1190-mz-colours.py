4894dfd6da42d266817b1e745283cfc7393391dc
Matt Amos <matt.amos@mapzen.com>
2017-04-24 18:23:24 +0100

    Enable palette colours for transit lines. Add integration test.

---
+++ integration-test/1190-mz-colours.py
@@ -0,0 +1,5 @@
+# http://www.openstreetmap.org/relation/366773
+assert_has_feature(
+    16, 19310, 24645, 'transit',
+    { 'kind': 'subway', 'ref': 'Z',
+      'colour': '#996633', 'palette': 'peru' })
