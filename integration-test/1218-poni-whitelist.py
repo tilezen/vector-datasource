c8548f95535f7b3f9f3542152ad7cd9648fdfdcd
Nathaniel Kelso <nvkelso@gmail.com>
2017-04-26 17:29:30 -0700

    fix bad grep job

--- integration-test/1218-poni-whitelist.py
+++ integration-test/1218-poni-whitelist.py
@@ -1,7 +1,7 @@
 # aeroway=helipad
 # min_zoom: 16
 # https://www.openstreetmap.org/node/2207370738
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 5230, 12657, 'pois',
     {'id': 2207370738})
 assert_has_feature(
@@ -12,7 +12,7 @@ assert_has_feature(
 # min_zoom: 17
 # it's on the edge, might be 16, 10481, 25338
 # https://www.openstreetmap.org/node/4113423533
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 5242, 12668, 'pois',
     {'id': 4113423533})
 assert_has_feature(
@@ -22,7 +22,7 @@ assert_has_feature(
 # amenity=bench
 # min_zoom: 18
 # https://www.openstreetmap.org/node/3951215438
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 5239, 12672, 'pois',
     {'id': 3951215438})
 assert_has_feature(
@@ -33,7 +33,7 @@ assert_has_feature(
 # when operator: false, then min_zoom: 16
 # when operator: true, then min_zoom: 17
 # https://www.openstreetmap.org/node/3509468129
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 5251, 12655, 'pois',
     {'id': 3509468129})
 assert_has_feature(
@@ -46,7 +46,7 @@ assert_has_feature(
 # amenity=car_sharing
 # min_zoom: 16
 # https://www.openstreetmap.org/node/4758733421
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 5266, 12706, 'pois',
     {'id': 4758733421})
 assert_has_feature(
@@ -55,7 +55,7 @@ assert_has_feature(
 
 # amenity=fuel
 # https://www.openstreetmap.org/node/2059530671
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 5239, 12671, 'pois',
     {'id': 2059530671})
 assert_has_feature(
@@ -64,7 +64,7 @@ assert_has_feature(
 
 # amenity=post_box
 # https://www.openstreetmap.org/node/4397690695
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 5239, 12672, 'pois',
     {'id': 4397690695})
 assert_has_feature(
@@ -73,7 +73,7 @@ assert_has_feature(
 
 # amenity=recycling
 # https://www.openstreetmap.org/node/2338524067
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 5243, 12666, 'pois',
     {'id': 2338524067})
 assert_has_feature(
@@ -82,7 +82,7 @@ assert_has_feature(
 
 # amenity=shelter
 # https://www.openstreetmap.org/node/4177497032
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 5250, 12700, 'pois',
     {'id': 4177497032})
 assert_has_feature(
@@ -91,7 +91,7 @@ assert_has_feature(
 
 # amenity=telephone
 # https://www.openstreetmap.org/node/1241673617
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 5241, 12670, 'pois',
     {'id': 1241673617})
 assert_has_feature(
@@ -100,7 +100,7 @@ assert_has_feature(
 
 # amenity=waste_basket
 # https://www.openstreetmap.org/node/4265260145
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 5240, 12672, 'pois',
     {'id': 4265260145})
 assert_has_feature(
@@ -109,7 +109,7 @@ assert_has_feature(
 
 # highway=bus_stop
 # https://www.openstreetmap.org/node/1229920715
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 5239, 12672, 'pois',
     {'id': 1229920715})
 assert_has_feature(
@@ -118,7 +118,7 @@ assert_has_feature(
 
 # highway=ford
 # https://www.openstreetmap.org/node/880807552
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 5767, 13110, 'pois',
     {'id': 880807552})
 assert_has_feature(
@@ -127,7 +127,7 @@ assert_has_feature(
 
 # highway=mini_roundabout
 # https://www.openstreetmap.org/node/110392851
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 5228, 12650, 'pois',
     {'id': 110392851})
 assert_has_feature(
@@ -136,7 +136,7 @@ assert_has_feature(
 
 # highway=platform
 # https://www.openstreetmap.org/node/1275126569
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 7539, 11137, 'pois',
     {'id': 1275126569})
 assert_has_feature(
@@ -145,7 +145,7 @@ assert_has_feature(
 
 # highway=traffic_signals
 # https://www.openstreetmap.org/node/65329980
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 5242, 12664, 'pois',
     {'id': 65329980})
 assert_has_feature(
@@ -154,7 +154,7 @@ assert_has_feature(
 
 # waterway=lock
 # https://www.openstreetmap.org/node/675426915
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 9399, 12418, 'pois',
     {'id': 675426915})
 assert_has_feature(
@@ -163,7 +163,7 @@ assert_has_feature(
 
 # landuse=quarry
 # https://www.openstreetmap.org/node/3356570361
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 5265, 12615, 'pois',
     {'id': 3356570361})
 assert_has_feature(
@@ -178,7 +178,7 @@ assert_has_feature(
 
 # leisure=dog_park
 # https://www.openstreetmap.org/node/1229112075
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 5235, 12669, 'pois',
     {'id': 1229112075})
 assert_has_feature(
@@ -187,7 +187,7 @@ assert_has_feature(
 
 # leisure=slipway
 # https://www.openstreetmap.org/node/2678961627
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 5238, 12652, 'pois',
     {'id': 2678961627})
 assert_has_feature(
@@ -196,7 +196,7 @@ assert_has_feature(
 
 # lock='yes'
 # https://www.openstreetmap.org/node/365485771
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 9527, 11985, 'pois',
     {'id': 365485771})
 assert_has_feature(
@@ -205,7 +205,7 @@ assert_has_feature(
 
 # man_made=adit
 # https://www.openstreetmap.org/node/4469596254
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 5626, 12731, 'pois',
     {'id': 4469596254})
 assert_has_feature(
@@ -214,7 +214,7 @@ assert_has_feature(
 
 # man_made=mineshaft
 # https://www.openstreetmap.org/node/1818064871
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 5378, 12607, 'pois',
     {'id': 1818064871})
 assert_has_feature(
@@ -223,7 +223,7 @@ assert_has_feature(
 
 # man_made=offshore_platform
 # https://www.openstreetmap.org/node/4239915448
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 7549, 13919, 'pois',
     {'id': 4239915448})
 assert_has_feature(
@@ -244,7 +244,7 @@ assert_has_feature(
 
 # man_made=telescope
 # https://www.openstreetmap.org/node/3310910810
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 5334, 12510, 'pois',
     {'id': 3310910810})
 assert_has_feature(
@@ -253,7 +253,7 @@ assert_has_feature(
 
 # natural=cave_entrance
 # https://www.openstreetmap.org/node/1050825518
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 5178, 12346, 'pois',
     {'id': 1050825518})
 assert_has_feature(
@@ -262,7 +262,7 @@ assert_has_feature(
 
 # natural=waterfall
 # https://www.openstreetmap.org/node/4719786091
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 5491, 12694, 'pois',
     {'id': 4719786091})
 assert_has_feature(
@@ -271,7 +271,7 @@ assert_has_feature(
 
 # public_transport=platform
 # https://www.openstreetmap.org/node/2073000913
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 5238, 12671, 'pois',
     {'id': 2073000913})
 assert_has_feature(
@@ -280,7 +280,7 @@ assert_has_feature(
 
 # public_transport=stop_area
 # https://www.openstreetmap.org/node/2991866242
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 5214, 12588, 'pois',
     {'id': 2991866242})
 assert_has_feature(
@@ -289,7 +289,7 @@ assert_has_feature(
 
 # railway=halt
 # https://www.openstreetmap.org/node/2382580308
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 9545, 12368, 'pois',
     {'id': 2382580308})
 assert_has_feature(
@@ -298,7 +298,7 @@ assert_has_feature(
 
 # railway=platform
 # https://www.openstreetmap.org/node/3987143106
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 9821, 12242, 'pois',
     {'id': 3987143106})
 assert_has_feature(
@@ -307,7 +307,7 @@ assert_has_feature(
 
 # railway=stop
 # https://www.openstreetmap.org/node/1130268570
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 9381, 12527, 'pois',
     {'id': 1130268570})
 assert_has_feature(
@@ -318,7 +318,7 @@ assert_has_feature(
 # public_transport=stop_position
 # railway=stop
 # https://www.openstreetmap.org/node/3721890342
-assert_no_matching_feature
+assert_no_matching_feature(
     13, 1316, 3176, 'pois',
     {'id': 3721890342})
 assert_has_feature(
@@ -327,7 +327,7 @@ assert_has_feature(
 
 # railway=subway_entrance
 # https://www.openstreetmap.org/node/3833748147
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 9367, 12536, 'pois',
     {'id': 3833748147})
 assert_has_feature(
@@ -336,7 +336,7 @@ assert_has_feature(
 
 # railway=tram_stop
 # https://www.openstreetmap.org/node/1719012916
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 5239, 12670, 'pois',
     {'id': 1719012916})
 assert_has_feature(
@@ -345,7 +345,7 @@ assert_has_feature(
 
 # railway=level_crossing (but make min_zoom 18)
 # https://www.openstreetmap.org/node/4665711307
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 5235, 12667, 'pois',
     {'id': 4665711307})
 assert_has_feature(
@@ -357,7 +357,7 @@ assert_has_feature(
 
 # tags->whitewater=egress
 # https://www.openstreetmap.org/node/4696619992
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 5362, 12461, 'pois',
     {'id': 4696619992})
 assert_has_feature(
@@ -366,7 +366,7 @@ assert_has_feature(
 
 # tags->whitewater=hazard
 # https://www.openstreetmap.org/node/4253919482
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 5387, 12481, 'pois',
     {'id': 4253919482})
 assert_has_feature(
@@ -375,7 +375,7 @@ assert_has_feature(
 
 # tags->whitewater=put_in
 # https://www.openstreetmap.org/node/3688927027
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 5541, 12666, 'pois',
     {'id': 3688927027})
 assert_has_feature(
@@ -384,7 +384,7 @@ assert_has_feature(
 
 # tags->whitewater=rapid
 # https://www.openstreetmap.org/node/4253919493
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 5385, 12482, 'pois',
     {'id': 4253919493})
 assert_has_feature(
@@ -393,7 +393,7 @@ assert_has_feature(
 
 # tourism=alpine_hut
 # https://www.openstreetmap.org/node/1076123765
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 5433, 12607, 'pois',
     {'id': 1076123765})
 assert_has_feature(
@@ -402,7 +402,7 @@ assert_has_feature(
 
 # tourism=viewpoint
 # https://www.openstreetmap.org/node/3065529317
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 5236, 12667, 'pois',
     {'id': 3065529317})
 assert_has_feature(
@@ -411,7 +411,7 @@ assert_has_feature(
 
 # tourism=wilderness_hut
 # https://www.openstreetmap.org/node/1837443430
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 17100, 11564, 'pois',
     {'id': 1837443430})
 assert_has_feature(
@@ -420,7 +420,7 @@ assert_has_feature(
 
 # waterway=waterfall
 # https://www.openstreetmap.org/node/4319935813
-assert_no_matching_feature
+assert_no_matching_feature(
     15, 5449, 12526, 'pois',
     {'id': 4319935813})
 assert_has_feature(
