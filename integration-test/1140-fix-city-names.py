5600df2cbabda47fa649bc103be2f5c92e6b23be
Matt Amos <matt.amos@mapzen.com>
2017-04-27 18:00:42 +0100

    Add migration to fix city names.

---
+++ integration-test/1140-fix-city-names.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# data is in the fixture ne_10m_populated_places/1140-fix-city-names.shp
+assert_has_feature(
+    7, 38, 45, 'places',
+    { 'name': 'Québec', 'source': 'naturalearthdata.com' })
+
+# this one has an interesting character outside of the extended latin
+# charset, so hopefully will test an extra path from just acute characters
+# and umlauts.
+assert_has_feature(
+    7, 57, 32, 'places',
+    { 'name': 'Sauðárkrókur', 'source': 'naturalearthdata.com' })
+
+# ditto interesting character
+assert_has_feature(
+    7, 8, 27, 'places',
+    { 'name': 'Utqiaġvik', 'source': 'naturalearthdata.com' })
