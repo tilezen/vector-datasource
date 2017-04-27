50bbdfd10bf43a6e018234348f6efe3fdad0abf3
Matt Amos <matt.amos@mapzen.com>
2017-04-27 11:05:16 +0100

    Adjust min zoom for piers.

--- data/migrations/v1.3.0-polygon.sql
+++ data/migrations/v1.3.0-polygon.sql
@@ -6,6 +6,7 @@ UPDATE
     (tags -> 'leisure' = 'garden' OR
      tags -> 'landuse' = 'village_green' OR
      tags -> 'natural' IN ('wood', 'forest') OR
+     tags -> 'man_made' = 'pier' OR
      (NOT tags ? 'name' AND mz_poi_min_zoom IS NOT NULL))
     AND (
       COALESCE(mz_poi_min_zoom, 999) <> COALESCE(mz_calculate_min_zoom_pois(p.*), 999) OR
