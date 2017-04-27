541d902ae0d980e923c850a78a316c2d1f3b9645
Nathaniel Kelso <nvkelso@gmail.com>
2017-04-26 19:35:03 -0700

    [migration] viewpoint

--- data/migrations/v1.3.0-point.sql
+++ data/migrations/v1.3.0-point.sql
@@ -5,5 +5,6 @@ UPDATE
     (tags -> 'leisure' = 'garden' OR
      tags -> 'landuse' = 'village_green' OR
      tags -> 'natural' IN ('wood', 'forest') OR
+     tags -> 'tourism' IN ('viewpoint') OR
      (NOT tags ? 'name' AND mz_poi_min_zoom IS NOT NULL))
     AND COALESCE(mz_poi_min_zoom, 999) <> COALESCE(mz_calculate_min_zoom_pois(p.*), 999);
