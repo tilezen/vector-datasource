0fffea0dd559956a3d025a57e062edb4ecbdb6f9
Robert Marianski <rob@marianski.com>
2017-05-11 10:10:34 -0400

    Don't assume root of repo as working dir

--- scripts/update-integration-test-coordinates.sh
+++ scripts/update-integration-test-coordinates.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-python integration-test.py -printcoords | python scripts/update-integration-test-coordinates.py
+
+basedir="$(dirname ${BASH_SOURCE[0]})/.."
+
+python ${basedir}/integration-test.py -printcoords | python ${basedir}/scripts/update-integration-test-coordinates.py
