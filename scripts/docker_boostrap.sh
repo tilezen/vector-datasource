07aa44d205f1a9597670ee9015379aebc1c3fb58
Ian Dees <ian.dees@gmail.com>
2017-05-24 14:24:16 -0400

    Switch to using the Havana extract because its smaller

--- scripts/docker_boostrap.sh
+++ scripts/docker_boostrap.sh
@@ -1,35 +1,38 @@
 set -e
-export
+set -x
 
-export PGPASSWORD=$POSTGRES_ENV_POSTGRES_PASSWORD
-psql -h "$POSTGRES_PORT_5432_TCP_ADDR" \
-     -p "$POSTGRES_PORT_5432_TCP_PORT" \
-     -U "$POSTGRES_ENV_POSTGRES_USER" \
-     -d "$POSTGRES_ENV_POSTGRES_DB" \
+[ -z "$POSTGRES_PASSWORD" ] && echo "Need to set POSTGRES_PASSWORD" && exit 1;
+export PGPASSWORD="$POSTGRES_PASSWORD"
+
+psql -h "${POSTGRES_HOST:-postgres}" \
+     -p "${POSTGRES_PORT:-5432}" \
+     -U "${POSTGRES_USER:-osm}" \
+     -d "${POSTGRES_DB:-osm}" \
      -c "create extension if not exists postgis; create extension if not exists hstore;"
 
-/usr/bin/wget https://s3.amazonaws.com/metro-extracts.mapzen.com/new-york_new-york.osm.pbf
+/usr/bin/wget https://s3.amazonaws.com/metro-extracts.mapzen.com/la-habana_cuba.osm.pbf
 osm2pgsql --slim \
           --cache 1024 \
           --style osm2pgsql.style \
           --hstore-all \
-          new-york_new-york.osm.pbf \
-          -H "$POSTGRES_PORT_5432_TCP_ADDR" \
-          -P "$POSTGRES_PORT_5432_TCP_PORT" \
-          -U "$POSTGRES_ENV_POSTGRES_USER" \
-          -d "$POSTGRES_ENV_POSTGRES_DB"
+          la-habana_cuba.osm.pbf \
+          -H "${POSTGRES_HOST:-postgres}" \
+          -P "${POSTGRES_PORT:-5432}" \
+          -U "${POSTGRES_USER:-osm}" \
+          -d "${POSTGRES_DB:-osm}"
+rm la-habana_cuba.osm.pbf
 
 cd data
 /usr/bin/python2.7 bootstrap.py
 /usr/bin/make -f Makefile-import-data
 ./import-shapefiles.sh | \
-    psql -h "$POSTGRES_PORT_5432_TCP_ADDR" \
-         -p "$POSTGRES_PORT_5432_TCP_PORT" \
-         -U "$POSTGRES_ENV_POSTGRES_USER" \
-         -d "$POSTGRES_ENV_POSTGRES_DB"
+    psql -h "${POSTGRES_HOST:-postgres}" \
+         -p "${POSTGRES_PORT:-5432}" \
+         -U "${POSTGRES_USER:-osm}" \
+         -d "${POSTGRES_DB:-osm}"
 ./perform-sql-updates.sh \
-    -h "$POSTGRES_PORT_5432_TCP_ADDR" \
-    -p "$POSTGRES_PORT_5432_TCP_PORT" \
-    -U "$POSTGRES_ENV_POSTGRES_USER" \
-    -d "$POSTGRES_ENV_POSTGRES_DB"
+    -h "${POSTGRES_HOST:-postgres}" \
+    -p "${POSTGRES_PORT:-5432}" \
+    -U "${POSTGRES_USER:-osm}" \
+    -d "${POSTGRES_DB:-osm}"
 cd ..
