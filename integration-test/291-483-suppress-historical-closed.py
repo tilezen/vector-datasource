2fe3405637be38e45fef634a397fc826a5c0559f
Nathaniel Kelso <nvkelso@gmail.com>
2017-06-05 13:11:46 -0700

    switch closed test from Best Buy (replaced with Target) to Cartoon Museum in SF

--- integration-test/291-483-suppress-historical-closed.py
+++ integration-test/291-483-suppress-historical-closed.py
@@ -1,17 +1,12 @@
-## Best buy (closed)
-# https://www.openstreetmap.org/way/241643507
+## Cartoon Art Museum (closed)
+# https://www.openstreetmap.org/node/368173967
 
-# building should be present at z15
-test.assert_has_feature(
-    15, 5232, 12654, 'buildings',
-    {'id': 241643507, 'kind': 'building', 'kind_detail': 'retail'})
-
-# but POI shouldn't
+# POI shouldn't be visible early
 test.assert_no_matching_feature(
-    15, 5232, 12654, 'pois',
-    {'id': 241643507})
+    15, 5242, 12664, 'pois',
+    {'id': 368173967})
 
 # but POI should be present at z17 and marked as closed
 test.assert_has_feature(
-    16, 10465, 25308, 'pois',
-    {'id': 241643507, 'kind': 'closed', 'min_zoom': 17})
+    16, 10485, 25328, 'pois',
+    {'id': 368173967, 'kind': 'closed', 'min_zoom': 17})
