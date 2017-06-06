5d66bb36bdd35038cfda920ff75b008227587b92
Robert Marianski <rob@marianski.com>
2017-06-06 16:39:03 -0400

    Inject necessary local variables into functions
    * add util functions to replace the corresponding sql
    * inject variables for way_area and volume when used
    * add basic buildings tests

--- vectordatasource/util.py
+++ vectordatasource/util.py
@@ -13,3 +13,35 @@ def to_float(x):
         return float(x)
     except ValueError:
         return None
+
+
+def calculate_way_area(shape):
+    result = 0
+    if shape.type in ('MultiPolygon', 'Polygon'):
+        result = shape.area
+    return result
+
+
+def get_height_from_props(props):
+    height_str = props.get('height')
+    if height_str:
+        height = to_float(height_str)
+        if height is not None:
+            return height
+    levels_str = props.get('levels')
+    if levels_str:
+        levels = to_float(levels_str)
+        if levels is not None:
+            result = max(levels, 1) * 3 + 2
+            return result
+    if height_str or levels_str:
+        return 1e10
+
+
+def calculate_volume(area, props):
+    result = 0
+    if area > 0:
+        height = get_height_from_props(props)
+        if height is not None and height > 0:
+            result = area * height
+    return result
