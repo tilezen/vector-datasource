90d8cdc3bb4b4fc3b158e5c486f78e86b0ad1304
Matt Amos <matt.amos@mapzen.com>
2017-08-18 20:11:16 +0100

    Ported tests 1171, 1175, 1178, 1185, 1186, 1190, 1191.

--- integration-test/1171-bicycle-yes-designated-roads.py
+++ integration-test/1171-bicycle-yes-designated-roads.py
@@ -1,13 +1,23 @@
-# Add bicycle properties to roads
+from . import OsmFixtureTest
 
-# Road with bicycle=yes in Washington, DC
-# http://www.openstreetmap.org/way/281677984
-test.assert_has_feature(
-    16, 18758, 25078, 'roads',
-    { 'id': 281677984, 'kind': 'major_road', 'is_bicycle_related': True, 'bicycle': 'yes'})
 
-# Road with bicycle=designated in Eureka, California
-# http://www.openstreetmap.org/way/10273013
-test.assert_has_feature(
-    16, 10163, 24621, 'roads',
-    { 'id': 10273013, 'kind': 'major_road', 'is_bicycle_related': True, 'bicycle': 'designated'})
+class BicycleYesDesignatedRoads(OsmFixtureTest):
+
+    def test_bicycle_yes(self):
+        # Add bicycle properties to roads
+        # Road with bicycle=yes in Washington, DC
+        self.load_fixtures(['http://www.openstreetmap.org/way/281677984'])
+
+        self.assert_has_feature(
+            16, 18758, 25078, 'roads',
+            {'id': 281677984, 'kind': 'major_road',
+             'is_bicycle_related': True, 'bicycle': 'yes'})
+
+    def test_bicycle_designated(self):
+        # Road with bicycle=designated in Eureka, California
+        self.load_fixtures(['http://www.openstreetmap.org/way/10273013'])
+
+        self.assert_has_feature(
+            16, 10163, 24621, 'roads',
+            {'id': 10273013, 'kind': 'major_road',
+             'is_bicycle_related': True, 'bicycle': 'designated'})
