9d508216fa15a91f152aa5d4e6bdb2dd31896b98
Matt Amos <matt.amos@mapzen.com>
2017-08-22 11:55:15 +0100

    Ported tests 443, 447, 448, 454, 465, 469.

--- integration-test/447-ice-cream-shops.py
+++ integration-test/447-ice-cream-shops.py
@@ -1,11 +1,19 @@
-# New York, NY (amenity=ice_cream)
-#https://www.openstreetmap.org/node/2782000317
-test.assert_has_feature(
-    16, 19299, 24629, 'pois',
-    { 'kind': 'ice_cream' })
+from . import OsmFixtureTest
 
-# Oakland, CA (shop=ice_cream)
-#https://www.openstreetmap.org/node/661742947
-test.assert_has_feature(
-    16, 10512, 25314, 'pois',
-    { 'kind': 'ice_cream' })
+
+class IceCreamShops(OsmFixtureTest):
+    def test_amenity_ice_cream(self):
+        # New York, NY (amenity=ice_cream)
+        self.load_fixtures(['https://www.openstreetmap.org/node/2782000317'])
+
+        self.assert_has_feature(
+            16, 19299, 24629, 'pois',
+            {'kind': 'ice_cream'})
+
+    def test_shop_ice_cream(self):
+        # Oakland, CA (shop=ice_cream)
+        self.load_fixtures(['https://www.openstreetmap.org/node/661742947'])
+
+        self.assert_has_feature(
+            16, 10512, 25314, 'pois',
+            {'kind': 'ice_cream'})
