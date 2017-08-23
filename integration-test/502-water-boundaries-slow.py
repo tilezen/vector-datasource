924c599431f606e14caf79c2e2457fd0dcc99b13
Matt Amos <matt.amos@mapzen.com>
2017-08-23 10:03:25 +0100

    Ported tests 502, 510, 520, 522, 523, 524.

--- integration-test/502-water-boundaries-slow.py
+++ integration-test/502-water-boundaries-slow.py
@@ -1,45 +1,62 @@
-# these are tiles which have water boundaries, but only water-to-water
-# boundaries. since we remove these, then we should have more than one water
-# polygon, but zero water boundaries actually in the tile.
-no_boundary_tiles = [
-    # https://www.openstreetmap.org/relation/275011
-    # https://www.openstreetmap.org/relation/1363854
-    [16, 23768, 33616], # River Tocanis, Brasil
-]
+from . import OsmFixtureTest
 
-# these are tiles which do have a boundary, to check that the first condition
-# isn't trivially fulfilled by having no boundaries whatsoever.
-#
-# each of these is close to the tests above, and so any data needed has already
-# been pulled in by those.
-boundary_tiles = [
-    [16, 23775, 33616], # R. Tocanis, Brasil
-]
 
-for z, x, y in no_boundary_tiles:
-    with test.features_in_tile_layer(z, x, y, 'water') as features:
-        num_polygons = 0
-        num_boundaries = 0
+class WaterBoundariesSlow(OsmFixtureTest):
 
-        for f in features:
-            geom_type = f['geometry']['type']
-            boundary = f['properties'].get('boundary', False)
+    def test_boundaries(self):
+        from shapely.ops import unary_union
 
-            if geom_type in ['Polygon', 'MultiPolygon']:
-                num_polygons += 1
+        # River Tocanis, Brasil
 
-            elif boundary == True:
-                num_boundaries += 1
+        # these are tiles which have water boundaries, but only
+        # water-to-water boundaries. since we remove these, then we should
+        # have more than one water polygon, but zero water boundaries
+        # actually in the tile.
+        no_boundary_tiles = [
+            [16, 23768, 33616],
+        ]
 
-        if num_polygons < 2:
-            test.fail('Expected at least 2 polygons in water boundary '
-                 'test tile, but found only %d' % num_polygons)
+        # these are tiles which do have a boundary, to check that the first
+        # condition isn't trivially fulfilled by having no boundaries
+        # whatsoever.
+        boundary_tiles = [
+            [16, 23775, 33616],
+        ]
 
-        if num_boundaries > 0:
-            test.fail('Expected an all-water tile with no land '
-                 'boundaries, but found %d boundaries.' % num_boundaries)
+        all_tiles = no_boundary_tiles + boundary_tiles
+        all_boxes = [self.tile_bbox(*t, padding=2) for t in all_tiles]
 
-for z, x, y in boundary_tiles:
-    test.assert_has_feature(
-        z, x, y, 'water',
-        {'boundary': True})
+        self.load_fixtures([
+            'https://www.openstreetmap.org/relation/275011',
+            'https://www.openstreetmap.org/relation/1363854',
+        ], clip=unary_union(all_boxes))
+
+        for z, x, y in no_boundary_tiles:
+            with self.features_in_tile_layer(z, x, y, 'water') as features:
+                num_polygons = 0
+                num_boundaries = 0
+
+                for f in features:
+                    geom_type = f['geometry']['type']
+                    boundary = f['properties'].get('boundary', False)
+
+                    if geom_type in ['Polygon', 'MultiPolygon']:
+                        num_polygons += 1
+
+                    elif boundary == True:
+                        num_boundaries += 1
+
+                self.assertFalse(
+                    num_polygons < 2,
+                    'Expected at least 2 polygons in water boundary test '
+                    'tile, but found only %d' % num_polygons)
+
+                self.assertFalse(
+                    num_boundaries > 0,
+                    'Expected an all-water tile with no land boundaries, '
+                    'but found %d boundaries.' % num_boundaries)
+
+        for z, x, y in boundary_tiles:
+            self.assert_has_feature(
+                z, x, y, 'water',
+                {'boundary': True})
--- integration-test/522-hotels.py
+++ integration-test/522-hotels.py
@@ -1,9 +1,17 @@
-#https://www.openstreetmap.org/way/32947245
-test.assert_has_feature(
-    15, 5242, 12663, 'pois',
-    { 'kind': 'hotel', 'name': 'Ritz-Carlton' })
+from . import OsmFixtureTest
 
-#http://www.openstreetmap.org/relation/1358120
-test.assert_has_feature(
-    16, 10483, 25323, 'pois',
-    { 'kind': 'hotel', 'name': 'Hotel Zephyr' })
+
+class Hotels(OsmFixtureTest):
+    def test_ritz_carlton(self):
+        self.load_fixtures(['https://www.openstreetmap.org/way/32947245'])
+
+        self.assert_has_feature(
+            15, 5242, 12663, 'pois',
+            {'kind': 'hotel', 'name': 'Ritz-Carlton'})
+
+    def test_zephyr(self):
+        self.load_fixtures(['http://www.openstreetmap.org/relation/1358120'])
+
+        self.assert_has_feature(
+            16, 10483, 25323, 'pois',
+            {'kind': 'hotel', 'name': 'Hotel Zephyr'})
