7cc4d1b64a068dc1c19ee2bf05d891caf0c16940
Matt Amos <matt.amos@mapzen.com>
2017-12-05 17:35:02 +0000

    Add test for #1226.
    This checks that buildings which cross tile boundaries are more likely to be assigned the correct landuse kind. The issue was that, before the change from query-per-layer to query-per-table, we were clipping the landuse to the tile but the building to a 3x3 tile area. This meant that large buildings had much less overlap with landuse polygons, and some would not be assigned a `landuse_kind`.
    
    Now we clip all inputs (except water) to the tile, which solves this problem, but means that the same building in adjacent tiles could be assigned different `landuse_kind`s.
    
    Additionally, it looks like `kind: building_part`s weren't being assigned a `landuse_kind`, although the test now passes, so something we changed has fixed that too.

---
+++ integration-test/1226-landuse-kind-buildings.py
@@ -0,0 +1,73 @@
+from . import FixtureTest
+
+
+class LanduseKindBuildings(FixtureTest):
+    def test_lax_airport_terminals(self):
+        self.load_fixtures([
+            # Terminal 2, which had landuse_kind set correctly at z14
+            'https://www.openstreetmap.org/way/413226245',
+
+            # Tom Bradley International Terminal was missing landuse_kind
+            'https://www.openstreetmap.org/relation/6168071',
+
+            # the aerodrome landuse polygon
+            'https://www.openstreetmap.org/way/131190417',
+        ])
+
+        # the tile which contains Terminal 2 should set the landuse_kind
+        # correctly.
+        self.assert_has_feature(
+            14, 2803, 6547, 'buildings',
+            {'id': 413226245, 'kind': 'building', 'landuse_kind': 'aerodrome'})
+
+        # the four tiles containing bits of Tom Bradley International should
+        # do so also.
+        for x in (2802, 2803):
+            for y in (6547, 6548):
+                self.assert_has_feature(
+                    14, x, y, 'buildings',
+                    {'id': -6168071, 'kind': 'building',
+                     'landuse_kind': 'aerodrome'})
+
+    def test_lax_united_airlines_building(self):
+        self.load_fixtures([
+            # United Airlines building spans two tiles at z16, and neither(?)
+            # of them were getting a landuse_kind assigned.
+            'https://www.openstreetmap.org/relation/6168075',
+
+            # the aerodrome landuse polygon
+            'https://www.openstreetmap.org/way/131190417',
+        ])
+
+        for x in (11209, 11210):
+            self.assert_has_feature(
+                16, x, 26192, 'buildings',
+                {'id': -6168075, 'kind': 'building',
+                 'landuse_kind': 'aerodrome'})
+
+    def test_building_part(self):
+        import dsl
+        from tilequeue.tile import coord_to_bounds
+        from shapely.geometry import box
+        from ModestMaps.Core import Coordinate
+
+        z, x, y = (16, 0, 0)
+        bounds = coord_to_bounds(Coordinate(zoom=z, column=x, row=y))
+        shape = box(*bounds)
+
+        self.generate_fixtures(
+            dsl.way(1, shape,
+                    {'landuse': 'park', 'source': 'openstreetmap.org'}),
+            dsl.way(2, shape,
+                    {'building': 'yes', 'source': 'openstreetmap.org'}),
+            dsl.way(3, shape,
+                    {'building:part': 'yes', 'source': 'openstreetmap.org'}),
+        )
+
+        self.assert_has_feature(
+            z, x, y, 'buildings',
+            {'id': 2, 'kind': 'building', 'landuse_kind': 'park'})
+        import pdb; pdb.set_trace()
+        self.assert_has_feature(
+            z, x, y, 'buildings',
+            {'id': 3, 'kind': 'building_part', 'landuse_kind': 'park'})
