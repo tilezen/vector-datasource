f73db878292dccdf24908536bffaf4e414dfb348
Matt Amos <matt.amos@mapzen.com>
2017-12-06 17:25:44 +0000

    Allow some kinds of non-numeric `shield_text`.
    This normalises the formatting of road `ref`s which consist of a single letter, optional space and then a number. These are often displayed with the letter as part of the signage.
    
    Also back-fill network values where they can be inferred from the operator.

--- integration-test/1211-fix-null-network.py
+++ integration-test/1211-fix-null-network.py
@@ -5,11 +5,11 @@ class FixNullNetwork(FixtureTest):
     def test_routes_with_no_network(self):
         # ref="N 4", route=road, but no network=*
         # so we should get something that has no network, but a shield text of
-        # '4'
+        # 'N4' (see #1062 regarding why it's 'N4' rather than '4').
         self.load_fixtures(
             ['http://www.openstreetmap.org/relation/2307408'],
             clip=self.tile_bbox(11, 1038, 705))
 
         self.assert_has_feature(
             11, 1038, 705, 'roads',
-            {'kind': 'major_road', 'shield_text': '4', 'network': type(None)})
+            {'kind': 'major_road', 'shield_text': 'N4', 'network': type(None)})
