601690cf566a18a3e7134aae36e081e6dbbb9571
Nathaniel Kelso <nvkelso@gmail.com>
2018-02-04 21:17:46 -0800

    [tests] line and point PONI cranes

---
+++ integration-test/1417-cranes.py
@@ -0,0 +1,18 @@
+from . import FixtureTest
+
+
+class TestCranes(FixtureTest):
+
+    def test_crane_landuse_line(self):
+        self.load_fixtures(['http://www.openstreetmap.org/way/173458931'])
+
+        self.assert_has_feature(
+            14, 2621, 6338, 'landuse',
+            {'id': 173458931, 'kind': 'crane', 'min_zoom': 16, 'sort_rank': 272})
+
+    def test_crane_pois(self):
+        self.load_fixtures(['http://www.openstreetmap.org/node/1842715058'])
+
+        self.assert_has_feature(
+            16, 32891, 21813, 'landuse',
+            {'id': 1842715058, 'kind': 'crane', 'min_zoom': 16})
