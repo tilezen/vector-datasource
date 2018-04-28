31d4ee3cc2bb7bf40535da6fd682b0fa08b320e1
Nathaniel V. KELSO <nvkelso@gmail.com>
2018-04-27 18:15:33 -0700

    http://pep8online.com/checkresult

--- integration-test/1483-e-road-sorting.py
+++ integration-test/1483-e-road-sorting.py
@@ -5,10 +5,11 @@ from . import FixtureTest
 class ERoadSortTest(FixtureTest):
     def test_eroad_a90_e90(self):
         # Sort A-road first, then e-road
-        self.load_fixtures(['https://www.openstreetmap.org/way/188060822',
-           'https://www.openstreetmap.org/relation/2000662',
-           'https://www.openstreetmap.org/relation/2870369',
-           'https://www.openstreetmap.org/relation/2084465'])
+        self.load_fixtures(
+            ['https://www.openstreetmap.org/way/188060822',
+             'https://www.openstreetmap.org/relation/2000662',
+             'https://www.openstreetmap.org/relation/2870369',
+             'https://www.openstreetmap.org/relation/2084465'])
 
         # We could test on GRA shield_text but there's an unrelated bug
         # We could test to ensure e-road network in all_networks
