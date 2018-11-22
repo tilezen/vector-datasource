43002fddbc6f2f09da110b36997a97a33ee6fafb
Matt Amos <zerebubuth@gmail.com>
2018-11-22 13:13:25 +0000

    Drop many "detail" tags on minor roads at mid zooms.
    On minor roads (only - this doesn't apply to major roads or highways), drop the following from zoom 15 down:
    
    * `colour`
    * `motor_vehicle`
    * `operator`
    * `route`
    * `route_name`
    * `state`
    * `surface`
    * `symbol`
    * `type`
    
    And the following from zoom 13 down:
    
    * `cutting`
    * `embankment`
    
    This helps a lot with road merging, as the fewer distinct combinations of road properties there are, the more roads can be merged into one another.
    
    Also, drop most values of the `access` property from zoom 13 down, keeping `access=no`, `access=private` and `access=official`. The `no` and `private` values are important for knowing which streets are inaccessible, but many other distinctions are unimportant at this zoom level, where streets might be a few pixels long, e.g: `access=customers` (if styled at all) can be made prominent as the view is zoomed in a bit more.
    
    Further, we merge some `access` values together; `emergency`, `military`, `restricted` and `forestry` all map to `access=official`, which indicates that there is some kind of restriction, but allows for geometry merging and value compression through re-use.
    
    These measures together produced a 14% reduction in the number of `minor_road` features on `12/1206/1540`, reflected in a similar drop in the size of the metadata and properties for the MVT tile. Overall, the `roads` layer was 4% smaller (about 3kB for this tile).

--- vectordatasource/transform.py
+++ vectordatasource/transform.py
@@ -8145,3 +8145,46 @@ def tags_set_ne_min_max_zoom(ctx):
             props['max_zoom'] = max_zoom
 
     return None
+
+
+def whitelist(ctx):
+    """
+    Applies a whitelist to a particular property on all features in the layer,
+    optionally also remapping some values.
+    """
+
+    params = _Params(ctx, 'whitelist')
+    layer_name = params.required('layer')
+    start_zoom = params.optional('start_zoom', default=0, typ=int)
+    end_zoom = params.optional('end_zoom', typ=int)
+    property_name = params.required('property')
+    whitelist = params.required('whitelist', typ=list)
+    remap = params.optional('remap', default={}, typ=dict)
+
+    # check that we're in the zoom range where this post-processor is supposed
+    # to operate.
+    if ctx.nominal_zoom < start_zoom:
+        return None
+    if end_zoom is not None and ctx.nominal_zoom >= end_zoom:
+        return None
+
+    layer = _find_layer(ctx.feature_layers, layer_name)
+
+    features = layer['features']
+    for feature in features:
+        _, props, _ = feature
+        value = props.get(property_name)
+        if value is not None:
+            if value in whitelist:
+                # leave value as-is
+                continue
+
+            elif value in remap:
+                # replace with replacement value
+                props[property_name] = remap[value]
+
+            else:
+                # drop the property
+                props.pop(property_name)
+
+    return None
