54062a3abdc1516fe854a7b3b8ab86e74364f8cf
Nathaniel V. KELSO <nvkelso@gmail.com>
2018-12-14 00:41:34 -0800

    update test for min_zoom

--- integration-test/1695-turning-circles-turning-loops.py
+++ integration-test/1695-turning-circles-turning-loops.py
@@ -14,14 +14,14 @@ class TurningCirclesAndLoops(FixtureTest):
 
         self.assert_has_feature(
             16, 32703, 21771, 'pois',
-            {'id': 106186562, 'kind': 'turning_circle'})
+            {'id': 106186562, 'kind': 'turning_circle', 'min_zoom': 17})
 
     def turning_loop(self):
         self.generate_fixtures(
             dsl.point(4260010359, (8.43452, 49.4596352),
                       {u'source': u'openstreetmap.org',
-                       u'highway': u'turning_loop'}))
+                       u'highway': u'turning_loop'})
 
         self.assert_has_feature(
             16, 34303, 22378, 'pois',
-            {'id': 4260010359, 'kind': 'turning_loop'})
+            {'id': 4260010359, 'kind': 'turning_loop', 'min_zoom': 17})
