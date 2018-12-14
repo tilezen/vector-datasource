e89527a37ab95e5f3872ac0d0865da9f7149e451
Nathaniel V. KELSO <nvkelso@gmail.com>
2018-12-14 08:45:50 -0800

    import math functions

--- vectordatasource/transform.py
+++ vectordatasource/transform.py
@@ -2,6 +2,8 @@
 # transformation functions to apply to features
 
 from collections import defaultdict, namedtuple
+from math import ceil
+from math import trunc
 from numbers import Number
 from shapely.geometry.collection import GeometryCollection
 from shapely.geometry import box as Box
