6a5a8e7d0a22cee22c553c71e5256ee1e74bfa89
Matt Amos <zerebubuth@gmail.com>
2019-01-22 10:06:26 +0000

    Start natural woods at zoom 9.

--- integration-test/1794-demote-early-landcover.py
+++ integration-test/1794-demote-early-landcover.py
@@ -25,3 +25,75 @@ class DemoteEarlyLandcover(FixtureTest):
                 'kind': 'forest',
                 'min_zoom': 9,
             })
+
+    def test_natural_wood_usfs_1(self):
+        # previously, being operator=USFS had meant a zoom bump, but no longer.
+        import dsl
+
+        z, x, y = (14, 2733, 6235)
+
+        self.generate_fixtures(
+            # https://www.openstreetmap.org/relation/7339333
+            dsl.way(7339333, dsl.box_area(z, x, y, 16961064), {
+                'access': 'yes',
+                'natural': 'wood',
+                'operator': 'United States Forest Service',
+                'source': 'openstreetmap.org',
+                'type': 'multipolygon',
+            }),
+        )
+
+        self.assert_has_feature(
+            z, x, y, 'landuse', {
+                'id': 7339333,
+                'kind': 'natural_wood',
+                'min_zoom': 9,
+            })
+
+    def test_natural_wood_usfs_2(self):
+        # previously, being operator=USFS had meant a zoom bump, but no longer.
+        import dsl
+
+        z, x, y = (14, 2734, 6245)
+
+        self.generate_fixtures(
+            # https://www.openstreetmap.org/relation/7352119
+            dsl.way(7352119, dsl.box_area(z, x, y, 19525176), {
+                'access': 'yes',
+                'natural': 'wood',
+                'operator': 'United States Forest Service',
+                'source': 'openstreetmap.org',
+                'type': 'multipolygon',
+            }),
+        )
+
+        self.assert_has_feature(
+            z, x, y, 'landuse', {
+                'id': 7352119,
+                'kind': 'natural_wood',
+                'min_zoom': 9,
+            })
+
+    def test_natural_wood(self):
+        import dsl
+
+        z, x, y = (14, 11782, 7469)
+
+        self.generate_fixtures(
+            # https://www.openstreetmap.org/relation/5617154
+            dsl.way(5617154, dsl.box_area(z, x, y, 9186148530), {
+                'name': 'Nallamala Forest',
+                'natural': 'wood',
+                'source': 'openstreetmap.org',
+                'type': 'multipolygon',
+                'wikidata': 'Q1579454',
+                'wikipedia': 'en:Nallamala Hills',
+            }),
+        )
+
+        self.assert_has_feature(
+            z, x, y, 'landuse', {
+                'id': 5617154,
+                'kind': 'natural_wood',
+                'min_zoom': 9,
+            })
