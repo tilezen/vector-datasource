1718e30d46d83576da86fa78497d94211934cead
Matt Amos <zerebubuth@gmail.com>
2019-06-17 11:43:58 +0100

    Add dilate / buffer into water and clip against water layer. This means that landcover should match up (modulo simplification) with the water layer and not have some landcover overlap into the water - or massive gaps between the landcover and water.
    Add modal pixel filter, which replaces the central pixel of a kernel with the most common pixel over that kernel. This consolidates blocks of pixels and reduces noise in the data, as the landcover raster can look a little like it has been dithered, with a lot of single pixels.
    
    Add a `GDALSieveFilter` pass to remove small blobs. This is necessary because the modal filter can still result in single, stand-alone, pixels and these don't look good when polygonized and put into the tiles. However, the sieve filter alone (without the modal filter) also doesn't look good, as it doesn't consolidate blocks of pixels, leading to odd artefacts where there's a lot of "dithering" (i.e: are these light green pixels on a dark green background, or dark green pixels on a light green background?).

--- vectordatasource/landcover.py
+++ vectordatasource/landcover.py
@@ -2,8 +2,10 @@ from vectordatasource.transform import _Params
 from vectordatasource.transform import _find_layer
 from tilequeue.tile import mercator_point_to_coord
 from tilequeue.tile import coord_to_mercator_point
+from tilequeue.tile import calc_meters_per_pixel_dim
 from ModestMaps.Core import Coordinate
 from os.path import join as path_join
+from collections import defaultdict
 
 
 class _UrlPattern(object):
@@ -28,6 +30,64 @@ def _geotransform(topleft, bottomright, width, height):
             maxy, 0, (miny - maxy) / height)
 
 
+def _memoize(f):
+    result = {}
+
+    def wrapped(*args, **kwargs):
+        cache_key = tuple(args)
+        if cache_key not in result:
+            result[cache_key] = f(*args, **kwargs)
+        return result[cache_key]
+
+    return wrapped
+
+
+def _polar(xy):
+    from math import atan2
+    x, y = xy
+    return (x**2 + y**2, atan2(y, x))
+
+
+@_memoize
+def _neighbourhood(radius):
+    """
+    Return a list of (dx, dy) offsets which make up a spiral search of the
+    given radius around a point.
+    """
+
+    r_squared = radius**2
+
+    pts = []
+    for x in xrange(-radius, radius + 1):
+        for y in xrange(-radius, radius + 1):
+            # we want to keep only points within the given distance of the
+            # central point.
+            if x**2 + y**2 <= r_squared:
+                pts.append((x, y))
+
+    # sort the points so that we examine nearby ones first.
+    pts = sorted(pts, key=_polar)
+
+    # discard first point, which will be the offset (0, 0), which is the
+    # original point and therefore not in the neighbourhood of the original
+    # point.
+    return pts[1:]
+
+
+def _get_tile(session, url_pattern, zoom, x, y, retries=3):
+    coord = Coordinate(zoom=zoom, column=x, row=y)
+    url = url_pattern(coord)
+
+    count = 0
+    while count < retries:
+        response = session.get(url)
+        if response.status_code == 200:
+            return response
+        count += 1
+    raise Exception("Failed to fetch tile %r: status = %r after %d retries" %
+                    (url, response.status_code, count))
+
+
 def _download_raster_png(bounds, zoom, url_pattern, output_file,
                          median_filter_size=3, cache=None):
     """
@@ -40,18 +100,22 @@ def _download_raster_png(bounds, zoom, url_pattern, output_file,
     from cachecontrol import CacheControl
     from cachecontrol.caches.file_cache import FileCache
     from PIL import Image
-    from PIL import ImageFilter
     from cStringIO import StringIO
 
     url = _UrlPattern(url_pattern)
     tile_size = 256
+    water_search_radius = 2 * median_filter_size
 
     session = requests.Session()
     if cache:
         session = CacheControl(session, cache=FileCache(cache))
 
-    topleft = mercator_point_to_coord(zoom, bounds[0], bounds[3])
-    bottomright = mercator_point_to_coord(zoom, bounds[2], bounds[1])
+    meters_per_px = calc_meters_per_pixel_dim(zoom)
+    margin = meters_per_px * median_filter_size
+    topleft = mercator_point_to_coord(
+        zoom, bounds[0] - margin, bounds[3] + margin)
+    bottomright = mercator_point_to_coord(
+        zoom, bounds[2] + margin, bounds[1] - margin)
 
     minx = int(topleft.column)
     miny = int(topleft.row)
@@ -61,34 +125,61 @@ def _download_raster_png(bounds, zoom, url_pattern, output_file,
     width = tile_size * (maxx - minx + 1)
     height = tile_size * (maxy - miny + 1)
 
-    if topleft == bottomright:
-        response = session.get(url(topleft))
-        assert response.status_code == 200
-        io = StringIO(response.content)
-        im = Image.open(io)
-        assert im.mode == 'P'
+    im = Image.new('RGBA', (width, height))
 
-    else:
-        im = Image.new('RGBA', (width, height))
+    for x in xrange(minx, maxx + 1):
+        for y in xrange(miny, maxy + 1):
+            response = _get_tile(session, url, zoom, x, y, retries=3)
 
-        for x in xrange(minx, maxx + 1):
-            for y in xrange(miny, maxy + 1):
-                coord = Coordinate(zoom=zoom, column=x, row=y)
-                response = session.get(url(coord))
-                assert response.status_code == 200
+            io = StringIO(response.content)
+            tile = Image.open(io)
 
-                io = StringIO(response.content)
-                tile = Image.open(io)
+            assert tile.mode == 'P'
+            assert tile.size == (tile_size, tile_size)
+            dx = x - minx
+            dy = y - miny
+            im.paste(tile, (dx * tile_size, dy * tile_size))
 
-                assert tile.mode == 'P'
-                assert tile.size == (tile_size, tile_size)
-                dx = x - minx
-                dy = y - miny
-                im.paste(tile, (dx * tile_size, dy * tile_size))
+    # TODO: configurable
+    water = (69, 128, 162, 255)
 
-        im = im.quantize()
+    im2 = Image.new('RGBA', (width, height))
+    for x in xrange(median_filter_size, width - median_filter_size):
+        for y in xrange(median_filter_size, height - median_filter_size):
+            mid = im.getpixel((x, y))
 
-    im.save(output_file)
+            if mid == water:
+                # for water, find the nearest non-water colour and use that
+                for dx, dy in _neighbourhood(water_search_radius):
+                    if 0 <= (x + dx) < width and 0 <= (y + dy) < height:
+                        pt = im.getpixel((x + dx, y + dy))
+                        if pt != water:
+                            mid = pt
+                            break
+
+            else:
+                # for non-water, do a modal filter (most common colour in
+                # neighbourhood which isn't water)
+                counts = defaultdict(lambda: 0)
+                counts[mid] += 1
+                for dx, dy in _neighbourhood(median_filter_size):
+                    pt = im.getpixel((x + dx, y + dy))
+                    if pt != water:
+                        counts[pt] += 1
+                max_count = 0
+                max_colour = None
+                for colour, count in counts.iteritems():
+                    if count > max_count or \
+                       (count == max_count and colour > max_colour):
+                        max_count = count
+                        max_colour = colour
+                assert max_colour is not None
+                mid = max_colour
+
+            im2.putpixel((x, y), mid)
+
+    im2 = im2.quantize()
+    im2.save(output_file)
 
     return _geotransform(topleft, bottomright, width, height)
 
@@ -120,9 +211,21 @@ def inject(ctx):
     kinds_mapping = params.required('kinds', typ=dict)
     cache = params.optional('cache')
     median_filter_size = params.optional('median-size', default=3, typ=int)
+    clipping_layer_name = params.optional('clipping-layer', default='water')
+    area_in_pixels = params.optional('area-in-pixels', typ=int)
+
+    # sensible default?
+    if area_in_pixels is None:
+        area_in_pixels = 4 * median_filter_size ** 2
 
     layer = _find_layer(ctx.feature_layers, layer_name)
     features = layer['features']
+
+    clipping_layer = _find_layer(ctx.feature_layers, clipping_layer_name)
+    clipping_shapes = []
+    for shape, props, fid in clipping_layer['features']:
+        if shape.geom_type in ('Polygon', 'MultiPolygon'):
+            clipping_shapes.append(shape)
 
     with _tempdir() as tmp:
         output_png = path_join(tmp, 'landcover.png')
@@ -139,6 +242,13 @@ def inject(ctx):
         ds.SetGeoTransform(geotransform)
         band = ds.GetRasterBand(1)
 
+        mem_drv = gdal.GetDriverByName('MEM')
+        mem_ds = mem_drv.Create('', band.XSize, band.YSize, 1, gdal.GDT_Byte)
+        mem_ds.SetProjection(srs.ExportToWkt())
+        mem_ds.SetGeoTransform(geotransform)
+        dst_band = ds.GetRasterBand(1)
+        gdal.SieveFilter(band, None, dst_band, area_in_pixels, 4)
+
         ogr_layername = 'layer'
         drv = ogr.GetDriverByName('memory')
         dst_ds = drv.CreateDataSource('')
@@ -147,7 +257,7 @@ def inject(ctx):
         field = ogr.FieldDefn('Red', ogr.OFTInteger)
         ogr_layer.CreateField(field)
 
-        gdal.Polygonize(band, None, ogr_layer, 0, [], None)
+        gdal.Polygonize(dst_band, None, ogr_layer, 0, [], None)
 
         palette = band.GetColorTable()
         for feature in ogr_layer:
@@ -160,10 +270,23 @@ def inject(ctx):
                 shape = wkb_loads(ogr_geom.ExportToWkb())
 
                 props = {'kind': 'landcover', 'kind_detail': kind_detail}
-                features.append((shape, props, None))
+                for clipping_shape in clipping_shapes:
+                    shape = shape.difference(clipping_shape)
+
+                if not shape.is_empty:
+                    # try the old "make valid" trick, as some of the polygons
+                    # coming from GDAL aren't valid (?!)
+                    if not shape.is_valid:
+                        shape = shape.buffer(0)
+
+                    if not shape.is_valid:
+                        import pdb; pdb.set_trace()
+                    assert shape.is_valid
+                    features.append((shape, props, None))
 
         # otherwise GDAL won't free any resources!
         ds = None
         dst_ds = None
+        mem_ds = None
 
     return layer
