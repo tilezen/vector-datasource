900f59d4f5703c70992ed4ae0a881ba7906e3d99
Vectura Games Maps <maps@vecturagames.com>
2019-07-29 15:43:10 +0200

    Revise conversion from cardinal to degrees for pois direction property

--- vectordatasource/transform.py
+++ vectordatasource/transform.py
@@ -129,10 +129,12 @@ def _to_int_degrees(x):
     # trim whitespace to simplify further matching
     x = x.strip()
     
-    cardinals = { 'N'   : 0,   'NNE' : 22,  'NE'  : 45,  'ENE' : 67,  
-                  'E'   : 90,  'ESE' : 112, 'SE'  : 135, 'SSE' : 157, 
-                  'S'   : 180, 'SSW' : 202, 'SW'  : 225, 'WSW' : 247, 
-                  'W'   : 270, 'WNW' : 292, 'NW'  : 315, 'NNW' : 337  }
+    cardinals = { 
+        'north' : 0  , 'N' : 0  , 'NNE' : 22 , 'NE' : 45 , 'ENE' : 67 , 
+        'east'  : 90 , 'E' : 90 , 'ESE' : 112, 'SE' : 135, 'SSE' : 157, 
+        'south' : 180, 'S' : 180, 'SSW' : 202, 'SW' : 225, 'WSW' : 247, 
+        'west'  : 270, 'W' : 270, 'WNW' : 292, 'NW' : 315, 'NNW' : 337  
+    }
     
     return cardinals[x]
 
