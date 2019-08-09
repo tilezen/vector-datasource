2dbf5f23dc2a8eb1ccf92459793171325d3d0ac3
Nathaniel V. KELSO <nvkelso@gmail.com>
2019-08-09 16:26:43 -0700

    flake8 and negative tests

--- integration-test/598-poi-direction-degree-angles.py
+++ integration-test/598-poi-direction-degree-angles.py
@@ -1,7 +1,3 @@
-from . import FixtureTest
-
-
-
 # -*- encoding: utf-8 -*-
 from . import FixtureTest
 
@@ -28,7 +24,7 @@ class AddDirectionToPOIs(FixtureTest):
                 'kind': u'viewpoint',
                 'direction': 315,
             })
-            
+
     def test_direction_as_int(self):
         import dsl
 
@@ -47,4 +43,56 @@ class AddDirectionToPOIs(FixtureTest):
                 'id': 3109053718,
                 'kind': u'viewpoint',
                 'direction': 270,
+            })
+
+    def test_direction_as_cardinal_abbreviation_bad(self):
+        import dsl
+
+        z, x, y = (16, 10482, 25333)
+
+        self.generate_fixtures(
+            # https://www.openstreetmap.org/node/665689214
+            dsl.point(665689214, (-122.4195493, 37.7653381), {
+                'tourism': u'viewpoint',
+                'direction': u'ZZZ',
+            }),
+        )
+
+        self.assert_has_feature(
+            z, x, y, 'pois', {
+                'id': 665689214,
+                'kind': u'viewpoint',
+            })
+
+        self.assert_no_matching_feature(
+            z, x, y, 'pois', {
+                'id': 665689214,
+                'kind': u'viewpoint',
+                'direction': u'ZZZ',
+            })
+
+    def test_direction_as_int_out_of_range(self):
+        import dsl
+
+        z, x, y = (16, 10482, 25333)
+
+        self.generate_fixtures(
+            # https://www.openstreetmap.org/node/3109053718
+            dsl.point(3109053718, (-122.4195493, 37.7653381), {
+                'tourism': u'viewpoint',
+                'direction': u'500',
+            }),
+        )
+
+        self.assert_has_feature(
+            z, x, y, 'pois', {
+                'id': 3109053718,
+                'kind': u'viewpoint',
+            })
+
+        self.assert_no_matching_feature(
+            z, x, y, 'pois', {
+                'id': 3109053718,
+                'kind': u'viewpoint',
+                'direction': 500,
             })
