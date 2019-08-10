f021a6698ac6961103946238002092ca1cc4e1a5
Nathaniel V. KELSO <nvkelso@gmail.com>
2019-08-09 17:03:19 -0700

    gaurd against semicolon and dash form of OSM values

--- integration-test/598-poi-direction-degree-angles.py
+++ integration-test/598-poi-direction-degree-angles.py
@@ -111,3 +111,64 @@ class AddDirectionToPOIs(FixtureTest):
                 'kind': u'viewpoint',
                 'direction': 350,
             })
+
+
+    def test_direction_as_int_out_of_range_semicolon(self):
+        import dsl
+
+        z, x, y = (16, 10482, 25333)
+
+        self.generate_fixtures(
+            # https://www.openstreetmap.org/node/3109053718
+            dsl.point(3109053718, (-122.4195493, 37.7653381), {
+                'tourism': u'viewpoint',
+                'direction': u'80;260',
+            }),
+        )
+
+        self.assert_has_feature(
+            z, x, y, 'pois', {
+                'id': 3109053718,
+                'kind': u'viewpoint',
+            })
+
+        self.assert_no_matching_feature(
+            z, x, y, 'pois', {
+                'id': 3109053718,
+                'kind': u'viewpoint',
+                'direction': u'80;260',
+            })
+
+
+    def test_direction_as_int_out_of_range_dash(self):
+        import dsl
+
+        z, x, y = (16, 10482, 25333)
+
+        self.generate_fixtures(
+            # https://www.openstreetmap.org/node/3109053718
+            dsl.point(3109053718, (-122.4195493, 37.7653381), {
+                'tourism': u'viewpoint',
+                'direction': u'45-225',
+            }),
+        )
+
+        self.assert_has_feature(
+            z, x, y, 'pois', {
+                'id': 3109053718,
+                'kind': u'viewpoint',
+            })
+
+        self.assert_no_matching_feature(
+            z, x, y, 'pois', {
+                'id': 3109053718,
+                'kind': u'viewpoint',
+                'direction': u'45-225',
+            })
+
+        self.assert_no_matching_feature(
+            z, x, y, 'pois', {
+                'id': 3109053718,
+                'kind': u'viewpoint',
+                'direction': 180,
+            })
