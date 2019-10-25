d7c9aca63b4a625e1e764f6885f857791d287275
Tomas Vajda <tomas.vajda@vecturagames.com>
2019-10-25 09:13:04 +0200

    Encode as utf-8

--- integration-test/1588-quay-wharf-other-sort-rank.py
+++ integration-test/1588-quay-wharf-other-sort-rank.py
@@ -34,7 +34,7 @@ class FarmlandCropTest(FixtureTest):
             # https://www.openstreetmap.org/way/145660534
             dsl.way(145660534, dsl.tile_box(z, x, y), {
                 'landuse': u'farmland',
-                'crop': u'cana-de-a\u00e7\u00facar',
+                'crop': u'cana-de-a\u00e7\u00facar'.encode('utf-8'),
                 'source': u'openstreetmap.org',
             }),
         )
