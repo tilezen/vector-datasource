2da724b9687ab83d25ecb8c0e8f9780ed8b374e7
Ian Dees <ian.dees@gmail.com>
2021-09-02 15:48:43 -0500

    Add integration test for big parking lot capacity

---
+++ integration-test/1972-big-parking-lots.py
@@ -0,0 +1,32 @@
+# -*- encoding: utf-8 -*-
+import dsl
+from shapely.wkt import loads as wkt_loads
+from tilequeue.tile import deg2num
+from . import FixtureTest
+
+
+def _tile_centre(z, x, y):
+    from tilequeue.tile import num2deg
+    lat, lon = num2deg(x + 0.5, y + 0.5, z)
+    return (lon, lat)
+
+
+class BigParkingLotsTest(FixtureTest):
+
+    def test_osm_big_parking_capacity(self):
+        lon, lat = (8.9140471, 48.8352716)
+        self.generate_fixtures(
+            dsl.point(8712714905, (lon, lat), {
+                u"amenity": u"parking",
+                u"capacity": u"6472217472217",
+                u"parking": u"surface",
+                u"source": u"openstreetmap.org",
+            })
+        )
+
+        x, y = deg2num(lat, lon, 15)
+        self.assert_no_matching_feature(
+            15, x, y, 'places', {
+                'id': 8712714905,
+                'source': u'openstreetmap.org',
+            })
