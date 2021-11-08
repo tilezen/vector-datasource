abe5d537bb3f7934dfc277a7e892aad6c2c1a7f6
Peiti Li <pli2@snapchat.com>
2021-11-08 15:03:49 -0800

    change

--- integration-test/1983-china-south-sea.py
+++ integration-test/1983-china-south-sea.py
@@ -13,10 +13,8 @@ class NaturalEarth(FixtureTest):
                 'featurecla': 'Claim boundary',
                 'name': 'Chinese claim',
                 'adm0_usa': -1,
-                'note': 'Taiwan',
                 'scalerank': 7,
                 'fclass_cn': 'International boundary (verify)',
-                'fclass_tw': 'Unrecognized',
                 'fclass_us': 'Unrecognized',
                 'min_zoom': 7.0,  # min_zoom is set to 5 in the source data
                 'source': 'naturalearthdata.com',
@@ -27,7 +25,6 @@ class NaturalEarth(FixtureTest):
                 zoom, x, y, 'boundaries', {
                     'kind': 'disputed_claim',
                     'kind:cn': 'country',
-                    'kind:tw': 'unrecognized_country',
                     'kind:us': 'unrecognized_country',
                     'min_zoom': 1,
                     # we change the min_zoom override in boundaries.yaml to
