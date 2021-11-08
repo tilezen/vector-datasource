edf4ac8c4c511d626c20ea4aa45e5619e9222102
Peiti Li <pli2@snapchat.com>
2021-11-08 14:59:46 -0800

    add integ test

---
+++ integration-test/1983-china-south-sea.py
@@ -0,0 +1,35 @@
+# -*- encoding: utf-8 -*-
+from . import FixtureTest
+
+
+class NaturalEarth(FixtureTest):
+    def test_claim_boundary(self):
+        import dsl
+
+        z, x, y = 1, 0, 0
+
+        self.generate_fixtures(
+            dsl.way(1, dsl.tile_diagonal_dexter(z, x, y), {
+                'featurecla': 'Claim boundary',
+                'name': 'Chinese claim',
+                'adm0_usa': -1,
+                'note': 'Taiwan',
+                'scalerank': 7,
+                'fclass_cn': 'International boundary (verify)',
+                'fclass_tw': 'Unrecognized',
+                'fclass_us': 'Unrecognized',
+                'min_zoom': 7.0,  # min_zoom is set to 5 in the source data
+                'source': 'naturalearthdata.com',
+            }),
+        )
+        for zoom in range(1, 17):
+            self.assert_has_feature(
+                zoom, x, y, 'boundaries', {
+                    'kind': 'disputed_claim',
+                    'kind:cn': 'country',
+                    'kind:tw': 'unrecognized_country',
+                    'kind:us': 'unrecognized_country',
+                    'min_zoom': 1,
+                    # we change the min_zoom override in boundaries.yaml to
+                    # 1 so we assert here  # noqa
+                })
