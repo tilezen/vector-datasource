4e184fd5294b05ce4f5baeeaac8dd5544413e67f
Travis Grigsby <tgrigsby@snapchat.com>
2021-11-10 09:27:33 -0800

    removing more things

--- integration-test/1979-shield-text-length.py
+++ integration-test/1979-shield-text-length.py
@@ -2,59 +2,64 @@
 import dsl
 from . import FixtureTest
 
+z, x, y = (16, 10470, 25340)
+
+way35 = {
+    'cycleway:right': 'lane',
+    'hgv': 'designated',
+    'hgv:state_network': 'yes',
+    'highway': 'primary',
+    'lanes': '2',
+    'lcn_ref': '50',
+    'maxspeed': '35 mph',
+    'name': 'Sloat Boulevard',
+    'name:etymology:wikidata': 'Q634931',
+    'oneway': 'yes',
+    'ref': 'CA 35',
+    'sidewalk': 'right',
+    'source': 'openstreetmap.org',
+    'source:hgv:state_network': 'Caltrans http://www.dot.ca.gov/hq/traffops/trucks/truckmap/',
+    'tiger:cfcc': 'A45',
+    'tiger:county': 'San Francisco, CA',
+    'tiger:name_base': 'Sloat',
+    'tiger:name_type': 'Blvd',
+}
+
+rel35 = {
+    'network': 'US:CA',
+    'ref': '35',
+    'route': 'road',
+    'source': 'openstreetmap.org',
+    'type': 'route',
+}
+
+rel50Cycle = {
+    'cycle_network': 'US:CA:SF',
+    'network': 'lcn',
+    'ref': '50',
+    'source': 'openstreetmap.org',
+    'route': 'bicycle',
+    'type': 'route',
+}
+
+rel23Bus = {
+    'network': 'Muni',
+    'ref': '23',
+    'route': 'bus',
+    'source': 'openstreetmap.org',
+    'type': 'route',
+}
+
 
 class ShieldTextLengthTest(FixtureTest):
 
     def test_create_shield_text_length(self):
-        import dsl
-
-        z, x, y = (16, 10470, 25340)
-
-        # this is real data. For the rest of the tests, it will be modified
         self.generate_fixtures(
             # https://www.openstreetmap.org/way/417097119
-            dsl.way(417097119, dsl.tile_diagonal(z, x, y), {
-                'cycleway:right': 'lane',
-                'hgv': 'designated',
-                'hgv:state_network': 'yes',
-                'highway': 'primary',
-                'lanes': '2',
-                'lcn_ref': '50',
-                'maxspeed': '35 mph',
-                'name': 'Sloat Boulevard',
-                'name:etymology:wikidata': 'Q634931',
-                'oneway': 'yes',
-                'ref': 'CA 35',
-                'sidewalk': 'right',
-                'source': 'openstreetmap.org',
-                'source:hgv:state_network': 'Caltrans http://www.dot.ca.gov/hq/traffops/trucks/truckmap/',
-                'tiger:cfcc': 'A45',
-                'tiger:county': 'San Francisco, CA',
-                'tiger:name_base': 'Sloat',
-                'tiger:name_type': 'Blvd',
-            }),
-            dsl.relation(1976278, {
-                'network': 'US:CA',
-                'ref': '35',
-                'route': 'road',
-                'source': 'openstreetmap.org',
-                'type': 'route',
-            }, ways=[417097119]),
-            dsl.relation(32312, {
-                'cycle_network': 'US:CA:SF',
-                'network': 'lcn',
-                'ref': '50',
-                'source': 'openstreetmap.org',
-                'route': 'bicycle',
-                'type': 'route',
-            }, ways=[417097119]),
-            dsl.relation(3002741, {
-                'network': 'Muni',
-                'ref': '23',
-                'route': 'bus',
-                'source': 'openstreetmap.org',
-                'type': 'route',
-            }, ways=[417097119])
+            dsl.way(417097119, dsl.tile_diagonal(z, x, y), way35),
+            dsl.relation(1976278, rel35, ways=[417097119]),
+            dsl.relation(32312, rel50Cycle, ways=[417097119]),
+            dsl.relation(3002741, rel23Bus, ways=[417097119])
         )
 
         self.assert_has_feature(
@@ -73,55 +78,22 @@ class ShieldTextLengthTest(FixtureTest):
 
     # empty strings and route refs over 6 chars in length don't report length
     def test_lengths_over_6_or_empty_are_not_reported(self):
-        import dsl
 
-        z, x, y = (16, 10470, 25340)
+        rel123456 = rel35.copy()
+        rel123456['ref'] = '123456'
 
-        # modify the data to include different names whose lengths shouldn't be included in the output
+        relCycleTooLong = rel50Cycle.copy()
+        relCycleTooLong['ref'] = '1234567'
+
+        relBusEmpty = rel23Bus.copy()
+        relBusEmpty['ref'] = ''
+
         self.generate_fixtures(
             # https://www.openstreetmap.org/way/417097119
-            dsl.way(417097119, dsl.tile_diagonal(z, x, y), {
-                'cycleway:right': 'lane',
-                'hgv': 'designated',
-                'hgv:state_network': 'yes',
-                'highway': 'primary',
-                'lanes': '2',
-                'lcn_ref': '50',
-                'maxspeed': '35 mph',
-                'name': 'Sloat Boulevard',
-                'name:etymology:wikidata': 'Q634931',
-                'oneway': 'yes',
-                'ref': 'CA 35',
-                'sidewalk': 'right',
-                'source': 'openstreetmap.org',
-                'source:hgv:state_network': 'Caltrans http://www.dot.ca.gov/hq/traffops/trucks/truckmap/',
-                'tiger:cfcc': 'A45',
-                'tiger:county': 'San Francisco, CA',
-                'tiger:name_base': 'Sloat',
-                'tiger:name_type': 'Blvd',
-            }),
-            dsl.relation(1976278, {
-                'network': 'US:CA',
-                'ref': '123456',
-                'route': 'road',
-                'source': 'openstreetmap.org',
-                'type': 'route',
-            }, ways=[417097119]),
-            dsl.relation(32312, {
-                'cycle_network': 'US:CA:SF',
-                'network': 'lcn',
-                'ref': '1234567',
-                'source': 'openstreetmap.org',
-                'route': 'bicycle',
-                'type': 'route',
-            }, ways=[417097119]),
-            dsl.relation(3002741, {
-                'network': 'Muni',
-                'ref': '',
-                'route': 'bus',
-                'source': 'openstreetmap.org',
-                'type': 'route',
-            }, ways=[417097119])
+            dsl.way(417097119, dsl.tile_diagonal(z, x, y), way35),
+            dsl.relation(1976278, rel123456, ways=[417097119]),
+            dsl.relation(32312, relCycleTooLong, ways=[417097119]),
+            dsl.relation(3002741, relBusEmpty, ways=[417097119])
         )
 
         self.assert_has_feature(
@@ -137,33 +109,11 @@ class ShieldTextLengthTest(FixtureTest):
             'id': 417097119, 'bicycle_shield_text_length': '7', 'bus_shield_text_length': '0'})
 
     def test_walking_shield_works(self):
-        import dsl
-
-        z, x, y = (16, 10470, 25340)
 
         # modify the data to include a trail to make sure we're adding walking shield text length too
         self.generate_fixtures(
             # https://www.openstreetmap.org/way/417097119
-            dsl.way(417097119, dsl.tile_diagonal(z, x, y), {
-                'cycleway:right': 'lane',
-                'hgv': 'designated',
-                'hgv:state_network': 'yes',
-                'highway': 'primary',
-                'lanes': '2',
-                'lcn_ref': '50',
-                'maxspeed': '35 mph',
-                'name': 'Sloat Boulevard',
-                'name:etymology:wikidata': 'Q634931',
-                'oneway': 'yes',
-                'ref': 'CA 35',
-                'sidewalk': 'right',
-                'source': 'openstreetmap.org',
-                'source:hgv:state_network': 'Caltrans http://www.dot.ca.gov/hq/traffops/trucks/truckmap/',
-                'tiger:cfcc': 'A45',
-                'tiger:county': 'San Francisco, CA',
-                'tiger:name_base': 'Sloat',
-                'tiger:name_type': 'Blvd',
-            }),
+            dsl.way(417097119, dsl.tile_diagonal(z, x, y), way35),
             dsl.relation(1976278, {
                 "name": "Pacific Crest Trail",
                 "network": "nwn",
@@ -183,8 +133,6 @@ class ShieldTextLengthTest(FixtureTest):
             })
 
     def test_hgv_shield_text_length(self):
-        z, x, y = (16, 0, 0)
-
         self.generate_fixtures(
             dsl.way(1, dsl.tile_diagonal(z, x, y),
                     {'highway': 'unclassified',
