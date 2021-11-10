5bf80de22cd5ef832c63e2fd33462a9247497ead
Peiti Li <pli2@snapchat.com>
2021-11-09 16:47:48 -0800

    linter

--- integration-test/1982-covered-water.py
+++ integration-test/1982-covered-water.py
@@ -4,13 +4,14 @@ from shapely.wkt import loads as wkt_loads
 from tilequeue.tile import deg2num
 from . import FixtureTest
 
+
 class CoveredWaterOSMTest(FixtureTest):
     def test_water_level_1(self):
         z, x, y = (16, 33199, 22547)
 
         self.generate_fixtures(
-            # https://www.openstreetmap.org/way/236735251
-            dsl.way(236735251, dsl.tile_box(z, x, y), {
+            # to represent https://www.openstreetmap.org/way/28575415
+            dsl.way(28575415, dsl.tile_box(z, x, y), {
                 'covered': 'yes',
                 'name': u'Sutro Reservoir',
                 'source': 'openstreetmap.org',
