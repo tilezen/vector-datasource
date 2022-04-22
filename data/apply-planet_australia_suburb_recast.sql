0a4cdcb0dc91cd366bc29765b134a58c05883f57
junderwood <junderwood@snapchat.com>
2022-04-22 12:24:09 -0700

    addressed nits

--- data/apply-planet_australia_suburb_recast.sql
+++ data/apply-planet_australia_suburb_recast.sql
@@ -1,6 +1,6 @@
--- Suburbs in Australia are treated more like cities, so this recasts them to  place=town
+-- Suburbs in Australia are treated more like cities, so this recasts them to place=town.
 
-with bbox as
+with bounds as
          (select *
           from (values
                     (st_transform(st_geomfromtext('Polygon ((116.16 -14.31, 104.29 -22.95, 113.34 -38.30, 155.33 -47.28, 156.30 -21.83, 144.91 -9.41, 127.14 -9.73, 116.16 -14.31))', 4326), 3857))
@@ -12,10 +12,11 @@ with bbox as
 
      selection as
          (select * from suburb,
-                        bbox
-          where st_within(way, bbox.geom)),
+                        bounds
+          where st_within(way, geom)),
 
-    trim_major_cities as
+-- These cities are already present in OSM as place=city so we don't want to show any locality point that could cause duplicates
+     trim_major_cities as
         (select * from selection
             where tags->'name' not in (
                 'Adelaide'
