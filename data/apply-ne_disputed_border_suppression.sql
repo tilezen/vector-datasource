e4288bdeb2da11b03d18289700c627a04f5c346a
junderwood <junderwood@snapchat.com>
2022-04-25 15:22:26 -0700

    remove table

--- data/apply-ne_disputed_border_suppression.sql
+++ data/apply-ne_disputed_border_suppression.sql
@@ -58,9 +58,5 @@ ne_10m_admin_0_boundary_lines_disputed_areas as
     (delete from ne_10m_admin_0_boundary_lines_disputed_areas l
     where l.ne_id in (select ne_id from remove_lines)),
 
-ne_50m_admin_0_boundary_lines_disputed_areas as
-    (delete from ne_50m_admin_0_boundary_lines_disputed_areas l
-    where l.ne_id in (select ne_id from remove_lines))
-
-delete from ne_110m_admin_0_boundary_lines_disputed_areas l
-where l.ne_id in (select ne_id from remove_lines)
+delete from ne_50m_admin_0_boundary_lines_disputed_areas l
+    where l.ne_id in (select ne_id from remove_lines)
