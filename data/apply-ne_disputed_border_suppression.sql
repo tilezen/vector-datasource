1211d72693a751f04413304c3f903b9e536e4534
junderwood <junderwood@snapchat.com>
2022-04-25 16:41:30 -0700

    typo

--- data/apply-ne_disputed_border_suppression.sql
+++ data/apply-ne_disputed_border_suppression.sql
@@ -56,7 +56,7 @@ ne_110m_admin_0_boundary_lines_land as
 
 ne_10m_admin_0_boundary_lines_disputed_areas as
     (delete from ne_10m_admin_0_boundary_lines_disputed_areas l
-    where l.ne_id in (select ne_id from remove_lines)),
+    where l.ne_id in (select ne_id from remove_lines))
 
 delete from ne_50m_admin_0_boundary_lines_disputed_areas l
     where l.ne_id in (select ne_id from remove_lines)
