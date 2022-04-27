65a490d051d748d3c26904214d32aabebfce3215
Travis Grigsby <tgrigsby@snapchat.com>
2022-04-27 10:35:29 -0700

    double whoops

--- vectordatasource/transform.py
+++ vectordatasource/transform.py
@@ -7,6 +7,12 @@ from collections import namedtuple
 from math import ceil
 from numbers import Number
 from sys import float_info
+from tilequeue.process import _make_valid_if_necessary
+from tilequeue.process import _visible_shape
+from tilequeue.tile import calc_meters_per_pixel_area
+from tilequeue.tile import normalize_geometry_type
+from tilequeue.tile import tolerance_for_zoom
+from tilequeue.transform import calculate_padded_bounds
 
 import hanzidentifier
 import kdtree
@@ -28,12 +34,6 @@ from shapely.ops import linemerge
 from shapely.strtree import STRtree
 from sort import pois as sort_pois
 from StreetNames import short_street_name
-from tilequeue.process import _make_valid_if_necessary
-from tilequeue.process import _visible_shape
-from tilequeue.tile import calc_meters_per_pixel_area
-from tilequeue.tile import normalize_geometry_type
-from tilequeue.tile import tolerance_for_zoom
-from tilequeue.transform import calculate_padded_bounds
 from util import safe_int
 from util import to_float
 from zope.dottedname.resolve import resolve
@@ -9728,7 +9728,7 @@ def create_dispute_ids(shape, props, fid, zoom):
     breakaway_code = props.pop('tz_breakaway_code', None)
     if breakaway_code is None:
         # no breakaway code, not a dispute
-        return
+        return shape, props, fid
 
     breakaway_code_str = str(breakaway_code)
     ne_id = str(props.pop('tz_ne_id', None))
