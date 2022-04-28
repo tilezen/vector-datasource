a784e74c8eee4190198a22e9a94a17280cb7a3b1
junderwood <junderwood@snapchat.com>
2022-04-28 12:37:41 -0700

    typo

--- osm2pgsql.lua
+++ osm2pgsql.lua
@@ -508,7 +508,7 @@ function osm2pgsql.process_node(object)
     end
 -- Show Abkhazia label as region for most countries
     if object.tags.place and object.tags.wikidata == 'Q23334' then
-        output_hstore['place'] = 'unrecognized'
+        output_hstore['place'] = 'region'
         output_hstore['place:RU'] = 'country'
     end
 -- Show South Ossetia label as region for most countries
