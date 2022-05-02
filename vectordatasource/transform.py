0976306e64812bf4533f06b36405d4e5085f27a6
junderwood <junderwood@snapchat.com>
2022-05-02 15:01:40 -0700

    updates

--- vectordatasource/transform.py
+++ vectordatasource/transform.py
@@ -8912,8 +8912,8 @@ def tags_set_ne_min_max_zoom(ctx):
             # way, we probably don't want to see them at zoom, which is lower
             # than most of the curated NE min zooms. see issue #1826 for more
             # information.
-            props['kind'] = 'unrecognised'
-            props['min_zoom'] = max(6, props['min_zoom'])
+            props['kind'] = 'unrecognized'
+            props['min_zoom'] = max(8, props['min_zoom'])
 
         elif props.get('kind') == 'region':
             props['min_zoom'] = max(8, props['min_zoom'])
