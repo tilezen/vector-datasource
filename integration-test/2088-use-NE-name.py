676bd8596c3f9c555c5ebbc41994f826f6611b8c
junderwood <junderwood@snapchat.com>
2022-06-23 15:05:48 -0700

    added another test

--- integration-test/2088-use-NE-name.py
+++ integration-test/2088-use-NE-name.py
@@ -63,3 +63,23 @@ class TestNEName(FixtureTest):
                 'id': 10200326532,
                 'name:en': u'Switzerland'
             })
+
+    def test_null_name(self):
+        z, x, y = (16, 10482, 25333)
+
+        self.generate_fixtures(
+            dsl.point(10200326532, (-122.4195493, 37.7653381), {
+                u'place': u'country',
+                u'name': u'Schweiz/Suisse/Svizzera/Svizra',
+                u'name:en': u'Switzerland',
+                u'source': u'openstreetmap.org',
+                u'wikidata': u'Q30',
+                u'ne_name_en': u''
+            }),
+        )
+
+        self.assert_has_feature(
+            z, x, y, 'places', {
+                'id': 10200326532,
+                'name:en': u'Switzerland'
+            })
