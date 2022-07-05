19e0d9994f276ea29b1bcf188f7301637f95b537
travisgrigsby <tgrigsby@snapchat.com>
2022-07-05 16:47:43 -0700

    Travisg/20220630 add root id to buildings (#2093)
    * add a root_id to all buildings that's a copy of the id
    * add tests

--- vectordatasource/transform.py
+++ vectordatasource/transform.py
@@ -8404,6 +8404,7 @@ def buildings_unify(ctx):
             if building_id:
                 indexed_building = geom_with_building_id(shape, building_id)
                 indexable_buildings.append(indexed_building)
+                props['root_id'] = building_id
         elif kind == 'building_part':
             parts.append(feature)
 
