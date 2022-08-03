cbbb78befc2bf17c8ed1b45756c98d06ff8995e9
jeffdefacto <jeffdefacto@gmail.com>
2022-08-03 12:30:05 -0700

    update test

--- integration-test/2088-use-NE-name.py
+++ integration-test/2088-use-NE-name.py
@@ -12,16 +12,22 @@ class TestNEName(FixtureTest):
                 u'place': u'country',
                 u'name': u'Schweiz/Suisse/Svizzera/Svizra',
                 u'name:en': u'Switzerland',
+                u'name:zh-Hans': u'瑞士',
+                u'name:zh-Hant': u'瑞士',
                 u'source': u'openstreetmap.org',
                 u'wikidata': u'Q30',
-                u'ne_name_en': u'Test'
+                u'ne_name_en': u'Test',
+                u'ne_name_zh-Hans': u'瑞士联邦',
+                u'ne_name_zh-Hant': u'瑞士聯邦'
             }),
         )
 
         self.assert_has_feature(
             z, x, y, 'places', {
                 'id': 10200326532,
-                'name:en': u'Test'
+                'name:en': u'Test',
+                'name:zh-Hans': u'瑞士联邦',
+                'name:zh-Hant': u'瑞士聯邦'
             })
 
     def test_use_ne_name_hamlet(self):
@@ -32,16 +38,22 @@ class TestNEName(FixtureTest):
                 u'place': u'hamlet',
                 u'name': u'Schweiz/Suisse/Svizzera/Svizra',
                 u'name:en': u'Switzerland',
+                u'name:zh-Hans': u'瑞士',
+                u'name:zh-Hant': u'瑞士',
                 u'source': u'openstreetmap.org',
                 u'wikidata': u'Q30',
-                u'ne_name_en': u'Test'
+                u'ne_name_en': u'Test',
+                u'ne_name_zh-Hans': u'瑞士联邦',
+                u'ne_name_zh-Hant': u'瑞士聯邦'
             }),
         )
 
         self.assert_has_feature(
             z, x, y, 'places', {
                 'id': 10200326532,
-                'name:en': u'Test'
+                'name:en': u'Test',
+                'name:zh-Hans': u'瑞士联邦',
+                'name:zh-Hant': u'瑞士聯邦'
             })
 
     def test_no_ne_name(self):
@@ -52,15 +64,19 @@ class TestNEName(FixtureTest):
                 u'place': u'country',
                 u'name': u'Schweiz/Suisse/Svizzera/Svizra',
                 u'name:en': u'Switzerland',
+                u'name:zh-Hans': u'瑞士',
+                u'name:zh-Hant': u'瑞士',
                 u'source': u'openstreetmap.org',
-                u'wikidata': u'Q30',
+                u'wikidata': u'Q30'
             }),
         )
 
         self.assert_has_feature(
             z, x, y, 'places', {
                 'id': 10200326532,
-                'name:en': u'Switzerland'
+                'name:en': u'Switzerland',
+                'name:zh-Hans': u'瑞士',
+                'name:zh-Hant': u'瑞士'
             })
 
     def test_null_name(self):
