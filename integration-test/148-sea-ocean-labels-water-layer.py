68bc5bf8b9717533f7e9daf3c11a7b6b61c1fcc1
Nathaniel Kelso <nvkelso@gmail.com>
2023-03-06 16:50:35 -0800

    add area tests, refine point tests

--- integration-test/148-sea-ocean-labels-water-layer.py
+++ integration-test/148-sea-ocean-labels-water-layer.py
@@ -11,7 +11,7 @@ from . import FixtureTest
 class SeaOceanLabelsWaterLayer(FixtureTest):
 
     def test_gulf_of_california(self):
-        z = 16     # visible starting at zoom 9
+        z = 16     # visible starting at zoom 9 because of NE - OSM transition, but with different min_zoom
         x, y = deg2num(27.5578100671435, -111.704650024786, z)
 
         # Gulf of California: http://www.openstreetmap.org/node/305639734
@@ -28,15 +28,15 @@ class SeaOceanLabelsWaterLayer(FixtureTest):
             {'kind': 'sea', 'name': 'Gulf of California'})
         # ensure the zoom range is correct vis-a-vis the size
         self.assert_no_matching_feature(
-            3, x, y, 'water',
+            4, x, y, 'water',
             {'kind': 'sea', 'name': 'Gulf of California'})
         self.assert_has_feature(
-            4, x, y, 'water',
+            5, x, y, 'water',
             {'kind': 'sea', 'name': 'Gulf of California',
              'label_placement': True})
 
     def test_greenland_sea(self):
-        z = 16     # visible starting at zoom 9
+        z = 16     # visible starting at zoom 9 because of NE - OSM transition, but with different min_zoom
         x, y = deg2num(74.9999999953685, -10.0000000185705, z)
         # Greenland Sea: http://www.openstreetmap.org/node/305639396
         self.generate_fixtures(dsl.way(305639396, wkt_loads('POINT (-10.0000000185705 74.9999999953685)'), {u'name:pt': u'Mar da Gronel\xe2ndia', u'name:ms': u'Laut Greenland', u'name:ko': u'\uadf8\ub9b0\ub780\ub4dc \ud574', u'gns:dsg': u'SEA', u'name:ar': u'\u0628\u062d\u0631 \u063a\u0631\u064a\u0646\u0644\u0627\u0646\u062f', u'name:cs': u'Gr\xf3nsk\xe9 mo\u0159e', u'wikidata': u'Q132868', u'name:it': u'Mare di Groenlandia', u'name:vi': u'Bi\u1ec3n Greenland', u'name:pl': u'Morze Grenlandzkie', u'name:fi': u'Gr\xf6nlanninmeri', u'name:da': u'Gr\xf8nlandshavet', u'wikipedia': u'en:Greenland Sea',
@@ -52,11 +52,110 @@ class SeaOceanLabelsWaterLayer(FixtureTest):
             {'kind': 'sea', 'name': 'Greenland Sea'})
         # ensure the zoom range is correct vis-a-vis the size
         self.assert_no_matching_feature(
-            3, x, y, 'water',
+            4, x, y, 'water',
             {'kind': 'sea', 'name': 'Greenland Sea'})
         self.assert_has_feature(
-            4, x, y, 'water',
+            5, x, y, 'water',
             {'kind': 'sea', 'name': 'Greenland Sea',
              'label_placement': True})
+
+    def test_hudson_bay(self):
+        lon, lat = (-122.417169, 37.769196)
+
+        for z in range(0, 16):
+            x, y = deg2num(lat, lon, z)
+            # without remapping min_zoom this area would result in min_zoom of 6,
+            # but after remapping it'd be 9
+            area = 265548000
+            self.generate_fixtures(
+                dsl.way(1, dsl.box_area(z, x, y, area), {
+                    'name': u'Hudson Bay',
+                    'place': u'sea',
+                    'wikidata': u'Q1099503',
+                    'source': u'openstreetmap.org',
+                }),
+            )
+
+            if z < 4:
+                self.assert_no_matching_feature(z, x, y, 'water',
+                                                {
+                                                    'kind': 'sea',
+                                                    'name': 'Hudson Bay',
+                                                    'label_placement': True
+                                                })
+            if 4 <= z <= 15:
+                with self.features_in_tile_layer(z, x, y, 'water') as features:
+                    for f in features:
+                        if f['geometry']['type'] == 'Point':
+                            assert f['id'] is None
+                            assert f['properties']['min_zoom'] == 4
+                            assert f['properties']['name'] == 'Hudson Bay'
+                            assert f['properties']['kind'] == 'sea'
+
+    def test_hudson_bay(self):
+        lon, lat = (-122.417169, 37.769196)
+
+        for z in range(0, 16):
+            x, y = deg2num(lat, lon, z)
+            # without remapping min_zoom this area would result in min_zoom of 6,
+            # but after remapping it'd be 9
+            area = 5000000001
+            self.generate_fixtures(
+                dsl.way(9441240, dsl.box_area(z, x, y, area), {
+                    'name': u'Hudson Bay',
+                    'place': u'sea',
+                    'wikidata': u'Q3040',
+                    'source': u'openstreetmap.org',
+                }),
+            )
+
+            if z < 3:
+                self.assert_no_matching_feature(z, x, y, 'water',
+                                                {
+                                                    'kind': 'sea',
+                                                    'name': 'Hudson Bay',
+                                                    'label_placement': True
+                                                })
+            if 3 <= z <= 15:
+                with self.features_in_tile_layer(z, x, y, 'water') as features:
+                    for f in features:
+                        if f['geometry']['type'] == 'Point':
+                            assert f['id'] is None
+                            assert f['properties']['min_zoom'] == 4
+                            assert f['properties']['name'] == 'Hudson Bay'
+                            assert f['properties']['kind'] == 'sea'
+
+    def test_james_bay(self):
+        lon, lat = (-122.417169, 37.769196)
+
+        for z in range(0, 16):
+            x, y = deg2num(lat, lon, z)
+            # without remapping min_zoom this area would result in min_zoom of 6,
+            # but after remapping it'd be 9
+            area = 265548000
+            self.generate_fixtures(
+                dsl.way(13360255, dsl.box_area(z, x, y, area), {
+                    'name': u'James Bay',
+                    'place': u'sea',
+                    'wikidata': u'Q223810',
+                    'source': u'openstreetmap.org',
+                }),
+            )
+
+            if z < 5:
+                self.assert_no_matching_feature(z, x, y, 'water',
+                                                {
+                                                    'kind': 'sea',
+                                                    'name': 'James Bay',
+                                                    'label_placement': True
+                                                })
+            if 5 <= z <= 15:
+                with self.features_in_tile_layer(z, x, y, 'water') as features:
+                    for f in features:
+                        if f['geometry']['type'] == 'Point':
+                            assert f['id'] is None
+                            assert f['properties']['min_zoom'] == 4
+                            assert f['properties']['name'] == 'James Bay'
+                            assert f['properties']['kind'] == 'sea'
 
 # NOTE: No ocean points in the North America extract :-(
